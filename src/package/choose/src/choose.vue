9e8d2e508b898a97cb8927a8c1b31945d1956356
liaoyanli5 <liaoyanli@jd.com>
2018-05-16 09:53:57 +0800

    20180516添加地址组件配置

---
+++ src/package/choose/src/choose.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="nut-choose"></div>
+</template>
+<script>
+export default {
+    name:'nut-choose',
+    props: {
+    },
+    data() {
+        return {};
+    },
+    methods: {
+    }
+}
+</script>
+<style lang="scss">
+</style>
