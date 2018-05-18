2acac2060f4444ba52affc056713414533699b5f
Franslee <dmon@foxmail.com>
2018-05-18 16:19:45 +0800

    update

--- dist/98.js
+++ dist/98.js
@@ -1,2 +1,2 @@
-/*! NutUI v1.2.5 Fri May 18 2018 09:29:07 GMT+0800 (中国标准时间) */
+/*! NutUI v1.2.6 Fri May 18 2018 15:11:19 GMT+0800 (中国标准时间) */
 webpackJsonpnutui([98],{800:function(l,n,u){var o=u(1)(null,null,null,null,null);l.exports=o.exports}});
