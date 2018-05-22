afab53da4c976244fd196ebe162bb5fa12fb86de
Franslee <dmon@foxmail.com>
2018-05-22 14:52:49 +0800

    优化ES2015+代码压缩

--- dist/100.js
+++ dist/100.js
@@ -1,2 +1,2 @@
-/*! NutUI v1.2.6 Mon May 21 2018 14:26:35 GMT+0800 (中国标准时间) */
+/*! NutUI v1.2.7 Tue May 22 2018 14:50:35 GMT+0800 (中国标准时间) */
 webpackJsonpnutui([100],{807:function(l,n,u){var o=u(1)(null,null,null,null,null);l.exports=o.exports}});
