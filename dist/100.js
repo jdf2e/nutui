826434f2c35e16a18dada5656a8d708d928f80c1
franslee <dmon@foxmail.com>
2018-06-05 15:49:31 +0800

    update

--- dist/100.js
+++ dist/100.js
@@ -1,2 +1,2 @@
-/*! NutUI v1.2.7 Mon Jun 04 2018 20:06:11 GMT+0800 (CST) */
+/*! NutUI v1.2.7 Tue Jun 05 2018 15:08:45 GMT+0800 (CST) */
 webpackJsonpnutui([100],{814:function(l,n,u){var o=u(1)(null,null,null,null,null);l.exports=o.exports}});
