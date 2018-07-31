823d0c71441e50b445ee48be5d37e2ab04a15fc0
wangnan <wangnan7@jd.com>
2018-07-31 20:22:15 +0800

    short pwd

---
+++ src/demo/shortpwd.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="wrapper">
+        <a href="javascript:;" @click="showShortpwd">点击打开短密码框</a>
+        <nut-shortpwd  :show="isShow"  @close="shortpwdClose"/>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    data(){
+        return{
+            isShow:false,
+        }
+    },
+    components: {
+       
+    },
+    methods:{
+        
+        showShortpwd(){
+            this.isShow = true;
+        },
+        shortpwdClose(password){
+            this.isShow = false;
+            alert('您输入的密码是：'+password);
+        }
+    },
+    mounted(){
+       
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper{
+    .box{
+        font-size:.5rem;
+    }
+    h3{
+        font-size:.3rem;
+    }
+}
+</style>
---
+++ src/view/shortpwd.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        
+        <h5>Props</h5>
+        <div class="tbl-wrapper">
+        <table class="u-full-width">
+          <thead>
+            <tr>
+              <th>参数</th>
+              <th>说明</th>
+              <th>类型</th>
+              <th>默认值</th>
+              <th>可选值</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>show</td>
+              <td>显示6位密码弹层</td>
+              <td>Boolean</td>
+              <td>false</td>
+              <td>true/false</td>
+            </tr>
+            
+            </tr>
+          </tbody>
+        </table>
+        </div>
+
+        <h5>Events</h5>
+        <div class="tbl-wrapper">
+        <table class="u-full-width">
+          <thead>
+            <tr>
+              <th>事件名</th>
+              <th>说明</th>
+              <th>回调参数</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>close</td>
+              <td>关闭弹层</td>
+              <td>password（关闭如果是6位填满的，将会返回密码）</td>
+            </tr>
+           
+          </tbody>
+        </table>
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+          demo1:`<nut-shortpwd  :show="isShow"  @close="shortpwdClose"/>`,
+         
+        }
+    },
+    methods:{
+      
+    }
+}
+</script>
+
+<style>
+
+</style>
