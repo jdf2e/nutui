7da91d11543256f8f6247d5e63f2ba0bc9099d0c
360BUYAD\wangyue92 <wangyue92@jd.com>
2018-09-19 17:33:08 +0800

    demo展示

--- src/demo/navbar.vue
+++ src/demo/navbar.vue
@@ -4,61 +4,63 @@
         :name="$route.name"
         ></nut-demoheader>
         <!-- DEMO区域 -->
-        
+        <nut-navBtn>
+
+        </nut-navBtn>
         <!-- DEMO代码 -->
     </div>
 </template>
-<script>
-export default {
-    data(){
-        return{
-            direction: 'h',
-            boundary: {top: 50, bottom: 20, left: 0, right: 0},
-            isSide: false,
-            demo1:`<nut-drag direction="h"></nut-drag>`,
-            demo2:`<nut-drag :boundary="{top: 20, bottom: 20}"></nut-drag>`,
-            demo3:`<nut-drag :isSide="false"></nut-drag>`,
-        }
-    },
-    methods:{
-        changeMove(x, e){
-            let dom = e.currentTarget;
-            let siblings = dom.parentElement.children;
-            this.removeClass(siblings);
-            dom.classList.add('curr');
-            this.direction = x;
-        },
-        changeBoundary(x, e){
-            let dom = e.currentTarget;
-            let siblings = dom.parentElement.children;
-            this.removeClass(siblings);
-            dom.classList.add('curr');
-            if (x=='y') {
-                this.boundary = {top: 50, bottom: 20, left: 0, right: 0};
-            }else{
-
-                this.boundary = {top: 50, bottom: 0, left: 0, right: 0};
-            }
+// <script>
+// export default {
+//     data(){
+//         return{
+//             direction: 'h',
+//             boundary: {top: 50, bottom: 20, left: 0, right: 0},
+//             isSide: false,
+//             demo1:`<nut-drag direction="h"></nut-drag>`,
+//             demo2:`<nut-drag :boundary="{top: 20, bottom: 20}"></nut-drag>`,
+//             demo3:`<nut-drag :isSide="false"></nut-drag>`,
             
-        },
-        changeSide(b, e){
-            let dom = e.currentTarget;
-            let siblings = dom.parentElement.children;
-            this.removeClass(siblings);
-            dom.classList.add('curr');
-            dom.classList.add('curr');
-            this.isSide = b;
-        },
-        removeClass(siblings){  //去除同级的class
-            for ( let i in siblings) {
-                if (siblings.hasOwnProperty(i) === true){
-                    siblings[i].className = 'button-primary';
-                }
-            }
-        },
-    }
-}
-</script>
+//         }
+//     },
+//     methods:{
+//         changeMove(x, e){
+//             let dom = e.currentTarget;
+//             let siblings = dom.parentElement.children;
+//             this.removeClass(siblings);
+//             dom.classList.add('curr');
+//             this.direction = x;
+//         },
+//         changeBoundary(x, e){
+//             let dom = e.currentTarget;
+//             let siblings = dom.parentElement.children;
+//             this.removeClass(siblings);
+//             dom.classList.add('curr');
+//             if (x=='y') {
+//                 this.boundary = {top: 50, bottom: 20, left: 0, right: 0};
+//             }else{
+//                 this.boundary = {top: 50, bottom: 0, left: 0, right: 0};
+//             }
+            
+//         },
+//         changeSide(b, e){
+//             let dom = e.currentTarget;
+//             let siblings = dom.parentElement.children;
+//             this.removeClass(siblings);
+//             dom.classList.add('curr');
+//             dom.classList.add('curr');
+//             this.isSide = b;
+//         },
+//         removeClass(siblings){  //去除同级的class
+//             for ( let i in siblings) {
+//                 if (siblings.hasOwnProperty(i) === true){
+//                     siblings[i].className = 'button-primary';
+//                 }
+//             }
+//         },
+//     }
+// }
+// </script>
 
 
 <style lang="scss" scoped>
