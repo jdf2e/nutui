7c84409fa169dc981e17694fb35b15eaecbcc32d
Frans <dmon@foxmail.com>
2019-01-08 17:30:41 +0800

    update

--- src/packages/radio/demo.vue
+++ src/packages/radio/demo.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="demo-list">
-    <h4>基本用法</h4>
+    <h4>Radio基本用法</h4>
+    <div>
+      <nut-cell>
+        <span slot="title">
+          <nut-radio v-model="radioVal" :label="2">备选项</nut-radio>
+        </span>
+      </nut-cell>
+    </div>
+    
+    <h4>组合使用Radio</h4>
     <div>
       <nut-cell>
         <span slot="title">
@@ -10,25 +19,48 @@
         <div slot="desc">radioVal1: {{radioVal1}}</div>
       </nut-cell>
     </div>
+    <p>组合使用 Radio 时推荐使用 radiogroup 组件，见下方示例</p>
 
-    <h4>禁用状态</h4>
+    <h4>RadioGroup基本用法</h4>
     <div>
       <nut-cell>
         <span slot="title">
-          <nut-radio :disabled="true" v-model="radioVal2" label="禁用">备选项1</nut-radio>
+          <nut-radiogroup v-model="radioGroupVal1">
+            <nut-radio label="a">备选项1</nut-radio>
+            <nut-radio label="b">备选项2</nut-radio>
+          </nut-radiogroup>
+        </span>
+      </nut-cell>
+    </div>
+
+    <h4>Radio禁用</h4>
+    <div>
+      <nut-cell>
+        <span slot="title">
+          未选中时的禁用状态：<nut-radio :disabled="true" v-model="radioVal2" label="禁用">备选项1</nut-radio>
         </span>
       </nut-cell>
       <nut-cell>
         <span slot="title">
-          <nut-radio :disabled="true" v-model="radioVal2" label="选中且禁用">备选项2</nut-radio>
+          已选中时的禁用状态：<nut-radio :disabled="true" v-model="radioVal2" label="选中且禁用">备选项2</nut-radio>
         </span>
-      </nut-cell>
-      <nut-cell>
-        <span slot="title">radioVal2: {{radioVal2}}</span>
       </nut-cell>
     </div>
 
-    <h4>自定义尺寸</h4>
+    <h4>RadioGroup整体禁用</h4>
+    <div>
+      <nut-cell>
+        <span slot="title">
+          <nut-radiogroup v-model="radioGroupVal2" :disabled="true">
+            <nut-radio label="1">备选项1</nut-radio>
+            <nut-radio label="2">备选项2</nut-radio>
+            <nut-radio label="3">备选项3</nut-radio>
+          </nut-radiogroup>
+        </span>
+      </nut-cell>
+    </div>
+
+    <h4>Radio自定义尺寸</h4>
     <div>
       <nut-cell>
         <span slot="title">
@@ -48,34 +80,45 @@
       <p>size可选值：'small', 'base', 'large'</p>
     </div>
 
-    <h4>禁用动效</h4>
+    <h4>RadioGroup整体定义尺寸</h4>
     <div>
       <nut-cell>
         <span slot="title">
-          <nut-radio :animation="false" v-model="radioVal3" label="a">备选项1</nut-radio>
+          <nut-radiogroup v-model="radioGroupVal3" size="large">
+            <nut-radio label="1">备选项1</nut-radio>
+            <nut-radio label="2">备选项2</nut-radio>
+            <nut-radio label="3">备选项3</nut-radio>
+          </nut-radiogroup>
         </span>
       </nut-cell>
-      <nut-cell>
-        <span slot="title">
-          <nut-radio :animation="false" v-model="radioVal3" label="b">备选项2</nut-radio>
-        </span>
-      </nut-cell>
-      <p>animation属性值为false时，禁用自带动效</p>
     </div>
 
-    <h4>事件</h4>
+    <h4>Radio禁用动效</h4>
     <div>
       <nut-cell>
         <span slot="title">
-          <nut-radio v-model="radioVal4" label="a" @change="radioChange">备选项1</nut-radio>
+          <nut-radio :animated="false" v-model="radioVal3" label="a">备选项1</nut-radio>
         </span>
       </nut-cell>
       <nut-cell>
         <span slot="title">
-          <nut-radio v-model="radioVal4" label="b" @change="radioChange">备选项2</nut-radio>
+          <nut-radio :animated="false" v-model="radioVal3" label="b">备选项2</nut-radio>
         </span>
       </nut-cell>
-      <p>值发生变化时，将触发change事件</p>
+      <p>animated 属性值为 false 时，禁用自带动效</p>
+    </div>
+
+    <h4>RadioGroup禁用动效</h4>
+    <div>
+      <nut-cell>
+        <span slot="title">
+          <nut-radiogroup v-model="radioGroupVal4" :animated="false">
+            <nut-radio label="a">备选项1</nut-radio>
+            <nut-radio label="b">备选项2</nut-radio>
+            <nut-radio label="c">备选项3</nut-radio>
+          </nut-radiogroup>
+        </span>
+      </nut-cell>
     </div>
 
     <h4>自定义Class</h4>
@@ -98,18 +141,19 @@
 export default {
   data() {
     return {
-      result: "",
+      radioVal:1,
       radioVal1: 2,
       radioVal2: "选中且禁用",
       radioVal3: "b",
       radioVal4: "b",
-      radioVal5: "a"
+      radioVal5: "a",
+      radioGroupVal1:"b",
+      radioGroupVal2:"2",
+      radioGroupVal3:"2",
+      radioGroupVal4:"c",
     };
   },
   methods: {
-    radioChange(label) {
-      alert("选中的是" + label);
-    }
   }
 };
 </script>
--- src/packages/radio/radio.vue
+++ src/packages/radio/radio.vue
@@ -1,52 +1,81 @@
 <template>
-<label :class="['nut-radio','nut-radio-size-'+size]">
-    <input 
-    type="radio"
-    :class="{'nut-radio-ani':animation}"
-    :checked="value===label" 
-    :disabled="disabled"
-    :label="label"
-    :value="value"
-    @change="changeEvt"
+  <label :class="['nut-radio','nut-radio-size-'+currentSize]" @click="clickEvt">
+    <input
+      type="radio"
+      :value="currentValue"
+      :class="{'nut-radio-ani':isAnimated}"
+      :checked="currentValue === label"
+      :disabled="isDisabled"
+      :label="label"
     >
     <span class="nut-radio-label">
       <slot></slot>
     </span>
-</label>
+  </label>
 </template>
 <script>
+import findCptUpward from "../../mixins/findCptUpward/index.js";
+
 export default {
   name: "nut-radio",
+  mixins: [findCptUpward],
   props: {
     value: {
-        type: [String, Number, Boolean],
-        default: false
+      type: [String, Number, Boolean],
+      default: false
     },
-    label:{
-      type: [String, Number, Boolean]
-    },
+    label: [String, Number, Boolean],
     size: {
       type: String,
-      default: 'base'
+      default: "base"
     },
     disabled: {
       type: Boolean,
       default: false
     },
-    animation: {
+    animated: {
       type: Boolean,
       default: true
     }
   },
   data() {
-    return {}
+    return {
+      parent: null
+    };
   },
-  mounted(){
+  computed: {
+    currentValue: {
+      get() {
+        return this.parent ? this.parent.value : this.value;
+      },
+
+      set(val) {
+        (this.parent || this).$emit("input", val);
+      }
+    },
+    currentSize() {
+      return this.parent ? this.parent.size || this.size : this.size;
+    },
+    isDisabled() {
+      return this.parent
+        ? this.parent.disabled || this.disabled
+        : this.disabled;
+    },
+    isAnimated() {
+      return this.parent
+        ? this.parent.animated && this.animated
+        : this.animated;
+    }
+  },
+  created() {
+    this.findCptUpward("nut-radiogroup");
   },
   methods: {
-    changeEvt(event) {
-      this.$emit("change", this.label);
-      this.$emit('input', this.label);
+    clickEvt(event) {
+      if (this.isDisabled) {
+        return false;
+      }
+      this.currentValue = this.label;
     }
   }
 };
