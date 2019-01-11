fab7722411870ccaaf03ba8ebe22989a8f5b1d70
宋成林 <songchenglin1@ZBMAC-C02VX5RNH.local>
2019-01-11 14:28:32 +0800

    增加测试用例，修改日历时间

--- src/packages/calendar/demo.vue
+++ src/packages/calendar/demo.vue
@@ -35,7 +35,7 @@
             @close="switchPicker('isVisible')"
             @choose="setChooseValue"
             :start-date="`2018-10-11`"
-            :end-date="`2019-11-11`"
+            :end-date="`2020-11-11`"
         >
         </nut-calendar>
         <nut-calendar :is-visible="isVisible2"
@@ -65,6 +65,7 @@
 </template>
 
 <script>
+import Utils from "../../utils/date.js";
 export default {
     data() {
         return {
@@ -74,8 +75,8 @@ export default {
             isVisible3: false,
             date: null,
             dateWeek: null,
-            date1: ['2018-12-22', '2019-01-08'],
-            date2: '2018-11-22',
+            date1: ['2018-12-22', '2020-01-08'],
+            date2: Utils.getDay(30),
             date3: null
             
         };
