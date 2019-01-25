3d6ad8d41593c090cee3dbc15aa143c80cbce5bb
lilinsen <lilinsen@jd.com>
2019-01-25 13:35:19 +0800

    增加回到顶部

--- scripts/mdToVue.js
+++ scripts/mdToVue.js
@@ -83,7 +83,7 @@ function createdFile(output, sorce, ishasCode) {
     var bufs = `<template><div  @click="dsCode">
         <div v-if="content" class="layer">
           <pre><span class="close-box" @click="closelayer"></span><div v-html="content"></div></pre>
-        </div>`+ res + `</div></template><script>import root from '../root.js';
+        </div>`+ res + `<nut-backtop></nut-backtop></div></template><script>import root from '../root.js';
         export default {
             mixins:[root]
         }</script>`;
--- sites/doc/root.js
+++ sites/doc/root.js
@@ -1,3 +1,5 @@
+
+
 var myMixin ={
     data(){
         return {
