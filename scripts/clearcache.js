5aac968bede14d197db35c7b281f15a7d7fb080a
lilinsen <lilinsen@jd.com>
2019-05-07 17:39:39 +0800

    修改手动清除缓存功能

--- scripts/clearcache.js
+++ scripts/clearcache.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-fs.writeFileSync(path.join(__dirname,'./localdocs.cache'),"",'utf-8');
-fs.writeFileSync(path.join(__dirname,'./localsrc.cache'),"",'utf-8');
+let rimraf = require("rimraf");
+rimraf('./cache',res=>{console.log(res)})
 console.log("缓存清除成功")
