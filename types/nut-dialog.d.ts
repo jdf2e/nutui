60c61ee5d916afd0654a71aea086f340c352b24a
kain <zhangtqx@vip.qq.com>
2019-05-14 15:16:59 +0800

    Module Augmentation

--- types/nut-dialog.d.ts
+++ types/nut-dialog.d.ts
@@ -1,34 +1,38 @@
 import Vue from 'vue';
 
 interface NutDialogOption {
-  id: string | number;
-  title: string;
-  content: string;
-  type: string;
-  closeOnClickModal: boolean;
-  noFooter: boolean;
-  noOkBtn: boolean;
-  noCancelBtn: boolean;
-  cancelBtnTxt: string;
-  okBtnTxt: string;
-  okBtnDisabled: boolean;
-  cancelAutoClose: boolean;
-  textAlign: string;
-  onOkBtn: Function;
-  onCancelBtn: Function;
-  onCloseBtn: Function;
-  closeCallback: Function;
-  onClickImageLink: Function;
-  maskBgStyle: string;
-  customClass: string;
-  link: string;
-  imgSrc: string;
-  animation: boolean;
-  lockBgScroll: boolean;
+    id?: string | number;
+    title: string;
+    content: string;
+    type?: string;
+    closeOnClickModal?: boolean;
+    noFooter?: boolean;
+    noOkBtn?: boolean;
+    noCancelBtn?: boolean;
+    cancelBtnTxt?: string;
+    okBtnTxt?: string;
+    okBtnDisabled?: boolean;
+    cancelAutoClose?: boolean;
+    textAlign?: string;
+    onOkBtn?: (
+        this: {
+            close: () => void
+        }
+    ) => void;
+    onCancelBtn?: () => void;
+    onCloseBtn?: () => void;
+    closeCallback?: () => void;
+    onClickImageLink?: () => void;
+    maskBgStyle?: string;
+    customClass?: string;
+    link?: string;
+    imgSrc?: string;
+    animation?: boolean;
+    lockBgScroll?: boolean;
 }
 
 declare module 'vue/types/vue' {
-  interface Vue {
-    $dialog: (option: NutDialogOption) => Vue;
-  }
+    interface Vue {
+        $dialog: (option: NutDialogOption) => Vue;
+    }
 }
--- types/nut-toast.d.ts
+++ types/nut-toast.d.ts
@@ -1,30 +1,34 @@
 import Vue from 'vue';
 
-interface NutToast {
-  text: (msg: string, option: NutToastOption) => Vue;
-  success: (msg: string, option: NutToastOption) => Vue;
-  fail: (msg: string, option: NutToastOption) => Vue;
-  warn: (msg: string, option: NutToastOption) => Vue;
-  loading: (msg: string, option: NutToastOption) => Vue;
+interface NutToast extends Vue {
+    hide: () => void;
+}
+
+interface NutToastInstance {
+    text: (msg: string, option?: NutToastOption) => NutToast;
+    success: (msg: string, option?: NutToastOption) => NutToast;
+    fail: (msg: string, option?: NutToastOption) => NutToast;
+    warn: (msg: string, option?: NutToastOption) => NutToast;
+    loading: (msg: string, option?: NutToastOption) => NutToast;
 }
 
 interface NutToastOption {
-  id: string | number;
-  duration: number;
-  center: boolean;
-  bottom: boolean;
-  textAlignCenter: boolean;
-  bgColor: string;
-  customClass: string;
-  icon: string;
-  size: string;
-  cover: boolean;
-  loadingRotate: boolean;
-  onClose: Function;
+    id?: string | number;
+    duration?: number;
+    center?: boolean;
+    bottom?: boolean;
+    textAlignCenter?: boolean;
+    bgColor?: string;
+    customClass?: string;
+    icon?: string;
+    size?: string;
+    cover?: boolean;
+    loadingRotate?: boolean;
+    onClose?: () => void;
 }
 
 declare module 'vue/types/vue' {
-  interface Vue {
-    $toast: NutToast;
-  }
+    interface Vue {
+        $toast: NutToastInstance;
+    }
 }
