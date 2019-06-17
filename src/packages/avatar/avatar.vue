237abd0a3e54d9af143dd7a4fbf8c6e657bca741
zhenyulei <zhenyulei@jd.com>
2019-06-17 14:46:39 +0800

    add avatar

---
+++ src/packages/avatar/avatar.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="nut-avatar" :style="styles">
+        <i class="icon" :style="iconStyles"></i>
+        <span class="text"><slot></slot></span>
+        <b class="dot" v-if="dot>0">{{dot}}</b>
+    </div>
+</template>
+<script>
+export default {
+    name:'nut-avatar',
+    props: {
+        size:{
+            type:Number,
+            default:40
+        },
+        shape:{
+            type:String,
+            default:''
+        },
+        bgColor:{
+            type:String,
+            default:'#eee'
+        },
+        bgIcon:{
+            type:String,
+            default:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAgCAMAAABNTyq8AAAASFBMVEUAAAAXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEcXIEdRCe4GAAAAF3RSTlMACSW88uTcQKeYWB7YOTB/xXdlkHFLzVTLks0AAAD+SURBVDjLjZILjoMwDAXzD5Q/LZ3733STbpYskoM6EkhBw7MdWV2JfYDQR9XGWArWtJzoOHGxkZOd56T11GdLzlrBz+rD7GEViwHJKRYgFexTrXp6Qi9IAaZ6miAIEqDrSQOC5L6Rxtp3YpbLdbDU0wKdID3A67Oah4cgpe916r78IUSdBRdKkNgV66H1sSJ3NGzW8x832s1clQ6RbqjO4Wjgzs5eJOxu9GVYs1sSrzrVGMXtGf+mHBxYrUS0BTf8Dj4mp2GNn8sYqHskMAGD2sCqGyxs+bXfSXsO8WDuJAO+bK1IXdH83FKkYHJsoIl6l6tf2s5bxVCSfMsJ8QdwfR4F9ZQcyQAAAABJRU5ErkJggg=='
+        },
+        dot:{
+            type:Number,
+            default:0
+        },
+        bgImage:{
+            type:String,
+            default:''
+        }
+    },
+    computed:{
+        styles:function(){
+            return {
+                'width':this.size+'px',
+                'height':this.size+'px',
+                'borderRadius':this.shape ==='square'?'':'50%',
+                'backgroundColor':this.bgColor,
+                'backgroundImage':'url('+this.bgImage+')'
+            }
+        },
+        iconStyles:function(){
+            return {
+                'backgroundImage': 'url('+this.bgIcon+')'
+            }
+        }
+    },
+    data() {
+        return {};
+    },
+    methods: {
+    }
+}
+</script>
---
+++ src/packages/avatar/demo.vue
@@ -0,0 +1,38 @@
+<template>
+      <div class="demo-list">
+      <h4>默认用法</h4>
+      <nut-cell><span slot="title"><nut-avatar></nut-avatar></span></nut-cell>
+      <h4>可以修改背景色</h4>
+      <nut-cell><span slot="title"><nut-avatar bgColor="#87d068"></nut-avatar></span></nut-cell>
+      <h4>可以修改大小</h4>
+      <nut-cell><span slot="title"><nut-avatar :size="30"></nut-avatar></span></nut-cell>
+      <h4>可以修改头像的形状类型</h4>
+      <nut-cell><span slot="title"><nut-avatar shape="square"></nut-avatar></span></nut-cell>
+      <h4>可以修改头像的内容</h4>
+      <nut-cell><span slot="title"><nut-avatar :size="30" bgIcon="">U</nut-avatar></span></nut-cell>
+      <h4>可以增加头像右上角的徽标</h4>
+      <nut-cell><span slot="title"><nut-avatar :dot="13"></nut-avatar></span></nut-cell>
+      <h4>可以修改头像背景图片</h4>
+      <nut-cell><span slot="title"><nut-avatar bgIcon="" bgImage="http://img30.360buyimg.com/uba/jfs/t1/84318/29/2102/10483/5d0704c1Eb767fa74/fc456b03fdd6cbab.png"></nut-avatar></span></nut-cell>
+      <h4>可以修改头像icon图标</h4>
+      <nut-cell><span slot="title"><nut-avatar bgIcon="http://img10.360buyimg.com/uba/jfs/t1/69001/30/2126/550/5d06f947Effd02898/95f18e668670e598.png"></nut-avatar></span></nut-cell>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {};
+  },
+  methods: {
+      
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
