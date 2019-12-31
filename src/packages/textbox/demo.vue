cc1ef7efd3212e701684816ca940ec51e1f07476
guoxiao158 <you@example.com>
2019-12-31 14:53:44 +0800

    fix：修复toast多实例关闭事件冲突 #181

--- src/packages/textbox/demo.vue
+++ src/packages/textbox/demo.vue
@@ -1,31 +1,31 @@
 <template>
     <div>
         <h5>示例</h5>
-        <p>默认用法</p>
+        <h6>默认用法</h6>
         <nut-textbox > </nut-textbox>
-        <p>自定义高度:100px</p>
+        <h6>自定义高度:100px</h6>
         <nut-textbox :txtAreaH="100" :maxNum="300"></nut-textbox>
 
-        <p>自定义提示语</p>
+        <h6>自定义提示语</h6>
         <nut-textbox :placeText="'请填写详细情况请填写详细情况'"></nut-textbox>
         
 
-        <p>自定义字数限制</p>
+        <h6>自定义字数限制</h6>
         <nut-textbox :maxNum="100"></nut-textbox>
         
 
-        <p>限制字数不可超出</p>
+        <h6>限制字数不可超出</h6>
         <nut-textbox :switchMax="true" :maxNum="10" :txtAreaH="100" textBgColor="#efefef"></nut-textbox>
         
 
-        <p>字数超出报错</p>
+        <h6>字数超出报错</h6>
         <nut-textbox :maxNum="10" :txtAreaH="100"  @errorFunc="overLength" ></nut-textbox>
         
 
-        <p>自定义文本框背景色</p>
+        <h6>自定义文本框背景色</h6>
         <nut-textbox :switchMax="true" :maxNum="10" :txtAreaH="100" textBgColor="#feefef"></nut-textbox>
 
-        <p>不显示字数限制</p>
+        <h6>不显示字数限制</h6>
         <nut-textbox :limitShow="false" :maxNum="10" ></nut-textbox>
        
         <h6>输入回调返回文字</h6>
