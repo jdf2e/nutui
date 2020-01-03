c2c63c2f3a5a62f2ee87a99736e085359f3334ce
yangkaixuan <439721838@qq.com>
2020-01-03 19:56:07 +0800

    feat:增加popup组件

--- src/nutui.js
+++ src/nutui.js
@@ -1,88 +1,88 @@
-import { version } from '../package.json';
-import { packages as pkgList } from './config.json';
-import { locale } from './locales';
-import Cell from './packages/cell/index.js';
-import './packages/cell/cell.scss';
-import Dialog from './packages/dialog/index.js';
-import './packages/dialog/dialog.scss';
-import Icon from './packages/icon/index.js';
-import './packages/icon/icon.scss';
-import Toast from './packages/toast/index.js';
-import './packages/toast/toast.scss';
-import ActionSheet from './packages/actionsheet/index.js';
-import './packages/actionsheet/actionsheet.scss';
-import Tab from './packages/tab/index.js';
-import './packages/tab/tab.scss';
-import TabPanel from './packages/tabpanel/index.js';
-import './packages/tabpanel/tabpanel.scss';
-import TabBar from './packages/tabbar/index.js';
-import './packages/tabbar/tabbar.scss';
-import Calendar from './packages/calendar/index.js';
-import './packages/calendar/calendar.scss';
-import DatePicker from './packages/datepicker/index.js';
-import './packages/datepicker/datepicker.scss';
-import NavBar from './packages/navbar/index.js';
-import './packages/navbar/navbar.scss';
-import NoticeBar from './packages/noticebar/index.js';
-import './packages/noticebar/noticebar.scss';
-import Switch from './packages/switch/index.js';
-import './packages/switch/switch.scss';
-import Slider from './packages/slider/index.js';
-import './packages/slider/slider.scss';
-import Range from './packages/range/index.js';
-import './packages/range/range.scss';
-import Picker from './packages/picker/index.js';
-import './packages/picker/picker.scss';
-import Progress from './packages/progress/index.js';
-import './packages/progress/progress.scss';
-import Price from './packages/price/index.js';
-import './packages/price/price.scss';
-import Flex from './packages/flex/index.js';
-import './packages/flex/flex.scss';
-import Col from './packages/col/index.js';
-import './packages/col/col.scss';
-import Row from './packages/row/index.js';
-import './packages/row/row.scss';
-import Steps from './packages/steps/index.js';
-import './packages/steps/steps.scss';
-import Button from './packages/button/index.js';
-import './packages/button/button.scss';
-import Badge from './packages/badge/index.js';
-import './packages/badge/badge.scss';
-import Rate from './packages/rate/index.js';
-import './packages/rate/rate.scss';
-import Swiper from './packages/swiper/index.js';
-import './packages/swiper/swiper.scss';
-import Menu from './packages/menu/index.js';
-import './packages/menu/menu.scss';
-import Stepper from './packages/stepper/index.js';
-import './packages/stepper/stepper.scss';
-import ButtonGroup from './packages/buttongroup/index.js';
-import './packages/buttongroup/buttongroup.scss';
-import SearchBar from './packages/searchbar/index.js';
-import './packages/searchbar/searchbar.scss';
-import ImagePicker from './packages/imagepicker/index.js';
-import './packages/imagepicker/imagepicker.scss';
-import Radio from './packages/radio/index.js';
-import './packages/radio/radio.scss';
-import RadioGroup from './packages/radiogroup/index.js';
-import './packages/radiogroup/radiogroup.scss';
-import CheckBox from './packages/checkbox/index.js';
-import './packages/checkbox/checkbox.scss';
-import CheckBoxGroup from './packages/checkboxgroup/index.js';
-import './packages/checkboxgroup/checkboxgroup.scss';
-import ShortPassword from './packages/shortpassword/index.js';
-import './packages/shortpassword/shortpassword.scss';
-import Skeleton from './packages/skeleton/index.js';
-import './packages/skeleton/skeleton.scss';
-import Scroller from './packages/scroller/index.js';
-import './packages/scroller/scroller.scss';
-import BackTop from './packages/backtop/index.js';
-import './packages/backtop/backtop.scss';
-import CountDown from './packages/countdown/index.js';
-import './packages/countdown/countdown.scss';
-import InfiniteLoading from './packages/infiniteloading/index.js';
-import './packages/infiniteloading/infiniteloading.scss';
+import { version } from "../package.json";
+import { packages as pkgList } from "./config.json";
+import { locale } from "./locales";
+import Cell from "./packages/cell/index.js";
+import "./packages/cell/cell.scss";
+import Dialog from "./packages/dialog/index.js";
+import "./packages/dialog/dialog.scss";
+import Icon from "./packages/icon/index.js";
+import "./packages/icon/icon.scss";
+import Toast from "./packages/toast/index.js";
+import "./packages/toast/toast.scss";
+import ActionSheet from "./packages/actionsheet/index.js";
+import "./packages/actionsheet/actionsheet.scss";
+import Tab from "./packages/tab/index.js";
+import "./packages/tab/tab.scss";
+import TabPanel from "./packages/tabpanel/index.js";
+import "./packages/tabpanel/tabpanel.scss";
+import TabBar from "./packages/tabbar/index.js";
+import "./packages/tabbar/tabbar.scss";
+import Calendar from "./packages/calendar/index.js";
+import "./packages/calendar/calendar.scss";
+import DatePicker from "./packages/datepicker/index.js";
+import "./packages/datepicker/datepicker.scss";
+import NavBar from "./packages/navbar/index.js";
+import "./packages/navbar/navbar.scss";
+import NoticeBar from "./packages/noticebar/index.js";
+import "./packages/noticebar/noticebar.scss";
+import Switch from "./packages/switch/index.js";
+import "./packages/switch/switch.scss";
+import Slider from "./packages/slider/index.js";
+import "./packages/slider/slider.scss";
+import Range from "./packages/range/index.js";
+import "./packages/range/range.scss";
+import Picker from "./packages/picker/index.js";
+import "./packages/picker/picker.scss";
+import Progress from "./packages/progress/index.js";
+import "./packages/progress/progress.scss";
+import Price from "./packages/price/index.js";
+import "./packages/price/price.scss";
+import Flex from "./packages/flex/index.js";
+import "./packages/flex/flex.scss";
+import Col from "./packages/col/index.js";
+import "./packages/col/col.scss";
+import Row from "./packages/row/index.js";
+import "./packages/row/row.scss";
+import Steps from "./packages/steps/index.js";
+import "./packages/steps/steps.scss";
+import Button from "./packages/button/index.js";
+import "./packages/button/button.scss";
+import Badge from "./packages/badge/index.js";
+import "./packages/badge/badge.scss";
+import Rate from "./packages/rate/index.js";
+import "./packages/rate/rate.scss";
+import Swiper from "./packages/swiper/index.js";
+import "./packages/swiper/swiper.scss";
+import Menu from "./packages/menu/index.js";
+import "./packages/menu/menu.scss";
+import Stepper from "./packages/stepper/index.js";
+import "./packages/stepper/stepper.scss";
+import ButtonGroup from "./packages/buttongroup/index.js";
+import "./packages/buttongroup/buttongroup.scss";
+import SearchBar from "./packages/searchbar/index.js";
+import "./packages/searchbar/searchbar.scss";
+import ImagePicker from "./packages/imagepicker/index.js";
+import "./packages/imagepicker/imagepicker.scss";
+import Radio from "./packages/radio/index.js";
+import "./packages/radio/radio.scss";
+import RadioGroup from "./packages/radiogroup/index.js";
+import "./packages/radiogroup/radiogroup.scss";
+import CheckBox from "./packages/checkbox/index.js";
+import "./packages/checkbox/checkbox.scss";
+import CheckBoxGroup from "./packages/checkboxgroup/index.js";
+import "./packages/checkboxgroup/checkboxgroup.scss";
+import ShortPassword from "./packages/shortpassword/index.js";
+import "./packages/shortpassword/shortpassword.scss";
+import Skeleton from "./packages/skeleton/index.js";
+import "./packages/skeleton/skeleton.scss";
+import Scroller from "./packages/scroller/index.js";
+import "./packages/scroller/scroller.scss";
+import BackTop from "./packages/backtop/index.js";
+import "./packages/backtop/backtop.scss";
+import CountDown from "./packages/countdown/index.js";
+import "./packages/countdown/countdown.scss";
+import InfiniteLoading from "./packages/infiniteloading/index.js";
+import "./packages/infiniteloading/infiniteloading.scss";
 import Uploader from "./packages/uploader/index.js";
 import "./packages/uploader/uploader.scss";
 import TextInput from "./packages/textinput/index.js";
@@ -94,7 +94,7 @@ import "./packages/textbox/textbox.scss";
 import TextBox from "./packages/textbox/index.js";
 import Elevator from "./packages/elevator/index.js";
 import "./packages/elevator/elevator.scss";
-
+import Popup from "./packages/popup/index.js";
 const packages = {
   Cell,
   Dialog,
@@ -141,7 +141,8 @@ const packages = {
   TextInput,
   TextBox,
   Avatar,
-  Elevator: Elevator
+  Elevator,
+  Popup
 };
 
 const components = {};
@@ -152,7 +153,7 @@ pkgList.map(item => {
   const pkg = packages[item.name];
   if (!pkg) return;
 
-  if (item.type == 'component') {
+  if (item.type == "component") {
     if (pkg.name) {
       components[pkg.name] = pkg;
     } else {
@@ -160,16 +161,16 @@ pkgList.map(item => {
         components[n] = pkg[n];
       }
     }
-  } else if (item.type == 'method') {
+  } else if (item.type == "method") {
     methods[item.name] = pkg;
-  } else if (item.type == 'filter') {
+  } else if (item.type == "filter") {
     filters[item.name] = pkg;
-  } else if (item.type == 'directive') {
+  } else if (item.type == "directive") {
     directives[item.name] = pkg;
   }
 });
 
-const install = function (Vue, opts = {}) {
+const install = function(Vue, opts = {}) {
   if (install.installed) return;
 
   if (opts.locale) {
@@ -180,10 +181,10 @@ const install = function (Vue, opts = {}) {
 
   for (let cptName in methods) {
     if (Array.isArray(methods[cptName])) {
-      Vue.prototype['$' + cptName.toLowerCase()] = methods[cptName][0];
+      Vue.prototype["$" + cptName.toLowerCase()] = methods[cptName][0];
       Vue.component(methods[cptName][1].name, methods[cptName][1]);
     } else {
-      Vue.prototype['$' + cptName.toLowerCase()] = methods[cptName];
+      Vue.prototype["$" + cptName.toLowerCase()] = methods[cptName];
     }
   }
 
@@ -207,11 +208,12 @@ const install = function (Vue, opts = {}) {
 
   Vue.use(Lazyload, {
     lazyComponent: true,
-    loading: '//img12.360buyimg.com/imagetools/jfs/t1/73967/28/14561/916/5dc142e4E0666555b/bf33454553c6035e.png'
+    loading:
+      "//img12.360buyimg.com/imagetools/jfs/t1/73967/28/14561/916/5dc142e4E0666555b/bf33454553c6035e.png"
   });
 };
 
-if (typeof window !== 'undefined' && window.Vue) {
+if (typeof window !== "undefined" && window.Vue) {
   install(window.Vue);
 }
 
