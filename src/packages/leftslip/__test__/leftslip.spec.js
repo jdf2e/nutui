f967af3196a557ae77d3d67c7304a3a0ce54c8a9
yewenwen <yewenwen1@jd.com>
2020-03-02 09:38:54 +0800

    update leftslip 单元测试问题修复

--- src/packages/leftslip/__test__/leftslip.spec.js
+++ src/packages/leftslip/__test__/leftslip.spec.js
@@ -1,18 +1,16 @@
-import { shallowMount } from '@vue/test-utils'
-import LeftSlip from "../leftslip.vue"
+import {shallowMount} from '@vue/test-utils';
+import LeftSlip from '../leftslip.vue';
 import Vue from 'vue';
 
 describe('LeftSlip.vue', () => {
     const wrapper = shallowMount(LeftSlip, {});
-    it('一键删除设置', () => {
+    it('设置单一删除键', () => {
         wrapper.setProps({
-            onlyDel: true,
+            onlyDelBtn: true
         });
 
         return Vue.nextTick().then(function() {
-            expect(wrapper.find('.delbtn').isVisible()).toBe(true);
-        })
-    })
-
-
+            expect(wrapper.find('.nut-delet-btn').isVisible()).toBe(true);
+        });
+    });
 });
