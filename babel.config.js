085f44db935a56b6fc25f72df06a0ce354a10929
richard1015 <51844712@qq.com>
2020-05-14 23:41:30 +0800

    feat: babel config

--- babel.config.js
+++ babel.config.js
@@ -13,9 +13,9 @@ const presets = [
 
 const plugins = [
 	'@babel/plugin-transform-runtime',
-	'@babel/plugin-transform-object-assign',
-	'@babel/plugin-proposal-object-rest-spread',
 	'@babel/plugin-syntax-dynamic-import',
+	'@babel/plugin-proposal-object-rest-spread',
+	'@babel/plugin-transform-object-assign',
 	'istanbul'
 ];
 
