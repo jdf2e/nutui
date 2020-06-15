65beaeab696dcdcd5d6656362c1b49a5db26d9d8
zhenyulei <zhenyulei@jd.com>
2020-06-15 14:04:46 +0800

    feat: 优化avatar组件

--- src/packages/avatar/demo.vue
+++ src/packages/avatar/demo.vue
@@ -1,37 +1,21 @@
 <template>
   <div class="demo-list">
     <h4>默认用法</h4>
-    <p>内置"small","normal","large"三种尺寸规格</p>
     <div class="white-bg">
-      <nut-avatar size="large"></nut-avatar>
-      <nut-avatar size="normal"></nut-avatar>
-      <nut-avatar size="small"></nut-avatar>
+      <nut-avatar size-num="80" :bg-image="require('../../assets/img/cell-avatar.jpeg')" @active-avatar="activeAvatar"></nut-avatar>
+      <nut-avatar size-num="60" :bg-image="require('../../assets/img/cell-avatar.jpeg')" @active-avatar="activeAvatar"></nut-avatar>
+      <nut-avatar size-num="40" :bg-image="require('../../assets/img/cell-avatar.jpeg')" @active-avatar="activeAvatar"></nut-avatar>
+      <nut-avatar size-num="32" :bg-image="require('../../assets/img/cell-avatar.jpeg')" @active-avatar="activeAvatar"></nut-avatar>
     </div>
-    <h4>修改形状类型</h4>
+
+    <h4>带徽标的头像</h4>
     <div class="white-bg">
-      <nut-avatar size="large" shape="square"></nut-avatar>
-      <nut-avatar size="normal" shape="square"></nut-avatar>
-      <nut-avatar size="small" shape="square"></nut-avatar>
-    </div>
-    <h4>修改背景色</h4>
-    <div class="white-bg">
-      <nut-avatar bg-color="#f0250f"></nut-avatar>
-    </div>
-    <h4>可以修改头像的内容</h4>
-    <div class="white-bg">
-      <nut-avatar size="large" bg-icon>U</nut-avatar>
-    </div>
-    <h4>修改背景图片</h4>
-    <div class="white-bg">
-      <nut-avatar bg-icon bg-image="http://img30.360buyimg.com/uba/jfs/t1/84318/29/2102/10483/5d0704c1Eb767fa74/fc456b03fdd6cbab.png"></nut-avatar>
-    </div>
-    <h4>修改头像ICON图标</h4>
-    <div class="white-bg">
-      <nut-avatar bg-icon="http://img10.360buyimg.com/uba/jfs/t1/69001/30/2126/550/5d06f947Effd02898/95f18e668670e598.png"></nut-avatar>
-    </div>
-    <h4>点击头像有触发事件</h4>
-    <div class="white-bg">
-      <nut-avatar @active-avatar="activeAvatar"></nut-avatar>
+      <nut-avatar size-num="40" :bg-image="require('../../assets/img/cell-avatar.jpeg')" @active-avatar="activeAvatar">
+        <nut-badge :value="9"  :max="99" class="item" top="5px" right="10px" ></nut-badge>
+      </nut-avatar>
+      <nut-avatar size-num="32" :bg-image="require('../../assets/img/cell-avatar.jpeg')" @active-avatar="activeAvatar">
+         <nut-badge :isDot="true" top="5px" right="6px" ></nut-badge>
+      </nut-avatar>
     </div>
   </div>
 </template>
@@ -52,8 +36,15 @@ export default {
 <style lang="scss" scoped>
 .white-bg {
 	padding: 10px;
-	background: #fff;
-	display: flex;
-	align-items: center;
+  background: #fff;
+  display: flex;
+  align-items: flex-end;
+  .nut-avatar{
+    margin-right: 23px;
+  }
+  .nut-badge{
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
