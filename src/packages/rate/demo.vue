c1ea29535ea423bdde2ce4cd6b8b2d8d3de701ef
yushuang24 <yushuang24@jd.com>
2020-07-01 18:40:03 +0800

    fix: 修复字体样式和增加动态监听和修改demo

--- src/packages/rate/demo.vue
+++ src/packages/rate/demo.vue
@@ -23,7 +23,7 @@
     <h4>自定义尺寸</h4>
     <div>
       <nut-cell>
-        <span slot="title"><nut-rate :size="30"></nut-rate></span>
+        <span slot="title"><nut-rate :size="30" :showHalf="false"></nut-rate></span>
       </nut-cell>
     </div>
 
@@ -67,8 +67,10 @@ export default {
       result: '',
       result2: '',
       checkedColor:'#FFC200',
-      icon1: require('./../../assets/img/checked.png'),
-      icon2: require('./../../assets/img/unchecked.png')
+      icon1: `url(${require('./../../assets/img/checked.png')})`,
+      icon2: `url(${require('./../../assets/img/unchecked.png')})`
+      // icon1: `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'%3E%3Cpath fill='rgb(255,0,0)' d='M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM6.5 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm7 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm2.16 3H4.34a6 6 0 0 0 11.32 0z'/%3E%3C/svg%3E")`,
+      // icon2: `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'%3E%3Cpath fill='rgb(255,0,0)' d='M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zM6.5 9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm7 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM7 13h6a1 1 0 0 1 0 2H7a1 1 0 0 1 0-2z'/%3E%3C/svg%3E")`
     };
   },
 
