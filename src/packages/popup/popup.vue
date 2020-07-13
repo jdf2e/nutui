235abdbc7d0ffea1887c488b7d4396204717e91b
杨凯旋 <yangkaixuan@yangkaixuandeMacBook-Pro.local>
2020-07-13 17:29:38 +0800

    fix: 解决滚动失效的问题

--- src/packages/popup/popup.vue
+++ src/packages/popup/popup.vue
@@ -178,7 +178,7 @@ export default {
       const { scrollHeight, offsetHeight, scrollTop } = el ? el : this.$el;
 
       if ((this.deltaY > 0 && scrollTop === 0) || (this.deltaY < 0 && scrollTop + offsetHeight >= scrollHeight)) {
-        //event.preventDefault();
+        event.preventDefault();
       }
     },
     getScroller(el) {
