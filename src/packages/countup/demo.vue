1e6d991e8b0f89e8801886fda372ec589e6d3391
yumingming11 <yumingming11@360buyad.local>
2020-07-16 16:06:34 +0800

    upd: countup 摸拟抽奖

--- src/packages/countup/demo.vue
+++ src/packages/countup/demo.vue
@@ -10,23 +10,53 @@
     </div>
     <h4>数字滚动</h4>
     <div class="show-demo">
-      <nut-countup :scrolling="true" :init-num="17.618" :during="600"> </nut-countup>
+      <!-- <nut-countup :scrolling="true" :init-num='17.618' :during="600">
+        </nut-countup> -->
     </div>
     <h4>自定义滚动图片展示</h4>
     <div class="show-demo">
-      <nut-countup :custom-change-num="customNumber" :custom-bg-img="bgImage" :custom-spac-num="11" :num-width="33" :num-height="47" :during="5000">
+      <!-- <nut-countup
+          :custom-change-num="customNumber"
+          :custom-bg-img="bgImage"
+          :custom-spac-num="11"
+          :num-width="33"
+          :num-height="47"
+          :during="5000"
+        >
+        </nut-countup> -->
+    </div>
+    <h4>抽奖（模拟滚动抽奖）</h4>
+    <div class="show-demo">
+      <nut-countup
+        ref="countup-machine"
+        type="machine"
+        :machine-num="machineNum"
+        :machine-prize-num="5"
+        :machine-prize-level="prizeLevel"
+        :custom-bg-img="bgImage2"
+        :num-width="100"
+        :num-height="100"
+        :during="3000"
+        @scroll-end="scrollAniEnd"
+      >
       </nut-countup>
+      <nut-button @click="startRole" :disabled="startFlag">抽奖</nut-button>
+      <!-- <nut-button @click="startRole2" :disabled="startFlag2">抽奖(不中奖)</nut-button> -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  components: {},
   data() {
     return {
       customNumber: 618,
-      bgImage: 'https://img10.360buyimg.com/imagetools/jfs/t1/133024/3/2251/2646/5ee7549aE8dc02d7e/de6901b6c72db396.png'
+      bgImage: 'https://img10.360buyimg.com/imagetools/jfs/t1/133024/3/2251/2646/5ee7549aE8dc02d7e/de6901b6c72db396.png',
+      startFlag: false,
+      startFlag2: false,
+      machineNum: 3,
+      bgImage2: 'https://img10.360buyimg.com/imagetools/jfs/t1/121466/20/6784/28830/5f06e7f2Edbb8998c/9bdd9e7b24dff9fe.png',
+      prizeLevel: 0
     };
   },
   methods: {
@@ -35,10 +65,26 @@ export default {
       timer = setInterval(() => {
         this.customNumber = Math.floor(Math.random() * (700 - 100 + 1) + 100);
       }, 5000);
+    },
+    startRole() {
+      this.prizeLevel = Math.floor(Math.random() * 5 + 1);
+      this.startFlag = true;
+      this.$refs['countup-machine'].machineLuck();
+    },
+    startRole2() {
+      this.prizeLevel = 9999;
+      this.startFlag2 = true;
+      this.$refs['countup-machine'].machineLuck();
+    },
+    scrollAniEnd() {
+      this.$toast.text('恭喜中奖！！！');
+      setTimeout(() => {
+        this.startFlag = false;
+      }, 300);
     }
   },
   mounted() {
-    this.run();
+    // this.run();
   }
 };
 </script>
@@ -46,5 +92,10 @@ export default {
 <style lang="scss" scoped>
 .show-demo {
   background: #ffffff;
+}
+.disabledClick {
+  pointer-events: none !important;
+  cursor: not-allowed !important;
+  opacity: 0.6 !important;
 }
 </style>
--- src/packages/countup/doc.md
+++ src/packages/countup/doc.md
@@ -51,6 +51,42 @@ export default {
 };
 ```
 
+## 抽奖
+
+```html
+<nut-countup
+    type="machine"
+    :custom-change-num="customNumber"
+    :custom-bg-img="bgImage"
+    :custom-spac-num="11"
+    :num-width="33"
+    :num-height="47"
+    :during="5000"
+>
+</nut-countup>
+```
+```javascript
+export default {
+    data() {
+        return {
+            customNumber: 618, 
+            bgImage: 'https://img10.360buyimg.com/imagetools/jfs/t1/133024/3/2251/2646/5ee7549aE8dc02d7e/de6901b6c72db396.png'
+        };
+    },
+    methods: {
+        run() {
+            let timer = null;
+            timer = setInterval(() => {
+                this.customNumber = Math.floor(Math.random() * (700 - 100 + 1) + 100);
+            }, 5000);
+        }
+    },
+    mounted() {
+        this.run();
+    }
+};
+```
+
 
 ## Prop
 
@@ -63,14 +99,19 @@ export default {
 | start-flag | 触发数字滚动的标识 | Boolean | true
 | during | 滚动一次运行时间 | Number | 1000
 | num-width | 数字宽度，常用于自定义无缝滚动 | Number | 20
-| num-height | 数字高度，常用于自定义无缝滚动 | Number | 20
+| num-height | 数字高度，常用于自定义无缝滚动（抽奖功能必传） | Number | 20
 | scrolling | 用于数字滚动展示 | Boolean | false
 | custom | 用于自定义图片数字滚动 | Boolean | false
 | custom-change-num | 要变化的数字（用于自定义图片,initNum\endNum在此无效） | Number | 1
-| custom-bg-img | 自定义图片（建议使用雪碧图实现） | - | -
+| custom-bg-img | 自定义图片（建议使用雪碧图实现，使用抽奖功能必须使用雪碧图） | - | -
 | custom-spac-num | 图片中数字之间可能会存在间距 | Number | 0
+| type | 使用抽奖功能必穿，“machine” | string | ''
+| machine-prize-num | 奖项数，一共多少个奖品，必传 | number | 4
+| machine-num | 抽奖位数，即几个图标，必传 | number | 3
+| machine-prize-level | 中奖图标,图标在雪碧图中的位置 | number | 0
+| machine-trun-more | 后面图标的滚动比前面多滚动次数 | number | 0
 
-
+###### 说明：抽奖功能是根据图标的定位实现，需要传入雪碧图中单个图标的高度 num-height，
 
 
 
