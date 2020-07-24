0dc0d912f57dd8a8efc8e9e43e25635555e5b8f2
richard1015 <51844712@qq.com>
2020-07-24 17:47:19 +0800

    upd: jdl 1.0.0-beta.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
-  extends: ['plugin:vue/essential'],
-  rules: {},
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
   parserOptions: {
     parser: 'babel-eslint',
-    ecmaVersion: 2017
-  }
+  },
 };
