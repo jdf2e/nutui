1be789057811378af9455109fb232ddc4ca37427
luobinbin <luobinbin.china@gmail.com>
2020-07-29 00:07:59 +0800

    style: 优化样式

--- src/packages/pullrefreshloadmore/demo.vue
+++ src/packages/pullrefreshloadmore/demo.vue
@@ -25,7 +25,7 @@ export default {
 
   mounted() {
     for (let i = 1; i <= 20; i++) {
-      this.items.push(i + ' - keep walking, be 2 with you.');
+      this.items.push(i + ' - 塑像本来就在石头里，我只是把不要的部分去掉');
     }
     this.top = 1;
     this.bottom = 20;
@@ -39,7 +39,7 @@ export default {
       setTimeout(() => {
         let start = this.top - 1;
         for (let i = start; i > start - 10; i--) {
-          this.items.splice(0, 0, i + ' - keep walking, be 2 with you.');
+          this.items.splice(0, 0, i + ' - 塑像本来就在石头里，我只是把不要的部分去掉');
         }
         this.top = this.top - 10;
 
@@ -53,7 +53,7 @@ export default {
       setTimeout(() => {
         let start = this.bottom + 1;
         for (let i = start; i < start + 10; i++) {
-          this.items.push(i + ' - keep walking, be 2 with you.');
+          this.items.push(i + ' - 塑像本来就在石头里，我只是把不要的部分去掉');
         }
         this.bottom = this.bottom + 10;
 
