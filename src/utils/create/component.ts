fa03568949f1d00997649c1320e429852e32cb46
richard1015 <51844712@qq.com>
2020-10-29 18:02:39 +0800

    fix: create component code

--- src/utils/create/component.ts
+++ src/utils/create/component.ts
@@ -1,21 +1,20 @@
 import { App, defineComponent, ComponentOptions } from 'vue';
 export function createComponent(name: string) {
-  return function(_component: ComponentOptions) {
-    _component.baseName = name;
-    _component.name = 'nut-' + name;
-    _component.install = (vue: App) => {
-      vue.component(_component.name as string, _component);
-    };
-    return defineComponent(_component);
-  } as typeof defineComponent;
-}
-export function createDemoComponent(name: string) {
-  return function(_component: ComponentOptions) {
-    _component.baseName = name;
-    _component.name = 'demo-' + name;
-    _component.install = (vue: App) => {
-      vue.component(_component.name as string, _component);
-    };
-    return defineComponent(_component);
-  } as typeof defineComponent;
+  const componentName = 'nut-' + name;
+  return {
+    componentName,
+    create: function(_component: ComponentOptions) {
+      _component.baseName = name;
+      _component.name = componentName;
+      _component.install = (vue: App) => {
+        vue.component(_component.name as string, _component);
+      };
+      return defineComponent(_component);
+    } as typeof defineComponent,
+    createDemo: function(_component: ComponentOptions) {
+      _component.baseName = name;
+      _component.name = 'demo-' + name;
+      return defineComponent(_component);
+    } as typeof defineComponent
+  };
 }
