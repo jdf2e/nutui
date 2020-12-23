ac7c1b52c4d8efcc323a8d0cc92b13e1c3042b0e
yumingming11 <being0008@163.com>
2020-12-23 14:50:01 +0800

    feat: radio 开发，配合使用radio-group

---
+++ src/packages/radio/doc.md
@@ -0,0 +1,124 @@
+# Radio 单选按钮
+
+## 基本用法
+
+通过 **v-model** 绑定值当前选中项的 **label** ，二者一致时 **Radio** 选中。
+```html
+<nut-radio 
+  v-model="radioVal"
+  :label="b"
+>备选项1
+</nut-radio>
+```
+```javascript
+export default {
+  data() {
+    return {
+      radioVal:"a",
+    }
+  }
+};
+```
+
+## 组合使用
+
+```html
+<nut-radio 
+  v-model="radioVal1"
+  :label="1"
+>备选项1
+</nut-radio>
+
+<nut-radio 
+  v-model="radioVal1"
+  :label="2"
+>备选项2
+</nut-radio>
+```
+
+
+## 禁用状态
+
+通过给 **disabled** 传布尔值 **false** ，可将组件禁用
+
+```html
+<nut-radio 
+  :disabled="true" 
+  v-model="radioVal2" 
+  label="禁用"
+>备选项1
+</nut-radio>
+```
+
+
+## 自定义尺寸
+
+内置 **small**，**base**，**large** 三种规格供使用。
+
+```html
+<nut-radio 
+  size="small"
+>小号
+</nut-radio>
+
+<nut-radio 
+  size="base"
+>默认
+</nut-radio>
+
+<nut-radio 
+  size="large"
+>大号
+</nut-radio>
+```
+
+## 禁用动效
+
+通过给 **animated** 传布尔值 **false** ，禁用自带动效
+
+```html
+<nut-radio 
+   :animated="false" 
+   v-model="radioVal3" 
+   label="a"
+>备选项1
+</nut-radio>
+
+<nut-radio 
+   :animated="false" 
+   v-model="radioVal3" 
+   label="b"
+>备选项2</nut-radio>
+```
+
+禁用 **RadioGroup** 动效
+
+```html
+<nut-radiogroup v-model="radioGroupVal4" :animated="false">
+  <nut-radio label="a">备选项1</nut-radio>
+  <nut-radio label="b">备选项2</nut-radio>
+  <nut-radio label="c">备选项3</nut-radio>
+</nut-radiogroup>
+```
+
+
+## 新增自定义class
+```html
+<nut-radio 
+   class="my-radio"
+>备选项</nut-radio>
+```
+**RadioGroup** 也支持新增自定义class
+
+## Prop
+
+### Radio
+
+| 字段 | 说明 | 类型 | 默认值
+|----- | ----- | ----- | ----- 
+| v-model | 当前选中项的标识符，与label值一致时呈选中状态 | 任意类型 | -
+| label | 标识符，与v-model值一致时呈选中状态 | 任意类型 | -
+| size | 尺寸，可选值small/base/large | String | base
+| disabled | 是否禁用 | Boolean | false
+| animated | 是否需要动效 | Boolean | true
+
---
+++ src/packages/radio/index.vue
@@ -0,0 +1,127 @@
+<template>
+  <label :class="['nut-radio', 'nut-radio-size-' + currentSize]">
+    <input
+      type="radio"
+      :value="currentValue"
+      :class="{ 'nut-radio-ani': isAnimated }"
+      :checked="currentValue === label"
+      :disabled="isDisabled"
+      :label="label"
+      @click="clickEvt"
+    />
+    <span class="nut-radio-label">
+      <slot></slot>
+    </span>
+  </label>
+</template>
+<script lang="ts">
+import {
+  compile,
+  computed,
+  reactive,
+  ref,
+  toRefs,
+  getCurrentInstance,
+  inject
+} from 'vue';
+import { createComponent } from '@/utils/create';
+const { componentName, create } = createComponent('radio');
+
+type Iparent = {
+  parentNode: boolean;
+  changeVal: Function;
+};
+export default create({
+  props: {
+    modelValue: {
+      type: [String, Number, Boolean],
+      default: false
+    },
+    label: [String, Number, Boolean],
+    size: {
+      type: String,
+      default: 'base'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    animated: {
+      type: Boolean,
+      default: true
+    }
+  },
+  components: {},
+  emits: ['input', 'update:modelValue'],
+  setup(props, { emit }) {
+    const parentGroup = <Iparent>inject('radiogroup', {
+      parentNode: false
+    });
+    const internalInstance = getCurrentInstance()?.parent;
+    const parentProps = internalInstance?.props;
+    // const parentEl = internalInstance?.type;
+
+    const currentValue = computed({
+      get: () => {
+        if (parentGroup && parentGroup.parentNode) {
+          return parentProps?.modelValue;
+        } else {
+          return props.modelValue;
+        }
+      },
+      set: val => {
+        if (parentGroup && parentGroup.parentNode) {
+          parentGroup?.changeVal(<string | number | boolean>val);
+        } else {
+          emit('input', val);
+        }
+      }
+    });
+
+    const currentSize = computed(() => {
+      if (parentGroup && parentGroup.parentNode) {
+        return parentProps?.size;
+      } else {
+        return props.size;
+      }
+    });
+
+    const isDisabled = computed(() => {
+      if (parentGroup && parentGroup.parentNode) {
+        return parentProps?.disabled;
+      } else {
+        return props.disabled;
+      }
+    });
+
+    const isAnimated = computed(() => {
+      if (parentGroup && parentGroup.parentNode) {
+        return parentProps?.animated;
+      } else {
+        return props.animated;
+      }
+    });
+
+    const clickEvt = (event: any) => {
+      event?.stopPropagation();
+      if (isDisabled.value) {
+        return false;
+      }
+      currentValue.value = props.label ?? '';
+      emit('update:modelValue', props.label);
+    };
+
+    return {
+      currentValue,
+      currentSize,
+      isDisabled,
+      isAnimated,
+      clickEvt
+    };
+  }
+});
+</script>
+
+<style lang="scss">
+@import 'index.scss';
+</style>
