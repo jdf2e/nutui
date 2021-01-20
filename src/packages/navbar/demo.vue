41e1a5d3018767439daf13021f36d290f88169fc
“liqiong-lab” <1450805902@qq.com>
2021-01-20 10:55:41 +0800

    fix: navbar

--- src/packages/navbar/demo.vue
+++ src/packages/navbar/demo.vue
@@ -1,27 +1,142 @@
 <template>
   <div class="demo">
     <h2>基础用法</h2>
-    <nut-navbar leftShow title="订单详情" icon="share"></nut-navbar>
-    <nut-navbar leftShow title="浏览记录" desc="清空"></nut-navbar>
-    <!-- 是否支持自定义属性titIcon -->
-    <nut-navbar :leftShow="false" title="购物车" titIcon="locationg3" desc="编辑" icon="more"></nut-navbar>
+    <nut-navbar @on-click-back="back" @on-click-title="title" @on-click-send="send" title="订单详情" icon="share"></nut-navbar>
+    <nut-navbar @on-click-back="back" @on-click-title="title" @on-click-clear="clear" title="浏览记录" desc="清空"></nut-navbar>
+    <nut-navbar
+      :leftShow="false"
+      @on-click-title="title"
+      @on-click-icon="icon"
+      @on-click-clear="edit"
+      @on-click-send="more"
+      title="购物车"
+      titIcon="locationg3"
+      desc="编辑"
+      icon="nut-icon-more1"
+    ></nut-navbar>
+
     <h2>增加tab及右侧按钮</h2>
-    <nut-navbar title="商品" desc="编辑" icon="horizontal"></nut-navbar>
+    <nut-navbar
+      tab
+      :tabs="[
+        {
+          id: '11',
+          name: '商品'
+        },
+        {
+          id: '22',
+          name: '店铺'
+        }
+      ]"
+      @switch-tab="switchTab"
+      @on-click-back="back"
+      @on-click-title="title"
+      @on-click-clear="edit"
+      @on-click-send="list"
+      desc="编辑"
+      icon="horizontal"
+    >
+    </nut-navbar>
+
     <h2>多tab切换导航</h2>
-    <nut-navbar title="商品" icon="more"></nut-navbar>
+    <nut-navbar :tabs="tabList" @switch-tab="switchTab" @on-click-back="back" icon="share" @on-click-send="send">
+      <template #icons>
+        <nut-icon class="icon" name="nut-icon-more1" @on-click-slot-send="morelist"></nut-icon>
+      </template>
+    </nut-navbar>
   </div>
 </template>
 
 <script lang="ts">
+import { reactive } from 'vue';
 import { createComponent } from '@/utils/create';
 const { createDemo } = createComponent('navbar');
 export default createDemo({
-  props: {},
-  setup() {
-    const testClick = (event: Event) => {
-      alert(event);
+  setup(props, { emit }) {
+    const tabList = [
+      {
+        id: '11',
+        name: '商品'
+      },
+      {
+        id: '22',
+        name: '评价'
+      },
+      {
+        id: '33',
+        name: '详情'
+      },
+      {
+        id: '44',
+        name: '推荐'
+      },
+      {
+        id: '55',
+        name: '商品'
+      },
+      {
+        id: '66',
+        name: '评价'
+      },
+      {
+        id: '77',
+        name: '详情'
+      },
+      {
+        id: '88',
+        name: '推荐'
+      }
+    ];
+    const back = () => {
+      alert('header头部， 点击返回');
     };
-    return { testClick };
+    const title = () => {
+      alert('header头部， 点击title');
+    };
+    const right = () => {
+      alert('header头部， 点击右侧按钮');
+    };
+    const icon = () => {
+      alert('icon');
+    };
+    const send = () => {
+      alert('发送');
+    };
+    const edit = () => {
+      alert('编辑');
+    };
+    const more = () => {
+      alert('更多');
+    };
+    const clear = () => {
+      alert('清空');
+    };
+    const list = () => {
+      alert('列表');
+    };
+    const morelist = () => {
+      alert('多个更多');
+    };
+    function switchTab(id: number, name: string) {
+      console.log(id, name);
+    }
+
+    return {
+      back,
+      title,
+      right,
+      send,
+      edit,
+      clear,
+      more,
+      list,
+      icon,
+      morelist,
+      switchTab,
+      ...reactive({
+        tabList
+      })
+    };
   }
 });
 </script>
