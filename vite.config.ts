039b2780b6a8cac8168c9b7a46e65e794980dcf7
richard1015 <51844712@qq.com>
2021-03-11 09:11:38 +0800

    chore: vite config del plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Markdown from 'vite-plugin-md';
 import path from 'path';
-import legacy from '@vitejs/plugin-legacy';
 const resolve = path.resolve;
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,10 +30,7 @@ export default defineConfig({
     vue({
       include: [/\.vue$/, /\.md$/]
     }),
-    Markdown(),
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })
+    Markdown()
   ],
   build: {
     cssCodeSplit: false,
