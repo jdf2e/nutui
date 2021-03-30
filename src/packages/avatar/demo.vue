4a8f901ee14f2d22d3a0fc33af46afeaeaf68eef
richard1015 <51844712@qq.com>
2021-03-30 18:03:00 +0800

    fix: com props

--- src/packages/avatar/demo.vue
+++ src/packages/avatar/demo.vue
@@ -6,19 +6,23 @@
       <nut-avatar
         size="80"
         bg-image="https://img12.360buyimg.com/imagetools/jfs/t1/143702/31/16654/116794/5fc6f541Edebf8a57/4138097748889987.png"
-      ></nut-avatar>
+      >
+      </nut-avatar>
       <nut-avatar
         size="large"
         bg-image="https://img12.360buyimg.com/imagetools/jfs/t1/143702/31/16654/116794/5fc6f541Edebf8a57/4138097748889987.png"
-      ></nut-avatar>
+      >
+      </nut-avatar>
       <nut-avatar
         size="normal"
         bg-image="https://img12.360buyimg.com/imagetools/jfs/t1/143702/31/16654/116794/5fc6f541Edebf8a57/4138097748889987.png"
-      ></nut-avatar>
+      >
+      </nut-avatar>
       <nut-avatar
         size="small"
         bg-image="https://img12.360buyimg.com/imagetools/jfs/t1/143702/31/16654/116794/5fc6f541Edebf8a57/4138097748889987.png"
-      ></nut-avatar>
+      >
+      </nut-avatar>
     </nut-cell>
     <h2>修改形状</h2>
     <!-- <p>内置"small","normal","large"三种尺寸规格</p> -->
@@ -34,7 +38,8 @@
     <nut-cell>
       <nut-avatar
         bg-icon="https://img30.360buyimg.com/uba/jfs/t1/84318/29/2102/10483/5d0704c1Eb767fa74/fc456b03fdd6cbab.png"
-      ></nut-avatar>
+      >
+      </nut-avatar>
     </nut-cell>
     <h2>可以修改头像的内容</h2>
     <nut-cell>
