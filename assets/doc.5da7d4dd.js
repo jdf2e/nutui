d89ccecdb9024477a48c31decfbe6830463d5202
richard1015 <richard1015@users.noreply.github.com>
2021-04-13 14:37:33 +0000

    deploy: 4e2314a294b06ba0b1e15c1d8f221e28f8db89cc

--- assets/doc.5da7d4dd.js
+++
@@ -1,1 +0,0 @@
-import{c as o,o as s}from"./vendor.36ea9f7d.js";const e={class:"markdown-body"},a={expose:[],setup:a=>(a,d)=>(s(),o("div",e))};export default a;
---
+++ assets/doc.71350fed.js
@@ -0,0 +1,1 @@
+import{c as s,o,f as e}from"./index.bd4b4460.js";const a={class:"markdown-body"},d=e("h1",null,"CollapseItem 折叠面板",-1),l={expose:[],setup:e=>(e,l)=>(o(),s("div",a,[d]))};export default l;
