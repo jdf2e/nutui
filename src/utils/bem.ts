7e29c81668e4b0e1cf5e5197cc09bf19078add02
wangbei16 <suanbanren@foxmail.com>
2021-05-31 18:14:02 +0800

    chore: 修改class前缀为nut

--- src/utils/bem.ts
+++ src/utils/bem.ts
@@ -1,17 +1,17 @@
 import { withNaming } from '@bem-react/classname'
 
-const cn = withNaming({ n: 'nutui-', e: '__', m: '--', v: '-' })
+const cn = withNaming({ n: 'nut-', e: '__', m: '--', v: '-' })
 
 const b = cn('icon', 'Element')
 //
-// console.log(b()) // nutui-icon__Element
-// console.log(b('Element')) // nutui-icon__Element
-// console.log(b({ Modifier: 'value' })) // nutui-icon__Element nutui-icon__Element--Modifier-value
-// console.log(b({ disable: true })) // nutui-icon__Element nutui-icon__Element--disable
-// console.log(b({ disable: false })) // nutui-icon__Element
+// console.log(b()) // nut-icon__Element
+// console.log(b('Element')) // nut-icon__Element
+// console.log(b({ Modifier: 'value' })) // nut-icon__Element nut-icon__Element--Modifier-value
+// console.log(b({ disable: true })) // nut-icon__Element nut-icon__Element--disable
+// console.log(b({ disable: false })) // nut-icon__Element
 //
-// console.log(b('Element', ['mixin'])) // nutui-icon__Element mixin
-// console.log(b({ disable: false }, ['mixin'])) // nutui-icon__Element mixin
-// console.log(b({ disable: true }, ['mixin'])) // nutui-icon__Element nutui-icon__Element--disable mixin
+// console.log(b('Element', ['mixin'])) // nut-icon__Element mixin
+// console.log(b({ disable: false }, ['mixin'])) // nut-icon__Element mixin
+// console.log(b({ disable: true }, ['mixin'])) // nut-icon__Element nut-icon__Element--disable mixin
 
 export default cn
