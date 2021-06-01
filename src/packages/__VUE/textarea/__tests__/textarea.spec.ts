4e9efe3000cbbd5985de4be4014eb39f353e81be
郭晓 <414247624@qq.com>
2021-06-01 10:28:09 +0800

    test: input function (#478)
    Co-authored-by: guoxiaoxiao8 <guoxiao158@126.com>

---
+++ src/packages/__VUE/textarea/__tests__/textarea.spec.ts
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils';
+
+import Textarea from '../index.vue';
+
+test('Textarea base', () => {
+  const wrapper = mount(Textarea);
+  // const input = wrapper.find('input');
+  // expect(input.exists()).toBe(true);
+  // expect(input.element.value).toBe("3");
+});
