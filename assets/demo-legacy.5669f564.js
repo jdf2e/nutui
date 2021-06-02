4300a86fbf8fc7504f9c5fa0264e74d27dcf408a
richard1015 <richard1015@users.noreply.github.com>
2021-06-02 09:04:20 +0000

    deploy: 03c8b7f6b35205f4907721614a57a31dc40c1423

--- assets/demo-legacy.5669f564.js
+++
@@ -1,1 +0,0 @@
-System.register(["./vendor-legacy.2cbae777.js"],(function(e){"use strict";var t,n;return{setters:[function(e){t=e.o,n=e.c}],execute:function(){const r=e("default",{}),c={class:"demo-list"};r.render=function(e,r){return t(),n("div",c)}}}}));
--- assets/demo.df29c63a.js
+++
@@ -1,1 +0,0 @@
-import{o,c as r}from"./vendor.96954379.js";const e={},s={class:"demo-list"};e.render=function(e,t){return o(),r("div",s)};export default e;
