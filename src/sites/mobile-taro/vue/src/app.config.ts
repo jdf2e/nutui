d3bb85545ea5245bf7869c93fbedfc12e6012555
Drjingfubo <50685476+Drjingfubo@users.noreply.github.com>
2021-06-21 14:53:54 +0800

    fix: checkbox间距调整，官网首页赋能案例样式修改 (#511)
    * fix: checkbox间距调整，官网案例样式调整
    
    * fix: checkbox样式调整
    
    * fix: taro drag
    
    * fix: taro drag doc

--- src/sites/mobile-taro/vue/src/app.config.ts
+++ src/sites/mobile-taro/vue/src/app.config.ts
@@ -1,5 +1,6 @@
 export default {
   pages: [
+    'pages/drag/index',
     'pages/toast/index',
     'pages/notify/index',
     'pages/dialog/index',
---
+++ src/sites/mobile-taro/vue/src/pages/drag/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="demo full">
+    <h2>基础用法</h2>
+    <nut-drag
+      style="width: 90%; height: 200px; border: 1px solid red"
+      direction="all"
+      width="120"
+      height="40"
+    >
+      <nut-button style="width: 120px; height: 40px" type="primary"
+        >触摸移动</nut-button
+      >
+    </nut-drag>
+    <h2 style="margin: 60px 0 0 0">限制拖拽方向</h2>
+    <nut-drag
+      style="width: 100%; height: 200px; position: absolute"
+      direction="x"
+      width="120"
+      height="40"
+    >
+      <nut-button style="width: 120px; height: 40px" type="primary"
+        >只能X轴拖拽</nut-button
+      >
+    </nut-drag>
+    <nut-drag
+      style="
+        width: 100%;
+        height: 200px;
+        position: absolute;
+        margin: 0 0 0 200px;
+      "
+      direction="y"
+      width="120"
+      height="40"
+    >
+      <nut-button style="width: 120px; height: 40px" type="primary"
+        >只能y轴拖拽</nut-button
+      >
+    </nut-drag>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {}
+};
+</script>
+
+<style lang="scss"></style>
