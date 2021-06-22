4fe5fed21caacda38b73a0a490ba11ca2fb8964b
suzigang <1039168735@qq.com>
2021-06-22 17:40:38 +0800

    docs: taro md

--- src/sites/mobile-taro/vue/src/app.ts
+++ src/sites/mobile-taro/vue/src/app.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import './app.scss';
 import NutUI from '../../../../packages/nutui.taro.vue';
+// import NutUI from '@nutui/nutui';
+// import '@nutui/nutui/dist/style.css';
 const App = createApp({
   onShow(options) {}
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
