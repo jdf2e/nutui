cab762776acefb50afdbc644c681891d12c66891
suzigang <1039168735@qq.com>
2021-06-28 13:37:12 +0800

    style: steps demo

--- src/sites/mobile-taro/vue/src/pages/index/index.vue
+++ src/sites/mobile-taro/vue/src/pages/index/index.vue
@@ -36,6 +36,16 @@ import { reactive, toRefs } from 'vue';
 import { nav } from '../../../../../../config.json';
 export default {
   name: 'NutUI',
+  // onShareAppMessage: function (res) {
+  //   if (res.from === 'button') {
+  //     // 来自页面内转发按钮
+  //     console.log(res.target)
+  //   }
+  //   return {
+  //     title: '自定义转发标题',
+  //     path: '/page/user?id=123'
+  //   }
+  // },
   setup() {
     const state = reactive({
       nav
