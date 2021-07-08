185a3bbae960245d2135b75f58a31fa8603180a4
richard1015 <richard1015@users.noreply.github.com>
2021-07-08 02:37:25 +0000

    deploy: 6b0c16bd5ca027394cecd0489f22782868b57c16

--- assets/doc-legacy.0d2e3a9a.js
+++
@@ -1,1 +0,0 @@
-System.register(["./vendor-legacy.466e82dd.js"],(function(e){"use strict";var t,s,n;return{setters:[function(e){t=e.c,s=e.o,n=e.f}],execute:function(){const c={class:"markdown-body"},o=n("h1",null,"CollapseItem 折叠面板",-1);e("default",{setup:e=>(e,n)=>(s(),t("div",c,[o]))})}}}));
--- assets/doc.a02e89d0.js
+++
@@ -1,1 +0,0 @@
-import{c as o,o as s,f as a}from"./vendor.b05df578.js";const d={class:"markdown-body"},e=a("h1",null,"CollapseItem 折叠面板",-1),l={setup:a=>(a,l)=>(s(),o("div",d,[e]))};export default l;
