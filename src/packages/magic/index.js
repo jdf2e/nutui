65c8443e44cdaae2c703c0d5ada8134466f16de1
richard1015 <51844712@qq.com>
2021-07-15 22:31:34 +0800

    fix(magic): load on demand bug

--- src/packages/magic/index.js
+++ src/packages/magic/index.js
@@ -1,8 +1,12 @@
 import Magic from './magic.vue';
+import Cube from './Cube/index.vue';
+import CubeItem from './Cube/CubeItem.vue';
 import './magic.scss';
 
 Magic.install = function(Vue) {
   Vue.component(Magic.name, Magic);
+  Vue.component('Cube', Cube);
+  Vue.component('CubeItem', CubeItem);
 };
 
 export default Magic;
