b0bf3d78d05db0c2005938d62124cff1f473291f
richard1015 <richard1015@users.noreply.github.com>
2021-07-31 07:52:50 +0000

    deploy: 1f92d1d3a5728c6f552e544622e8cd33a256013c

--- 3.1.1/doc.126d1ec9.js
+++ 3.1.1/doc.126d1ec9.js
@@ -1,1 +1,1 @@
-import{c as o,o as s}from"./vendor.7295d1e3.js";const d={class:"markdown-body"},a={setup:a=>(a,e)=>(s(),o("div",d))};export default a;
+import{c as o,o as s}from"./vendor.7295d1e3.js";const a={class:"markdown-body"},d={setup:d=>(d,e)=>(s(),o("div",a))};export{d as default};
