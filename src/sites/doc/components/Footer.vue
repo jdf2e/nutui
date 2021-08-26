ce78367727f9361f0bc1bb6709c28d54c1418051
richard1015 <51844712@qq.com>
2021-08-26 10:07:25 +0800

    docs: nutui 微信群二维码链接替换

--- src/sites/doc/components/Footer.vue
+++ src/sites/doc/components/Footer.vue
@@ -74,7 +74,7 @@
           <div class="vx-box">
             <p class="vx-desc">微信交流群</p>
             <p class="vx-desc">扫码添加好友</p>
-            <img class="img-code" src="../../assets/images/vx-code.png" />
+            <img class="img-code" src="../../assets/images/vx-code1.png" />
             <p class="vx-desc">回复<span class="vx-red">NutUI</span>即刻进群</p>
           </div>
         </div>
