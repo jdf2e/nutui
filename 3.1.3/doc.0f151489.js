c54ff2741ab3debdd36971d5e6cfc839c663f74f
richard1015 <richard1015@users.noreply.github.com>
2021-08-30 09:05:24 +0000

    deploy: b570d6d5cd9a0289b72194dc9e31b2815852321c

--- 3.1.3/doc.0f151489.js
+++
@@ -1,1 +0,0 @@
-import{e as o,o as s}from"./vendor.21cda21c.js";const a={class:"markdown-body"},d={setup:d=>(d,e)=>(s(),o("div",a))};export{d as default};
--- 3.1.3/doc.taro.4d22d7d5.js
+++
@@ -1,1 +0,0 @@
-import{e as o,o as s,g as a,l as r}from"./vendor.21cda21c.js";const e={class:"markdown-body"},l=[a("h1",null,"Picker 选择器",-1),a("h3",null,"介绍",-1),a("p",null,"提供多个选项集合供用户选择其中一项。",-1),a("h4",null,[r("直接使用 Taro 现有 Picker 组件开发 "),a("a",{href:"https://docs.taro.zone/docs/components/forms/picker"},"参考文档")],-1)],c={setup:a=>(a,r)=>(s(),o("div",e,l))};export{c as default};
