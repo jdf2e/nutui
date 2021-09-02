8b494078ef8b6f1e142deb011b32b23272827aeb
guoxiaoxiao8 <guoxiao158@126.com>
2021-09-02 18:10:53 +0800

    feat: 添加签名组件

---
+++ src/packages/__VUE/signature/demo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="demo">
+    <h2>基础用法</h2>
+    <nut-cell>
+      <nut-signature></nut-signature>
+    </nut-cell>
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '../../utils/create';
+const { createDemo } = createComponent('signature');
+export default createDemo({
+  props: {},
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.demo {
+}
+</style>
