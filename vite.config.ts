16f366b6eb29036c591875948a14aef1c92d1fa4
richard1015 <51844712@qq.com>
2021-09-16 10:36:00 +0800

    chore: add postcss plugin autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,19 @@ export default defineConfig({
         // dont need include file extend .scss
         additionalData: `@import "@/packages/styles/variables.scss";@import "@/sites/assets/styles/variables.scss";`
       }
+    },
+    postcss: {
+      plugins: [
+        require('autoprefixer')({
+          overrideBrowserslist: [
+            '> 0.5%',
+            'last 2 versions',
+            'ie > 11',
+            'iOS >= 10',
+            'Android >= 5'
+          ]
+        })
+      ]
     }
   },
   plugins: [
