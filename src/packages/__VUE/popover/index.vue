ea7669388a5320a8c6067e96904d5fefdb00a005
liqiong-lab <62087481+liqiong-lab@users.noreply.github.com>
2021-09-27 11:17:29 +0800

    feat: tag/badge/popover组件提交 (#685)
    * fix: tag/badge/popover组件提交
    
    * fix: tag/popover/badge组件优化

---
+++ src/packages/__VUE/popover/index.vue
@@ -0,0 +1,124 @@
+<template>
+  <div @click="openPopover()" :class="classes">
+    <slot name="reference"></slot>
+
+    <template v-if="showPopup">
+      <div class="more-background" @click="closePopover()"> </div>
+      <div :class="popoverContent">
+        <div :class="popoverArrow"> </div>
+
+        <slot name="content"></slot>
+
+        <div
+          v-for="item in iconItemList"
+          :key="item.name"
+          :class="{ 'title-item': true, disabled: item.disabled }"
+        >
+          <slot v-if="item.icon">
+            <nut-icon class="item-img" :name="item.icon"></nut-icon
+          ></slot>
+          <div class="title-name">{{ item.name }}</div>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+<script lang="ts">
+import { onMounted, computed, watch, ref, PropType, toRefs } from 'vue';
+import { createComponent } from '../../utils/create';
+const { componentName, create } = createComponent('popover');
+import Popup, { popupProps } from '../popup/index.vue';
+import Button from '../button/index.vue';
+
+export type PopoverTheme = 'light' | 'dark';
+
+export type PopoverLocation = 'bottom' | 'top' | 'left' | 'right';
+
+export default create({
+  inheritAttrs: false,
+  components: {
+    [Popup.name]: Popup,
+    [Button.name]: Button
+  },
+  props: {
+    ...popupProps,
+    iconItemList: {
+      type: Array,
+      default: []
+    },
+
+    theme: {
+      type: String as PropType<PopoverTheme>,
+      default: 'light'
+    },
+
+    location: {
+      type: String as PropType<PopoverLocation>,
+      default: 'bottom'
+    }
+  },
+  emits: ['update', 'update:visible', 'close'],
+  setup(props, { emit }) {
+    const showPopup = ref(props.visible);
+
+    const { theme, location } = toRefs(props);
+
+    const classes = computed(() => {
+      const prefixCls = componentName;
+      return {
+        [prefixCls]: true,
+        [`${prefixCls}--${theme.value}`]: theme.value
+      };
+    });
+
+    const popoverContent = computed(() => {
+      const prefixCls = 'popoverContent';
+      return {
+        [prefixCls]: true,
+        [`${prefixCls}--${location.value}`]: location.value
+      };
+    });
+
+    const popoverArrow = computed(() => {
+      const prefixCls = 'popoverArrow';
+      return {
+        [prefixCls]: true,
+        [`${prefixCls}--${location.value}`]: location.value
+      };
+    });
+
+    onMounted(() => {});
+
+    watch(
+      () => props.visible,
+      (value) => {
+        showPopup.value = value;
+      }
+    );
+
+    const update = (val: boolean) => {
+      emit('update', val);
+      emit('update:visible', val);
+    };
+
+    const openPopover = () => {
+      update(!props.visible);
+    };
+
+    const closePopover = (e) => {
+      e.stopPropagation();
+      emit('close');
+      emit('update:visible', false);
+    };
+
+    return {
+      classes,
+      showPopup,
+      openPopover,
+      popoverContent,
+      popoverArrow,
+      closePopover
+    };
+  }
+});
+</script>
---
+++ src/sites/mobile-taro/vue/src/dentry/pages/badge/index.vue
@@ -0,0 +1,127 @@
+<template>
+  <view class="demo">
+    <h2>默认用法</h2>
+    <view class="demo-w">
+      <nut-badge :value="8" class="item">
+        <view class="demo-svg"></view>
+      </nut-badge>
+      <nut-badge :value="76" class="item">
+        <view class="demo-svg"></view>
+      </nut-badge>
+      <nut-badge value="NEW" class="item">
+        <view class="demo-svg"></view>
+      </nut-badge>
+      <nut-badge isDot class="item">
+        <view class="demo-svg"></view>
+      </nut-badge>
+    </view>
+    <h2>最大值</h2>
+    <view class="demo-w">
+      <nut-badge :value="200" :max="9" class="item">
+        <view class="demo-svg"></view
+      ></nut-badge>
+      <nut-badge :value="200" :max="20" class="item">
+        <view class="demo-svg"></view
+      ></nut-badge>
+      <nut-badge :value="200" :max="99" class="item">
+        <view class="demo-svg"></view
+      ></nut-badge>
+    </view>
+
+    <h2>自定义颜色</h2>
+    <view class="demo-w">
+      <nut-badge
+        :value="8"
+        class="item"
+        color="linear-gradient(315deg, rgba(73,143,242,1) 0%,rgba(73,101,242,1) 100%)"
+      >
+        <view class="demo-svg"></view
+      ></nut-badge>
+      <nut-badge
+        :value="76"
+        class="item"
+        color="linear-gradient(315deg, rgba(73,143,242,1) 0%,rgba(73,101,242,1) 100%)"
+      >
+        <view class="demo-svg"></view
+      ></nut-badge>
+      <nut-badge
+        value="NEW"
+        class="item"
+        color="linear-gradient(315deg, rgba(73,143,242,1) 0%,rgba(73,101,242,1) 100%)"
+      >
+        <view class="demo-svg"></view
+      ></nut-badge>
+      <nut-badge
+        isDot
+        class="item"
+        color="linear-gradient(315deg, rgba(73,143,242,1) 0%,rgba(73,101,242,1) 100%)"
+      >
+        <view class="demo-svg"></view
+      ></nut-badge>
+    </view>
+
+    <h2>自定义徽标内容</h2>
+    <view class="demo-w">
+      <nut-badge class="item">
+        <template #icons>
+          <nut-icon name="Check" color="#ffffff" size="12"></nut-icon>
+        </template>
+        <view class="demo-svg"></view>
+      </nut-badge>
+      <nut-badge class="item">
+        <template #icons>
+          <nut-icon name="link" color="#ffffff" size="12"></nut-icon>
+        </template>
+        <view class="demo-svg"></view
+      ></nut-badge>
+      <nut-badge class="item">
+        <template #icons>
+          <nut-icon name="download" color="#ffffff" size="12"></nut-icon>
+        </template>
+        <view class="demo-svg"></view
+      ></nut-badge>
+    </view>
+
+    <h2>自定义位置</h2>
+    <view class="demo-w">
+      <nut-badge :value="8" top="5" right="5" class="item">
+        <view class="demo-svg"></view
+      ></nut-badge>
+      <nut-badge :value="76" top="10" right="10" class="item">
+        <view class="demo-svg"></view
+      ></nut-badge>
+      <nut-badge value="NEW" class="item">
+        <view class="demo-svg"></view
+      ></nut-badge>
+    </view>
+
+    <h2>独立展示</h2>
+    <view class="demo-w">
+      <nut-badge :value="8" class="item"> </nut-badge>
+      <nut-badge :value="76" class="item"> </nut-badge>
+      <nut-badge value="NEW" class="item"> </nut-badge>
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {}
+};
+</script>
+
+<style lang="scss">
+.demo-w {
+  margin: 20px 0;
+  .item {
+    margin: 10px 20px;
+    .demo-svg {
+      display: inline-block;
+      width: 40px;
+      height: 40px;
+      background: rgba(229, 232, 232);
+      border-radius: 3px;
+    }
+  }
+}
+</style>
