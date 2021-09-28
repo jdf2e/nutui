69ebaace393dba6ce1670a8c30dd34fabedc5ca1
richard1015 <51844712@qq.com>
2021-09-28 16:39:49 +0800

    docs:  pagination 优化

--- src/packages/__VUE/pagination/demo.vue
+++ src/packages/__VUE/pagination/demo.vue
@@ -1,26 +1,13 @@
 <template>
   <div class="demo">
     <h2>基础用法</h2>
-    <nut-pagination
-      v-model="currentPage"
-      :total-items="25"
-      :items-per-page="5"
-    />
+    <nut-pagination v-model="currentPage" :total-items="25" :items-per-page="5" @change="pageChange" />
     <h2>简单模式</h2>
-    <nut-pagination v-model="currentPage1" :page-count="12" mode="simple" />
+    <nut-pagination v-model="currentPage1" :page-count="12" mode="simple" @change="pageChange" />
     <h2>显示省略号</h2>
-    <nut-pagination
-      v-model="currentPage2"
-      :total-items="125"
-      :show-page-size="3"
-      force-ellipses
-    />
+    <nut-pagination v-model="currentPage2" :total-items="125" :show-page-size="3" force-ellipses @change="pageChange" />
     <h2>自定义按钮</h2>
-    <nut-pagination
-      v-model="currentPage3"
-      :total-items="500"
-      :show-page-size="5"
-    >
+    <nut-pagination v-model="currentPage3" :total-items="500" :show-page-size="5" @change="pageChange">
       <template #prev-text>
         <nut-icon name="left" size="10px" />
       </template>
@@ -46,8 +33,8 @@ export default createDemo({
       currentPage2: 1,
       currentPage3: 1
     });
-    const pageChange = (value) => {
-      console.log(value, 444);
+    const pageChange = (value: number) => {
+      console.log(value);
     };
 
     return {
@@ -57,5 +44,3 @@ export default createDemo({
   }
 });
 </script>
-
-<style lang="scss" scoped></style>
