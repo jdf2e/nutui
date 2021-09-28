948d98c42b5b017c97c8977fb198652f6204f397
yushuang-d <yush9210@163.com>
2021-09-28 14:44:24 +0800

    fix: 分页样式提取

--- src/packages/__VUE/pagination/index.taro.vue
+++ src/packages/__VUE/pagination/index.taro.vue
@@ -159,7 +159,3 @@ export default create({
   }
 });
 </script>
-
-<style lang="scss">
-@import 'index.scss';
-</style>
--- src/packages/__VUE/pagination/index.vue
+++ src/packages/__VUE/pagination/index.vue
@@ -160,7 +160,3 @@ export default create({
   }
 });
 </script>
-
-<style lang="scss">
-@import 'index.scss';
-</style>
