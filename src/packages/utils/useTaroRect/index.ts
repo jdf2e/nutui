5f79419146af5ac878da56bab467a8dcbc00134f
richard1015 <51844712@qq.com>
2021-09-28 18:42:40 +0800

    fix(range、uploader): taro h5 env bug #682

--- src/packages/utils/useTaroRect/index.ts
+++ src/packages/utils/useTaroRect/index.ts
@@ -9,18 +9,48 @@
  */
 
 import { Ref, unref } from 'vue';
-
-export const useTaroRect = (
-  elementRef: (Element | Window) | Ref<Element | Window | undefined>,
-  Taro: any
-): any => {
-  const element = unref(elementRef);
+function isWindow(val: unknown): val is Window {
+  return val === window;
+}
+export const useTaroRect = (elementRef: (Element | Window) | Ref<Element | Window | undefined>, Taro: any): any => {
+  let element = unref(elementRef);
 
   return new Promise((resolve) => {
-    const query = Taro.createSelectorQuery();
-    query.select(`#${(element as any).id}`).boundingClientRect();
-    query.exec(function (res: any) {
-      resolve(res[0]);
-    });
+    if (Taro.getEnv() === 'WEB') {
+      if (element && element.$el) {
+        element = element.$el;
+      }
+      if (isWindow(element)) {
+        const width = element.innerWidth;
+        const height = element.innerHeight;
+
+        resolve({
+          top: 0,
+          left: 0,
+          right: width,
+          bottom: height,
+          width,
+          height
+        });
+      }
+      if (element && element.getBoundingClientRect) {
+        resolve(element.getBoundingClientRect());
+      }
+
+      resolve({
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        width: 0,
+        height: 0
+      });
+    } else {
+      const query = Taro.createSelectorQuery();
+      query.select(`#${(element as any).id}`).boundingClientRect();
+      query.exec(function (res: any) {
+        resolve(res[0]);
+      });
+    }
   });
 };
