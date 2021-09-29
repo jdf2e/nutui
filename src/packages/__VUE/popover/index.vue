7bf954444402ad9c22a3c3e1a848e4a97c775740
liqiong43 <1450805902@qq.com>
2021-09-29 11:04:02 +0800

    fix: popover组件review修改

--- src/packages/__VUE/popover/index.vue
+++ src/packages/__VUE/popover/index.vue
@@ -1,21 +1,21 @@
 <template>
-  <div @click="openPopover()" :class="classes">
+  <view @click="openPopover()" :class="classes">
     <slot name="reference"></slot>
 
     <template v-if="showPopup">
-      <div class="more-background" @click="closePopover()"> </div>
-      <div :class="popoverContent">
-        <div :class="popoverArrow"> </div>
+      <view class="more-background" @click="closePopover()"> </view>
+      <view :class="popoverContent">
+        <view :class="popoverArrow"> </view>
 
         <slot name="content"></slot>
 
-        <div v-for="item in lists" :key="item.name" :class="{ 'title-item': true, disabled: item.disabled }">
+        <view v-for="item in lists" :key="item.name" :class="{ 'title-item': true, disabled: item.disabled }">
           <slot v-if="item.icon"> <nut-icon class="item-img" :name="item.icon"></nut-icon></slot>
-          <div class="title-name">{{ item.name }}</div>
-        </div>
-      </div>
+          <view class="title-name">{{ item.name }}</view>
+        </view>
+      </view>
     </template>
-  </div>
+  </view>
 </template>
 <script lang="ts">
 import { onMounted, computed, watch, ref, PropType, toRefs } from 'vue';
