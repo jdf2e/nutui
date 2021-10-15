023dd28879b6bf578fea61d0264ee70397db2c35
wangbei16 <suanbanren@foxmail.com>
2021-10-15 15:00:50 +0800

    chore: gitignore

--- src/utils/raf.ts
+++ src/utils/raf.ts
@@ -2,7 +2,6 @@ function requestAniFrame() {
   if (typeof window !== 'undefined') {
     return (
       window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
       function (callback) {
         window.setTimeout(callback, 1000 / 60)
       }
