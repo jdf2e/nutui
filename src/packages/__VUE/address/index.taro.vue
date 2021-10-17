4ea5e30a47c5f8e731e7be4eb2a408da4fce132c
suzigang <1039168735@qq.com>
2021-10-17 10:04:34 +0800

    fix(address): add data error check

--- src/packages/__VUE/address/index.taro.vue
+++ src/packages/__VUE/address/index.taro.vue
@@ -256,6 +256,13 @@ export default create({
 
       if (!data.length) return [];
 
+      data.forEach((item: RegionData) => {
+        if (!item.title) {
+          console.error('[NutUI] <Address> 请检查数组选项的 title 值是否有设置 ,title 为必填项 .');
+          return;
+        }
+      });
+
       const newData: CustomRegionData[] = [];
 
       data = data.sort((a: RegionData, b: RegionData) => {
