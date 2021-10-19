baacaf81ca1234a5f36840c686895030babcfc8e
zongyue3 <415269619@qq.com>
2021-10-19 16:28:55 +0800

    feat: searchbar 开发初步完成；迁移至taro

--- src/packages/__VUE/searchbar/demo.vue
+++ src/packages/__VUE/searchbar/demo.vue
@@ -1,29 +1,54 @@
 <template>
   <div class="demo">
     <h2>基础用法</h2>
-    <nut-cell>
-      <nut-searchbar>
-        <template v-slot:left>lalala</template>
-        <template v-slot:left-in>2222</template>
-        <template v-slot:right>rrr</template>
-        <template v-slot:right-in>ririri</template>
-      </nut-searchbar>
-    </nut-cell>
+    <nut-searchbar class="wrap" v-model="searchValue" @search="search">
+      <template v-slot:leftout>
+        <nut-icon @click="clickLeft" size="20" name="left"></nut-icon>
+      </template>
+      <template v-slot:leftin>
+        <nut-icon size="14" name="search2"></nut-icon>
+      </template>
+      <template v-slot:rightin>
+        <nut-icon size="20" name="photograph"></nut-icon>
+      </template>
+      <template v-slot:rightout>
+        <nut-icon size="20" name="message"></nut-icon>
+      </template>
+    </nut-searchbar>
   </div>
 </template>
 
 <script lang="ts">
+import { toRefs, reactive } from 'vue';
 import { createComponent } from '../../utils/create';
 const { createDemo } = createComponent('searchbar');
 export default createDemo({
   props: {},
   setup() {
-    return {};
+    const state = reactive({
+      searchValue: ''
+    });
+
+    const search = function () {
+      console.log('ENTER clicked');
+    };
+
+    const clickLeft = function () {
+      console.log('left clicked');
+    };
+
+    return {
+      clickLeft,
+      search,
+      ...toRefs(state)
+    };
   }
 });
 </script>
 
 <style lang="scss" scoped>
-.demo {
+.wrap {
+  height: 80px;
+  background: rgba(0, 0, 0, 0.1);
 }
 </style>
