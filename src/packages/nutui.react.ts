3ce715a59656ceb213c0984cd8068acd60c78b41
zhenyulei <yulei20080106@126.com>
2021-10-29 10:44:00 +0800

    fix: 优化collapse组件

---
+++ src/packages/nutui.react.ts
@@ -0,0 +1,41 @@
+import Avatar from './avatar'
+import Button from './button'
+import Cell from './cell'
+import Icon from './icon'
+import Price from './price'
+import Overlay from './overlay'
+import Drag from './drag'
+import Collapse from './collapse'
+import Toast from './toast'
+import BackTop from './backtop'
+import Steps from './steps'
+import NavBar from './navbar'
+import Tabbar from './tabbar'
+import InputNumber from './inputnumber'
+import Rate from './rate'
+import Uploader from './uploader'
+import Input from './input'
+import TextArea from './textarea'
+import CheckBox from './checkbox'
+
+export {
+  Avatar,
+  Button,
+  Cell,
+  Icon,
+  Price,
+  Overlay,
+  Drag,
+  Collapse,
+  Toast,
+  BackTop,
+  Steps,
+  NavBar,
+  Tabbar,
+  InputNumber,
+  Rate,
+  Uploader,
+  Input,
+  TextArea,
+  CheckBox,
+}
