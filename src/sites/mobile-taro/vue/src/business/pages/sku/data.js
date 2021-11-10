1bce9b03fd30bfddb9cd4a15b27e0a127617f3c0
yangxiaolu1993 <32215990+yangxiaolu1993@users.noreply.github.com>
2021-11-10 15:10:52 +0800

    feat: sku 商品规格组件 #64 (#827)
    * feat: sku
    
    * feat: 可配置颜色提出
    
    * feat: prop 调整

---
+++ src/sites/mobile-taro/vue/src/business/pages/sku/data.js
@@ -0,0 +1,57 @@
+export const Sku = [
+  {
+    id: 1,
+    name: '种类',
+    list: [
+      {
+        name: '五香味150g*3',
+        id: 100016015112,
+        _active: true,
+        _disable: false
+      },
+      {
+        name: '五香味150g*8',
+        id: 100016015142,
+        _active: false,
+        _disable: true
+      },
+      {
+        name: '香辣味150g*3',
+        id: 100016015078,
+        _active: false,
+        _disable: false
+      },
+      {
+        name: '香辣味150g*8',
+        id: 100009064831,
+        _active: false,
+        _disable: true
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: '规格',
+    list: [
+      {
+        name: '150g',
+        id: 100016015102,
+        _active: true,
+        _disable: false
+      },
+      {
+        name: '150g*8',
+        id: 100016015122,
+        _active: false,
+        _disable: false
+      }
+    ]
+  }
+];
+
+export const Goods = {
+  skuId: '100016015112',
+  price: '9.10',
+  imagePath:
+    'https://img13.360buyimg.com/imagetools/s750x750_jfs/t1/155184/15/3792/210311/5f98da84Efb6e1da6/d2a3b5ca1fc7019c.jpg'
+};
