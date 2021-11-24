c56e55cb6f73736ec06db60b9fb5f35bf33744ec
Drjnigfubo <1783194754@qq.com>
2021-11-24 21:14:51 +0800

    feat(toast): 增加标题属性，调整自定义位置

--- src/packages/__VUE/toast/demo.vue
+++ src/packages/__VUE/toast/demo.vue
@@ -1,43 +1,18 @@
 <template>
   <div class="demo">
     <h2>基本用法</h2>
-    <nut-cell
-      title="Text 文字提示"
-      is-link
-      @click="textToast('网络失败，请稍后再试~')"
-    ></nut-cell>
-    <nut-cell
-      title="Success 成功提示"
-      is-link
-      @click="successToast('成功提示')"
-    ></nut-cell>
-    <nut-cell
-      title="Error 失败提示"
-      is-link
-      @click="errorToast('失败提示')"
-    ></nut-cell>
-    <nut-cell
-      title="Warning 警告提示"
-      is-link
-      @click="warningToast('警告提示')"
-    ></nut-cell>
-    <nut-cell
-      title="Loading 加载提示"
-      is-link
-      @click="loadingToast('加载中')"
-    ></nut-cell>
+    <nut-cell title="Text 文字提示" is-link @click="textToast('网络失败，请稍后再试~')"></nut-cell>
+    <nut-cell title="Title 标题展示" is-link @click="titleToast('网络失败，请稍后再试~')"></nut-cell>
+    <nut-cell title="Success 成功提示" is-link @click="successToast('成功提示')"></nut-cell>
+    <nut-cell title="Error 失败提示" is-link @click="errorToast('失败提示')"></nut-cell>
+    <nut-cell title="Warning 警告提示" is-link @click="warningToast('警告提示')"></nut-cell>
+    <nut-cell title="Loading 加载提示" is-link @click="loadingToast('加载中')"></nut-cell>
     <h2>Toast不消失</h2>
-    <nut-cell
-      title="Toast 文字提示不消失"
-      is-link
-      @click="NoToast('Toast不消失~')"
-    ></nut-cell>
+    <nut-cell title="Toast 文字提示不消失" is-link @click="NoToast('Toast不消失~')"></nut-cell>
+    <h2>Toast自定义距离底部高度</h2>
+    <nut-cell title="Toast 自定义底部高度" is-link @click="BottomToast('自定义距离~')"></nut-cell>
     <h2>Loading带透明遮罩</h2>
-    <nut-cell
-      title="带文案+带透明遮罩+自动消失"
-      is-link
-      @click="NoLoading('加载中~')"
-    ></nut-cell>
+    <nut-cell title="带文案+带透明遮罩+自动消失" is-link @click="NoLoading('加载中~')"></nut-cell>
   </div>
 </template>
 
@@ -49,6 +24,11 @@ export default createDemo({
   setup() {
     const textToast = (msg) => {
       Toast.text(msg);
+    };
+    const titleToast = (msg) => {
+      Toast.text(msg, {
+        title: '标题文字'
+      });
     };
     const successToast = (msg) => {
       Toast.success(msg);
@@ -67,6 +47,12 @@ export default createDemo({
         duration: 0
       });
     };
+    const BottomToast = (msg) => {
+      Toast.text(msg, {
+        center: false,
+        bottom: '10%'
+      });
+    };
     const NoLoading = (msg) => {
       Toast.loading(msg, {
         cover: true
@@ -74,12 +60,14 @@ export default createDemo({
     };
     return {
       textToast,
+      titleToast,
       successToast,
       errorToast,
       warningToast,
       loadingToast,
       NoToast,
-      NoLoading
+      NoLoading,
+      BottomToast
     };
   }
 });
