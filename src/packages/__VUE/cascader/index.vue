ca88619c5f5b4c5bfc9af5effc6fd2e88966a166
dongji136 <475586651@qq.com>
2021-11-26 14:32:03 +0800

    feat: 新增级联选择器组件

---
+++ src/packages/__VUE/cascader/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <nut-cascader-item />
+</template>
+<script lang="ts">
+import { createComponent } from '../../utils/create';
+const { create } = createComponent('cascader');
+import CascaderItem from './cascader-item.vue';
+
+export default create({
+  components: {
+    [CascaderItem.name]: CascaderItem
+  }
+});
+</script>
