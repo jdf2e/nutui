da8aa93138fadfae12353603c8c5f41b90281102
dongj0316 <475586651@qq.com>
2021-12-12 18:47:07 +0800

    feat: cascader 级联选择器 (#856)
    * feat: 新增级联选择器组件
    
    * fix: fix lazyLoad
    
    * fix: fix lazyLoad
    
    * feat: add popup, code optimization
    
    * fix: sync value
    
    * test: add test
    
    * style: add variables
    
    * style: add variables
    
    * feat: add taro
    
    * fix: test
    
    * upd: perf
    
    * upd: code/docs

--- src/sites/mobile-taro/vue/src/app.config.ts
+++ src/sites/mobile-taro/vue/src/app.config.ts
@@ -73,7 +73,8 @@ export default {
         'pages/numberkeyboard/index',
         'pages/badge/index',
         'pages/tag/index',
-        'pages/popover/index'
+        'pages/popover/index',
+        'pages/cascader/index'
       ]
     },
     {
