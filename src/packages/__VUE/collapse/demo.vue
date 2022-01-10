ba3fa50034b6a76246dfbe0d4d6916a26050dd47
Ymm0008 <being0008@163.com>
2022-01-10 17:50:25 +0800

    fix: collapse 无法动态更新问题修复

--- src/packages/__VUE/collapse/demo.vue
+++ src/packages/__VUE/collapse/demo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="demo full">
     <h2>基本用法</h2>
-    <nut-collapse v-model:active="active1" icon="down-arrow">
+    <nut-collapse v-model:active="active1" icon="down-arrow" @change="change">
       <nut-collapse-item :name="1">
         <template v-slot:mTitle>
           {{ title1 }}
@@ -11,38 +11,26 @@
       <nut-collapse-item :title="title2" :name="2">
         在产品的功能、体验、易用性和灵活性等各个方面做了全面的升级！
       </nut-collapse-item>
-      <nut-collapse-item :title="title3" :name="3" disabled>
-      </nut-collapse-item>
+      <nut-collapse-item :title="title3" :name="3" disabled> </nut-collapse-item>
     </nut-collapse>
     <h2>无icon样式</h2>
     <nut-collapse v-model:active="active4" :accordion="true">
       <nut-collapse-item :title="title1" :name="1">
         引入Vue3新特性 Composition API、Teleport、Emits 等
       </nut-collapse-item>
-      <nut-collapse-item :title="title2" :name="2">
-        全面使用 TypeScipt
-      </nut-collapse-item>
+      <nut-collapse-item :title="title2" :name="2"> 全面使用 TypeScipt </nut-collapse-item>
     </nut-collapse>
     <h2>手风琴</h2>
     <nut-collapse v-model:active="active2" :accordion="true" icon="down-arrow">
-      <nut-collapse-item :title="title1" :name="1">
-        基于京东设计语言体系，构建场景
-      </nut-collapse-item>
-      <nut-collapse-item :title="title2" :name="2" :sub-title="subTitle">
-        提高界⾯的模块化通用程度
-      </nut-collapse-item>
+      <nut-collapse-item :title="title1" :name="1"> 基于京东设计语言体系，构建场景 </nut-collapse-item>
+      <nut-collapse-item :title="title2" :name="2" :sub-title="subTitle"> 提高界⾯的模块化通用程度 </nut-collapse-item>
       <nut-collapse-item :title="title3" :name="3">
         采用组合式 API Composition 语法重构，结构清晰，功能模块化
       </nut-collapse-item>
     </nut-collapse>
     <h2>自定义折叠图标</h2>
 
-    <nut-collapse
-      v-model:active="active3"
-      :accordion="true"
-      icon="arrow-right2"
-      rotate="90"
-    >
+    <nut-collapse v-model:active="active3" :accordion="true" icon="arrow-right2" rotate="90">
       <nut-collapse-item :title="title1" :name="1">
         <template v-slot:sTitle> 文本测试 </template>
         NUTUI3.0重新思考其内在的一致性和可组合性
@@ -61,9 +49,7 @@
       icon="down-arrow"
       :accordion="true"
     >
-      <nut-collapse-item :title="title1" :name="1">
-        组件 emits 事件单独提取，增强代码可读性
-      </nut-collapse-item>
+      <nut-collapse-item :title="title1" :name="1"> 组件 emits 事件单独提取，增强代码可读性 </nut-collapse-item>
       <nut-collapse-item :title="title2" :name="2" sub-title="文本内容">
         使用 Teleport 新特性重构挂载类组件
       </nut-collapse-item>
