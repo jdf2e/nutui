5b5f0ff29344fe5959d71662e94801eb6582b83b
yangxiaolu3 <962468124@qq.com>
2022-01-12 17:27:03 +0800

    feat: sync-docs 内容修改

--- jd/copymd.js
+++ jd/copymd.js
@@ -1,4 +1,4 @@
-const targetBaseUrl = 'site_docs';
+const targetBaseUrl = `${process.cwd()}/site_docs`;
 const fse = require('fs-extra');
 const copyFile = (from, to) => {
   fse
@@ -10,20 +10,58 @@ const copyFile = (from, to) => {
       console.error(err);
     });
 };
+const removeFile = async (url) => {
+  return new Promise((res, rej) => {
+    fse.remove(url, (err) => {
+      if (err) {
+        throw err;
+      }
+      res(true);
+    });
+  });
+};
+
 const copy = async () => {
   let configPath = `src/config.json`;
   let configPkgPath = `package.json`;
   let nutuiDocsConfigPath = `${targetBaseUrl}/config.json`;
 
-  const exists = await fse.pathExists(configPath);
+  // 判断 site_docs 文件是否存在根路径中
+  const existsRoot = await fse.pathExists(targetBaseUrl);
 
-  if (exists) {
-    const fromConfig = await fse.readJson(configPath);
+  if (existsRoot) await removeFile(targetBaseUrl);
+  // 复制所有组件
+  const fromConfig = await fse.readJson(configPath);
+  fromConfig.nav.forEach(({ packages }) => {
+    packages.forEach((item) => {
+      if (item.show) {
+        let cmpName = item.name.toLowerCase();
+        let docpath = `src/packages/__VUE/${cmpName}/doc.md`;
+        let doctaropath = `src/packages/__VUE/${cmpName}/doc.taro.md`;
+        fse.readFile(docpath, (err, data) => {
+          if (!err) {
+            copyFile(docpath, `${targetBaseUrl}/docs/${cmpName}/doc.md`);
+          }
+        });
+        fse.readFile(doctaropath, (err, data) => {
+          if (!err) {
+            copyFile(doctaropath, `${targetBaseUrl}/docs/${cmpName}/doc.taro.md`);
+          }
+        });
+      }
+    });
+  });
 
-    const fromPkgConfig = await fse.readJson(configPkgPath);
+  // 复制 config.json
+  const fromPkgConfig = await fse.readJson(configPkgPath);
 
-    const docsConfig = await fse.readJson(nutuiDocsConfigPath);
-
+  const obj = {
+    version: '',
+    nav: [],
+    docs: []
+  };
+  fse.outputJSON(nutuiDocsConfigPath, obj, () => {
+    const docsConfig = fse.readJson(nutuiDocsConfigPath);
     docsConfig.version = fromPkgConfig.version;
     docsConfig.nav = fromConfig.nav;
     docsConfig.docs = fromConfig.docs;
@@ -33,28 +71,9 @@ const copy = async () => {
         spaces: 2
       })
       .then(() => {
-        console.log(`${docsConfig.version} success!`);
+        console.log(`${fromPkgConfig.version} success!`);
       });
-    fromConfig.nav.forEach(({ packages }) => {
-      packages.forEach((item) => {
-        if (item.show) {
-          let cmpName = item.name.toLowerCase();
-          let docpath = `src/packages/__VUE/${cmpName}/doc.md`;
-          let doctaropath = `src/packages/__VUE/${cmpName}/doc.taro.md`;
-          fse.readFile(docpath, (err, data) => {
-            if (!err) {
-              copyFile(docpath, `${targetBaseUrl}/docs/${cmpName}/doc.md`);
-            }
-          });
-          fse.readFile(doctaropath, (err, data) => {
-            if (!err) {
-              copyFile(doctaropath, `${targetBaseUrl}/docs/${cmpName}/doc.taro.md`);
-            }
-          });
-        }
-      });
-    });
-  }
+  });
 };
 // copy(reactBaseUrl, 'react');
 copy();
