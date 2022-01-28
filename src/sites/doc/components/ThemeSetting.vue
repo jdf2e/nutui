4a9851afdf687fb8f9d904a368a82cd032b2a5b4
richard1015 <51844712@qq.com>
2022-01-28 17:52:39 +0800

    feat: mockdate theme setting

--- src/sites/doc/components/ThemeSetting.vue
+++ src/sites/doc/components/ThemeSetting.vue
@@ -2,43 +2,74 @@
   <div class="theme-setting">
     {{ name }}
     <ul>
-      <li :key="index" v-for="(item, index) in styleList">
+      <li :key="index" v-for="(item, index) in state.styleList">
         <p>{{ item.name }}</p>
         <div class="color-picker">
-          <span>{{ item.value }}</span>
-          <input type="color" v-model="item.value" />
+          <n-color-picker v-if="item.isColor" :modes="['rgb', 'hex']" v-model="item.value" />
+          <n-input v-else v-model:value="item.value" type="text" />
         </div>
       </li>
     </ul>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
+import { defineComponent, reactive, watch } from 'vue';
+import { varcss } from './mockdata';
+import { NColorPicker, NInput } from 'naive-ui';
 export default defineComponent({
   name: 'theme-setting',
   props: {
     name: String
   },
+  components: {
+    NColorPicker,
+    NInput
+  },
   setup(props) {
     // 获取样式文件，正则匹配
+    let state = reactive({
+      styleList: [
+        {
+          name: '$primary-color',
+          value: '#2c68ff',
+          isColor: true
+        },
+        {
+          name: '$primary-color',
+          value: '#2c68ff',
+          isColor: true
+        }
+      ]
+    });
 
-    // https://raw.githubusercontent.com/jdf2e/nutui/next/src/packages/styles/variables.scss
-    var pattern = /\$button.*;/;
-    // 需要包含换行
-    console.log(pattern.test('str'));
+    const findStyle = (componentName: string) => {
+      // https://raw.githubusercontent.com/jdf2e/nutui/next/src/packages/styles/variables.scss
+      // var pattern = /\$button.*;/g;
+      var p = new RegExp(`\\$${componentName}.*;`, 'g');
+      let parray: any[] = varcss.match(p) || [];
+      // 需要包含换行
+      let commponetns = parray.map((item) => {
+        let cArray = item.split(':');
+        let name = cArray[0],
+          value: string = cArray[1].replace(' !default;', '').trim();
+        return {
+          name,
+          value,
+          isColor: value.includes('#') || value.includes('rgb')
+        };
+      });
+      console.log(commponetns);
+      state.styleList = commponetns;
+    };
 
-    const styleList = reactive([
-      {
-        name: '$primary-color',
-        value: '#2c68ff'
-      },
-      {
-        name: '$primary-color',
-        value: '#2c68ff'
+    watch(
+      () => props.name,
+      (value: any) => {
+        findStyle(value);
       }
-    ]);
+    );
 
-    return { styleList };
+    return { state };
   }
 });
 </script>
@@ -46,29 +77,21 @@ export default defineComponent({
 <style lang="scss" scoped>
 .theme-setting {
   li {
-    width: 500px;
     list-style: none;
     margin-bottom: 12px;
+    display: flex;
+    align-items: center;
     &::before {
       display: none;
     }
     .color-picker {
-      display: flex;
-      align-items: center;
+      width: 300px;
+      margin-left: 20px;
     }
+
     p {
       font-size: 14px;
       text-overflow: ellipsis;
-      margin-bottom: 12px;
-    }
-    input {
-      padding: 0;
-      margin: 0;
-      width: 100%;
-      background: none;
-      border: 1px solid #ccc;
-      border-radius: 2px;
-      height: 34px;
     }
   }
 }
