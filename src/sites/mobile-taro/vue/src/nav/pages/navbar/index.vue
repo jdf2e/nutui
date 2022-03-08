8d6e6719971168e323f3aeb2f5bcf564d46fa667
liqiong-lab <62087481+liqiong-lab@users.noreply.github.com>
2022-03-08 20:44:29 +0800

    upd: navbar 新增顶部固定props及安全区适配  (#1058)

--- src/sites/mobile-taro/vue/src/nav/pages/navbar/index.vue
+++ src/sites/mobile-taro/vue/src/nav/pages/navbar/index.vue
@@ -1,51 +1,55 @@
 <template>
   <div class="demo full">
     <h2>基础用法</h2>
+
+    <nut-navbar @on-click-back="back" @on-click-title="title" title="订单详情">
+      <template #left>
+        <div>返回</div>
+      </template>
+      <template #right>
+        <nut-icon class="right" name="share-n"></nut-icon>
+      </template>
+    </nut-navbar>
+
     <nut-navbar
       @on-click-back="back"
       @on-click-title="title"
-      @on-click-send="send"
-      title="订单详情"
-      icon="share-n"
-    ></nut-navbar>
-    <nut-navbar
-      @on-click-back="back"
-      @on-click-title="title"
-      @on-click-clear="clear"
+      @on-click-right="rightClick"
       title="浏览记录"
       desc="清空"
     ></nut-navbar>
+
     <nut-navbar
       :left-show="false"
+      @on-click-back="back"
       @on-click-title="title"
       @on-click-icon="icon"
-      @on-click-clear="edit"
-      @on-click-send="more"
+      @on-click-right="rightClick"
       title="购物车"
       titIcon="cart2"
       desc="编辑"
-      icon="more-x"
-    ></nut-navbar>
+    >
+      <template #right>
+        <nut-icon class="right" name="more-x"></nut-icon>
+      </template>
+    </nut-navbar>
 
     <h2>自定义导航栏中间内容</h2>
-    <nut-navbar
-      @on-click-back="back"
-      @on-click-title="title"
-      @on-click-clear="edit"
-      @on-click-send="list"
-      desc="编辑"
-      icon="horizontal-n"
-    >
+    <nut-navbar @on-click-back="back" @on-click-title="title" @on-click-right="rightClick" desc="编辑">
       <template #content>
         <nut-tabs v-model="tab1value" @click="changeTab">
           <nut-tabpane title="商品"> </nut-tabpane>
           <nut-tabpane title="店铺"> </nut-tabpane>
         </nut-tabs>
       </template>
+
+      <template #right>
+        <nut-icon class="right" name="more-x"></nut-icon>
+      </template>
     </nut-navbar>
 
     <h2>多tab切换导航</h2>
-    <nut-navbar @on-click-back="back" @on-click-send="list" icon="more-x">
+    <nut-navbar @on-click-back="back">
       <template #content>
         <nut-tabs v-model="tab2value" @click="changeTabList">
           <nut-tabpane title="商品"> </nut-tabpane>
@@ -55,7 +59,11 @@
         </nut-tabs>
       </template>
       <template #icons>
-        <nut-icon class="icon" name="share" @on-click-slot-send="morelist"></nut-icon>
+        <nut-icon class="icon" name="share"></nut-icon>
+      </template>
+
+      <template #right>
+        <nut-icon class="right" name="horizontal-n"></nut-icon>
       </template>
     </nut-navbar>
   </div>
@@ -64,7 +72,7 @@
 <script lang="ts">
 import { ref, defineComponent } from 'vue';
 export default defineComponent({
-  setup(props, { emit }) {
+  setup({}) {
     const tab1value = ref(0);
     const tab2value = ref(0);
     const methods = {
@@ -77,23 +85,9 @@ export default defineComponent({
       icon() {
         alert('icon');
       },
-      send() {
-        alert('发送');
-      },
-      edit() {
-        alert('编辑');
-      },
-      more() {
-        alert('更多');
-      },
-      clear() {
-        alert('清空');
-      },
-      list() {
-        alert('列表');
-      },
-      morelist() {
-        alert('多个更多');
+
+      rightClick() {
+        alert('右侧点击事件');
       },
       changeTab(tab: any) {
         tab1value.value = tab.paneKey as number;
