8bc7d10209a9421871f82c5cfcd3a9e5a6599e9e
1450805902@qq.com <1450805902@qq.com>
2022-03-17 18:25:57 +0800

    fix: badge test

--- src/packages/__VUE/badge/__test__/badge.spec.ts
+++ src/packages/__VUE/badge/__test__/badge.spec.ts
@@ -4,7 +4,7 @@ import { mount } from '@vue/test-utils';
 test('should render nothing when content is empty string', () => {
   const wrapper = mount(Badge, {
     props: {
-      content: ''
+      value: ''
     }
   });
 
@@ -14,7 +14,7 @@ test('should render nothing when content is empty string', () => {
 test('should render nothing when content is undefined', () => {
   const wrapper = mount(Badge, {
     props: {
-      content: undefined
+      value: undefined
     }
   });
 
@@ -24,7 +24,7 @@ test('should render nothing when content is undefined', () => {
 test('should render nothing when content is zero', () => {
   const wrapper = mount(Badge, {
     props: {
-      content: 0
+      value: 0
     }
   });
 
@@ -34,7 +34,7 @@ test('should render nothing when content is zero', () => {
 test('should render content slot correctly', () => {
   const wrapper = mount(Badge, {
     slots: {
-      content: () => 'Custom Content'
+      default: () => 'Custom Content'
     }
   });
 
