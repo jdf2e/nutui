cb5db1efe772bffee6f01ad07ff73303e4b9ca4d
richard1015 <51844712@qq.com>
2022-03-22 18:50:28 +0800

    test: dialog add

---
+++ src/packages/__VUE/dialog/__tests__/function.spec.ts
@@ -0,0 +1,23 @@
+import DialogFunction from './dialog';
+
+function sleep(delay = 0): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+describe('function Dialog', () => {
+  test('show dialog info ', async () => {
+    console.log(DialogFunction);
+    DialogFunction({
+      title: 'title',
+      content: 'content'
+    });
+    await sleep(1000);
+    expect(document.querySelector('.nut-dialog')).toBeTruthy();
+    let textTitle = document.querySelector('.nut-dialog .nut-dialog__header') as HTMLDivElement;
+    expect(textTitle.innerHTML).toEqual('title');
+    let content = document.querySelector('.nut-dialog .nut-dialog__content view') as HTMLDivElement;
+    expect(content.innerHTML).toEqual('content');
+  });
+});
