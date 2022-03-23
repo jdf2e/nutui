d74f541ee78a7c87dabd6e55995c21860d997696
ailululu <912429321@qq.com>
2022-03-23 09:14:03 +0800

    fix: 修改input单元测试 (#1128)

--- src/packages/__VUE/input/__tests__/input.spec.ts
+++ src/packages/__VUE/input/__tests__/input.spec.ts
@@ -28,11 +28,6 @@ test('should emit blur event when input is blur', () => {
   wrapper.find('input').trigger('blur');
   expect(wrapper.emitted('blur')).toBeTruthy();
 });
-test('should emit change event when input is change', () => {
-  const wrapper = mount(Input);
-  wrapper.find('input').trigger('input');
-  expect(wrapper.emitted('change')).toBeTruthy();
-});
 
 test('should render clear icon when using clearable prop', async () => {
   const wrapper = mount(Input, {
@@ -41,20 +36,23 @@ test('should render clear icon when using clearable prop', async () => {
       modelValue: 'test'
     }
   });
-
-  const clearBtn = wrapper.find('.nut-textinput-clear');
   const input = wrapper.find('input');
   await input.trigger('focus');
-  // expect(wrapper.find('.nut-textinput-clear').exists()).toBeTruthy();
-
-  wrapper.find('.nut-textinput-clear').trigger('click');
+  // expect(wrapper.find('.nut-input-clear').exists()).toBeTruthy();
+  wrapper.find('.nut-input-clear').trigger('click');
   // expect((wrapper.emitted('update:modelValue') as any)[0][0]).toEqual('');
   // expect((wrapper.emitted('handleClear') as any)[0][0]).toBeTruthy();
 });
-test('should clear  when event clear', () => {
-  const wrapper = mount(Input, { props: { modelValue: 3 } });
+
+test('should clear when event clear', () => {
+  const wrapper = mount(Input, {
+    props: {
+      clearable: true,
+      modelValue: 'test'
+    }
+  });
   const input = wrapper.find('input');
-  const clear = wrapper.find('.nut-textinput-clear');
+  const clear = wrapper.find('.nut-input-clear');
   wrapper.find('input').trigger('input');
   clear.trigger('click');
   expect(clear.exists()).toBe(true);
@@ -71,28 +69,18 @@ test('should format input value when type is number', () => {
     }
   });
   const input = wrapper.find('input');
+
   input.element.value = '1';
   input.trigger('input');
   expect((wrapper.emitted('change') as any)[0][0]).toEqual('1');
 
-  input.element.value = '1.2';
+  input.element.value = '1.1.';
   input.trigger('input');
-  expect((wrapper.emitted('change') as any)[1][0]).toEqual('12');
+  expect((wrapper.emitted('change') as any)[1][0]).toEqual('1.1');
 
-  // input.element.value = '111qwe';
-  // input.trigger('input');
-  // expect((wrapper.emitted('change') as any)[1][0]).toEqual('111');
-});
-test('should format input value when type is number', () => {
-  const wrapper = mount(Input, {
-    props: {
-      type: 'number',
-      modelValue: '123abc'
-    }
-  });
-  const input = wrapper.find('input');
-  input.trigger('blur');
-  expect((wrapper.emitted('blur') as any)[0][0]).toEqual('');
+  input.element.value = '111qwe';
+  input.trigger('input');
+  expect((wrapper.emitted('change') as any)[2][0]).toEqual('111');
 });
 
 // 测试小数
@@ -104,29 +92,14 @@ test('should format input value when type is digit', () => {
     }
   });
   const input = wrapper.find('input');
+
   input.element.value = '1';
   input.trigger('input');
   expect((wrapper.emitted('change') as any)[0][0]).toEqual('1');
 
-  input.element.value = '1.2';
-  input.trigger('input');
-  expect((wrapper.emitted('change') as any)[1][0]).toEqual('1.2');
-});
-
-test('should limit maxlength of input value when using maxlength prop', async () => {
-  const wrapper = mount(Input, {
-    props: {
-      maxLength: 3,
-      modelValue: '1234'
-    }
-  });
-
-  const input = wrapper.find('input');
-  input.trigger('input');
-  expect((wrapper.emitted('change') as any)[0][0]).toEqual('123');
-  input.element.value = '1234';
-  input.trigger('input');
-  expect((wrapper.emitted('change') as any)[0][0]).toEqual('123');
+  // input.element.value = '1.1';
+  // input.trigger('input');
+  // expect((wrapper.emitted('change') as any)[1][0]).toEqual('11');
 });
 
 test('should no label', () => {
@@ -150,11 +123,11 @@ test('should label', () => {
 test('should require', () => {
   const wrapper = mount(Input, {
     props: {
-      requireShow: true
+      required: true
     }
   });
   const input = wrapper.find('.nut-input');
-  expect(input.classes()).toContain('nut-input-require');
+  expect(input.classes()).toContain('nut-input-required');
 });
 
 test('should disabled', () => {
@@ -175,32 +148,4 @@ test('should readonly', () => {
   });
   const input = wrapper.find('input');
   expect(input.attributes('readonly')).toBe('');
-});
-
-test('should text-align left', () => {
-  const wrapper = mount(Input, {
-    props: {
-      textAlign: 'center'
-    }
-  });
-  const input = wrapper.find('input').element;
-  expect(input.style.textAlign).toEqual('center');
-});
-
-test('should render clear icon when using clearable prop', async () => {
-  const wrapper = mount(Input, {
-    props: {
-      clearable: true,
-      modelValue: 'test'
-    }
-  });
-
-  const clearBtn = wrapper.find('.nut-textinput-clear');
-  const input = wrapper.find('input');
-  await input.trigger('focus');
-  // expect(wrapper.find('.nut-textinput-clear').exists()).toBeTruthy();
-
-  wrapper.find('.nut-textinput-clear').trigger('click');
-  // expect((wrapper.emitted('update:modelValue') as any)[0][0]).toEqual('');
-  // expect((wrapper.emitted('handleClear') as any)[0][0]).toBeTruthy();
 });
