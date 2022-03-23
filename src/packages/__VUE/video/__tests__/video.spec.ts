9d7bb0e537ce88f7d76391e23b6b879410b279b3
lzzwoniu <237622967@qq.com>
2022-03-23 09:15:51 +0800

    test(fixednav): add test (#1124)

---
+++ src/packages/__VUE/video/__tests__/video.spec.ts
@@ -0,0 +1,60 @@
+import { mount } from '@vue/test-utils';
+import Video from '../index.vue';
+
+describe('Video', () => {
+  test('base Video', () => {
+    const wrapper = mount(Video);
+    const rate = wrapper.find('.nut-video');
+    expect(rate.exists()).toBe(true);
+  });
+  test('should be displayed after setting the source src and type', () => {
+    const wrapper = mount(Video, {
+      props: {
+        source: {
+          src: 'xxx.mp4',
+          type: 'video/mp4'
+        },
+        options: {
+          controls: true
+        }
+      }
+    });
+    expect(wrapper.html()).toContain('src');
+    expect(wrapper.html()).toContain('type');
+    expect(wrapper.html()).toContain('controls');
+  });
+  test('should be displayed after setting the options autoplay and muted and loop', () => {
+    const wrapper = mount(Video, {
+      props: {
+        options: {
+          autoplay: true,
+          muted: true,
+          loop: true
+        }
+      }
+    });
+    expect(wrapper.html()).toContain('autoplay');
+    expect(wrapper.html()).toContain('muted');
+    expect(wrapper.html()).toContain('loop');
+  });
+  test('should be displayed after setting the options poster and playsinline', () => {
+    const wrapper = mount(Video, {
+      props: {
+        options: {
+          poster: 'xxx.png',
+          playsinline: true
+        }
+      }
+    });
+    expect(wrapper.html()).toContain('xxx.png');
+    expect(wrapper.html()).toContain('playsinline');
+  });
+  test('should be displayed after setting the click', () => {
+    const wrapper = mount(Video);
+    const _html1 = wrapper.find('.show-control');
+    expect(_html1.exists()).toBe(true);
+    wrapper.find('.control-play-btn').trigger('click');
+    const _html2 = wrapper.find('.hide-control');
+    expect(_html2.exists()).toBe(false);
+  });
+});
