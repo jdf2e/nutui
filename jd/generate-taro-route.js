afd047fd2d337573a97670b8d71097494421444c
yangxiaolu1993 <32215990+yangxiaolu1993@users.noreply.github.com>
2022-04-06 17:26:26 +0800

    feat: 根据config.json自动生成taroConfig.ts (#1181)
    * fix: countdown 组件修改 warning
    
    * feat: 自动创建 taro 文件脚本
    
    * feat: 自动更新 Taro

---
+++ jd/generate-taro-route.js
@@ -0,0 +1,47 @@
+const fse = require('fs-extra');
+const config = require('../src/config.json');
+const targetBaseUrl = `${process.cwd()}/src`;
+const taroConfig = `${targetBaseUrl}/sites/mobile-taro/vue/src/app.config.ts`;
+
+// 创建 config
+const createConfig = async () => {
+  let configRef = [];
+
+  return new Promise((res, rej) => {
+    config.nav.map((item) => {
+      let co = {
+        root: item.enName,
+        pages: []
+      };
+
+      item.packages.map((it) => {
+        if (!(it.exportEmpty == false) && it.show) {
+          co.pages.push(`pages/${it.name.toLowerCase()}/index`);
+        }
+      });
+
+      configRef.push(co);
+    });
+
+    res(configRef);
+  });
+};
+
+const create = async () => {
+  const configTemplate = {
+    pages: ['pages/index/index'],
+    subpackages: '',
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'NutUI',
+      navigationBarTextStyle: 'black'
+    }
+  };
+
+  configTemplate.subpackages = await createConfig();
+
+  fse.writeFileSync(taroConfig, `export default ${JSON.stringify(configTemplate)}`, 'utf8');
+};
+
+create();
