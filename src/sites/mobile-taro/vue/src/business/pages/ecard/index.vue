d936f0c56b675ebd4fc9c8afca547ee2d82317f1
yangxiaolu1993 <32215990+yangxiaolu1993@users.noreply.github.com>
2022-04-07 19:31:51 +0800

    feat: ecard、datepicker taro bug (#1184)
    * feat: ecard新增
    
    * fix: datepicker在Taro上更新

---
+++ src/sites/mobile-taro/vue/src/business/pages/ecard/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="demo">
+    <h2>基础用法</h2>
+    <nut-cell>
+      <nut-ecard
+        chooseText="请选择电子卡面值"
+        @inputChange="inputChange"
+        @change="change"
+        @changeStep="changeStep"
+        :data-list="dataList"
+        v-model="money"
+      ></nut-ecard>
+    </nut-cell>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, reactive, ref } from 'vue';
+export default defineComponent({
+  props: {},
+  setup() {
+    const dataList = reactive([
+      {
+        price: 10
+      },
+      {
+        price: 20
+      },
+      {
+        price: 30
+      },
+      {
+        price: 40
+      }
+    ]);
+    const money = ref(0);
+    const inputChange = (val: any) => {
+      money.value = val;
+    };
+    const change = (item: any) => {
+      money.value = item.price;
+    };
+    const changeStep = (num: any) => {
+      const val = money.value * num;
+      if (val > 100) {
+        money.value = val * 0.9;
+      } else {
+        money.value = val;
+      }
+    };
+    return {
+      dataList,
+      inputChange,
+      change,
+      money,
+      changeStep
+    };
+  }
+});
+</script>
