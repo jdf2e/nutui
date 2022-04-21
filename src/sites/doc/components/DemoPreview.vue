1250b7b51dbf47037b3e6d30a0302fc4c7030f5f
ailululu <912429321@qq.com>
2022-04-21 16:10:58 +0800

    fix: clearfix

--- src/sites/doc/components/DemoPreview.vue
+++ src/sites/doc/components/DemoPreview.vue
@@ -16,11 +16,13 @@ export default defineComponent({
 <style lang="scss">
 .doc {
   &-demo-preview {
-    height: 667px;
+    // height: 667px;
+    height: 637px;
     width: 375px;
     position: fixed;
     right: 30px;
     top: 100px;
+    // top: 135px;
     box-shadow: #ebedf0 0 4px 12px;
     border-radius: 12px;
     overflow: hidden;
