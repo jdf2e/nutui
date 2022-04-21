214a3b44e516bb23a88d37971ed33cd26a9a142c
Ymm0008 <being0008@163.com>
2022-04-21 09:27:08 +0800

    upd: notify 增加组件调用方式

--- src/packages/__VUE/notify/demo.vue
+++ src/packages/__VUE/notify/demo.vue
@@ -16,12 +16,19 @@
       <nut-cell is-Link @click="timeNotify('自定义时长')"> 自定义时长 </nut-cell>
       <nut-cell is-Link @click="positionNotify('自定义位置')"> 自定义位置 </nut-cell>
     </nut-cell-group>
+    <nut-cell-group title="组件调用">
+      <nut-cell is-Link @click="showNotify">组件调用</nut-cell>
+      <nut-notify v-model:visible="show">
+        <span>通知内容</span>
+      </nut-notify>
+    </nut-cell-group>
   </div>
 </template>
 
 <script lang="ts">
 import { createComponent } from '../../utils/create';
 import { Notify } from '../../nutui.vue';
+import { ref } from 'vue';
 const { createDemo } = createComponent('notify');
 export default createDemo({
   setup() {
@@ -36,7 +43,7 @@ export default createDemo({
       });
     };
     const primaryNotify = (msg: string) => {
-      Notify.primary(msg);
+      Notify.primary(msg, { duration: 1000000 });
     };
     const successNotify = (msg: string) => {
       Notify.success(msg);
@@ -56,6 +63,13 @@ export default createDemo({
     const positionNotify = (msg: string) => {
       Notify.text(msg, { position: 'bottom' });
     };
+    const show = ref(false);
+    const showNotify = () => {
+      show.value = true;
+      setTimeout(() => {
+        show.value = false;
+      }, 200000);
+    };
     return {
       baseNotify,
       primaryNotify,
@@ -64,7 +78,9 @@ export default createDemo({
       warningNotify,
       cusBgNotify,
       timeNotify,
-      positionNotify
+      positionNotify,
+      show,
+      showNotify
     };
   }
 });
