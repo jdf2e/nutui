3725af0d907c527d0a1d59683a782e62878cc952
suzigang <1039168735@qq.com>
2022-05-10 18:10:30 +0800

    docs(popup): add international en-us

--- src/packages/__VUE/popup/demo.vue
+++ src/packages/__VUE/popup/demo.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="demo">
-    <h2>基础用法</h2>
-    <nut-cell title="展示弹出层" is-link @click="state.showBasic = true"></nut-cell>
-    <nut-popup pop-class="popclass" :style="{ padding: '30px 50px' }" v-model:visible="state.showBasic" :z-index="100"
-      >正文</nut-popup
+    <h2>{{ translate('basic') }}</h2>
+    <nut-cell :title="translate('cell1')" is-link @click="state.showBasic = true"></nut-cell>
+    <nut-popup
+      pop-class="popclass"
+      :style="{ padding: '30px 50px' }"
+      v-model:visible="state.showBasic"
+      :z-index="100"
+      >{{ translate('text') }}</nut-popup
     >
-    <h2>弹出位置</h2>
-    <nut-cell title="顶部弹出" is-link @click="state.showTop = true"></nut-cell>
+    <h2>{{ translate('position') }}</h2>
+    <nut-cell :title="translate('cell2')" is-link @click="state.showTop = true"></nut-cell>
     <nut-popup position="top" :style="{ height: '20%' }" v-model:visible="state.showTop"></nut-popup>
-    <nut-cell title="底部弹出" is-link @click="state.showBottom = true"></nut-cell>
+    <nut-cell :title="translate('cell3')" is-link @click="state.showBottom = true"></nut-cell>
     <nut-popup position="bottom" :style="{ height: '20%' }" v-model:visible="state.showBottom"></nut-popup>
-    <nut-cell title="左侧弹出" is-link @click="state.showLeft = true"></nut-cell>
+    <nut-cell :title="translate('cell4')" is-link @click="state.showLeft = true"></nut-cell>
     <nut-popup position="left" :style="{ width: '20%', height: '100%' }" v-model:visible="state.showLeft"></nut-popup>
-    <nut-cell title="右侧弹出" is-link @click="state.showRight = true"></nut-cell>
+    <nut-cell :title="translate('cell5')" is-link @click="state.showRight = true"></nut-cell>
     <nut-popup position="right" :style="{ width: '20%', height: '100%' }" v-model:visible="state.showRight"></nut-popup>
-    <h2>关闭图标</h2>
-    <nut-cell title="关闭图标" is-link @click="state.showIcon = true"></nut-cell>
+    <h2>{{ translate('close') }}</h2>
+    <nut-cell :title="translate('close')" is-link @click="state.showIcon = true"></nut-cell>
     <nut-popup position="bottom" closeable :style="{ height: '20%' }" v-model:visible="state.showIcon"></nut-popup>
-    <nut-cell title="图标位置" is-link @click="state.showIconPosition = true"></nut-cell>
+    <nut-cell :title="translate('iposition')" is-link @click="state.showIconPosition = true"></nut-cell>
     <nut-popup
       position="bottom"
       closeable
@@ -25,7 +29,7 @@
       :style="{ height: '20%' }"
       v-model:visible="state.showIconPosition"
     ></nut-popup>
-    <nut-cell title="自定义图标" is-link @click="state.showCloseIcon = true"></nut-cell>
+    <nut-cell :title="translate('cicon')" is-link @click="state.showCloseIcon = true"></nut-cell>
     <nut-popup
       position="bottom"
       closeable
@@ -34,8 +38,8 @@
       :style="{ height: '20%' }"
       v-model:visible="state.showCloseIcon"
     ></nut-popup>
-    <h2>圆角弹框</h2>
-    <nut-cell title="圆角弹框" is-link @click="state.showRound = true"></nut-cell>
+    <h2>{{ translate('circle') }}</h2>
+    <nut-cell :title="translate('circle')" is-link @click="state.showRound = true"></nut-cell>
     <nut-popup
       position="bottom"
       closeable
@@ -43,8 +47,8 @@
       :style="{ height: '30%' }"
       v-model:visible="state.showRound"
     ></nut-popup>
-    <h2>指定挂载节点</h2>
-    <nut-cell title="指定挂载节点" is-link @click="state.showTeleport = true"></nut-cell>
+    <h2>{{ translate('teleport') }}</h2>
+    <nut-cell :title="translate('teleport')" is-link @click="state.showTeleport = true"></nut-cell>
     <nut-popup :style="{ padding: '30px 50px' }" teleport="#app" v-model:visible="state.showTeleport">app</nut-popup>
   </div>
 </template>
@@ -52,7 +56,40 @@
 <script lang="ts">
 import { reactive } from 'vue';
 import { createComponent } from '@/packages/utils/create';
-const { createDemo } = createComponent('popup');
+const { createDemo, translate } = createComponent('popup');
+import { useTranslate } from '@/sites/assets/util/useTranslate';
+useTranslate({
+  'zh-CN': {
+    basic: '基本用法',
+    cell1: '展示弹出层',
+    text: '正文',
+    position: '弹出位置',
+    cell2: '顶部弹出',
+    cell3: '底部弹出',
+    cell4: '左侧弹出',
+    cell5: '右侧弹出',
+    close: '关闭图标',
+    iposition: '图标位置',
+    cicon: '自定义图标',
+    circle: '圆角弹框',
+    teleport: '指定挂载节点'
+  },
+  'en-US': {
+    basic: 'Basic Usage',
+    cell1: 'Show pop-up layer',
+    text: 'text',
+    position: 'Eject position',
+    cell2: 'Top pop-up',
+    cell3: 'Bottom pop-up',
+    cell4: 'Left pop-up',
+    cell5: 'Right pop-up',
+    close: 'Close icon',
+    iposition: 'Icon location',
+    cicon: 'Custom icon',
+    circle: 'Rounded bullet frame',
+    teleport: 'Specify mount node'
+  }
+});
 export default createDemo({
   props: {},
   setup() {
@@ -68,7 +105,7 @@ export default createDemo({
       showRound: false,
       showCombination: false
     });
-    return { state };
+    return { state, translate };
   }
 });
 </script>
