df6d68ddfdbc3e178ff39bd10e87c0406041a9f3
mushroomli <15733267361@163.com>
2022-06-23 14:05:32 +0800

    feat(animate): ug (#1398)
    * feat:动效
    
    * feat: 单元测试demo国际化
    
    Co-authored-by: lijiamiao3 <lijiamiao3@jd.com>
    Co-authored-by: richard1015 <51844712@qq.com>

---
+++ src/packages/__VUE/animate/type.ts
@@ -0,0 +1,2 @@
+export type AnimateType = 'shake' | 'ripple' | 'breath' | 'float' | 'slide-right' | 'slide-left' | 'slide-top' | 'slide-bottom' | 'jump' | 'twinkle' | 'flicker';
+export type AnimateAction = 'initial' | 'click';
