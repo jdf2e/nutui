a756f05d5e579e25d9bd6dac605514021bbe6dba
yangxiaolu3 <962468124@qq.com>
2022-11-24 11:47:58 +0800

    feat: 组件 Popover 重构

--- src/packages/__VUE/popover/index.vue
+++ src/packages/__VUE/popover/index.vue
@@ -2,78 +2,63 @@
   <view :class="classes">
     <view class="nut-popover-wrapper" @click="openPopover" ref="popoverRef"><slot name="reference"></slot></view>
 
-    <!-- <Teleport :to="teleport" > -->
-    <Transition name="nut-popover">
-      <view :class="popoverContent" :style="getStyles" ref="contentRef" v-if="showPopup">
-        <!-- 气泡弹出层  箭头 -->
-        <view :class="popoverArrow" v-if="showArrow"> </view>
-        <slot name="content"></slot>
-        <view
-          v-for="(item, index) in list"
-          :key="index"
-          :class="[item.className, item.disabled && 'nut-popover-menu-disabled', 'nut-popover-menu-item']"
-          @click.stop="chooseItem(item, index)"
-        >
-          <slot v-if="item.icon"> <nut-icon v-bind="$attrs" class="item-img" :name="item.icon"></nut-icon></slot>
-          <view class="nut-popover-menu-name">{{ item.name }}</view>
-        </view>
+    <nut-popup
+      :popClass="popoverContent"
+      :style="getStyles"
+      v-model:visible="showPopup"
+      position=""
+      transition="nut-popover"
+      :overlay="overlay"
+      :duration="duration"
+      :overlayStyle="overlayStyle"
+      :overlayClass="overlayClass"
+      :closeOnClickOverlay="closeOnClickOverlay"
+      ref="contentRef"
+    >
+      <!-- 气泡弹出层  箭头 -->
+      <view :class="popoverArrow" v-if="showArrow"> </view>
+      <slot name="content"></slot>
+      <view
+        v-for="(item, index) in list"
+        :key="index"
+        :class="[item.className, item.disabled && 'nut-popover-menu-disabled', 'nut-popover-menu-item']"
+        @click.stop="chooseItem(item, index)"
+      >
+        <slot v-if="item.icon">
+          <nut-icon v-bind="$attrs" class="item-img" :classPrefix="iconPrefix" :name="item.icon"></nut-icon
+        ></slot>
+        <view class="nut-popover-menu-name">{{ item.name }}</view>
       </view>
-    </Transition>
-    <!-- </Teleport> -->
+    </nut-popup>
   </view>
 </template>
 <script lang="ts">
 import { computed, watch, ref, PropType, toRefs, CSSProperties, reactive, onMounted, onUnmounted } from 'vue';
 import { createComponent } from '@/packages/utils/create';
+import Popup from '../popup/index.vue';
 const { componentName, create } = createComponent('popover');
 
 export default create({
   inheritAttrs: false,
-  components: {},
+  components: {
+    [Popup.name]: Popup
+  },
   props: {
-    visible: {
-      type: Boolean,
-      default: false
-    },
-    list: {
-      type: Array,
-      default: []
-    },
-
-    theme: {
-      type: String as PropType<import('./type').PopoverTheme>,
-      default: 'light'
-    },
-
-    location: {
-      type: String as PropType<import('./type').PopoverLocation>,
-      default: 'bottom'
-    },
-    // 位置出现的偏移量
-    offset: {
-      type: Array,
-      default: [0, 12]
-    },
-    customClass: {
-      type: String,
-      default: ''
-    },
-    showArrow: {
-      type: Boolean,
-      default: true
-    },
-    closeOnClickAction: {
-      type: Boolean,
-      default: true
-    },
-    closeOnClickOutside: {
-      type: Boolean,
-      default: true
-    },
-    teleport: {
-      type: [String, Element],
-      default: 'body'
-    }
+    visible: { type: Boolean, default: false },
+    list: { type: Array, default: [] },
+    theme: { type: String as PropType<import('./type').PopoverTheme>, default: 'light' },
+    location: { type: String as PropType<import('./type').PopoverLocation>, default: 'bottom' },
+    offset: { type: Array, default: [0, 12] },
+    customClass: { type: String, default: '' },
+    showArrow: { type: Boolean, default: true },
+    iconPrefix: { type: String, default: 'nut-icon' },
+    duration: { type: [Number, String], default: 0.3 },
+    overlay: { type: Boolean, default: false },
+    overlayClass: { type: String, default: '' },
+    overlayStyle: { type: Object as PropType<CSSProperties> },
+    closeOnClickOverlay: { type: Boolean, default: true },
+    closeOnClickAction: { type: Boolean, default: true },
+    closeOnClickOutside: { type: Boolean, default: true }
   },
   emits: ['update', 'update:visible', 'close', 'choose', 'open'],
   setup(props, { emit }) {
@@ -91,15 +76,12 @@ export default create({
     const classes = computed(() => {
       const prefixCls = componentName;
       const { theme, customClass } = props;
-      return `${prefixCls} ${prefixCls}--${theme} ${customClass}`;
+      return `${prefixCls} ${prefixCls}--${theme} ${customClass} `;
     });
 
     const popoverContent = computed(() => {
       const prefixCls = 'nut-popover-content';
-      return {
-        [prefixCls]: true,
-        [`${prefixCls}--${location.value}`]: location.value
-      };
+      return `${prefixCls} ${prefixCls}--${location.value}`;
     });
 
     const popoverArrow = computed(() => {
@@ -174,7 +156,7 @@ export default create({
 
     const clickAway = (event: Event) => {
       const element = popoverRef.value;
-      const elContent = contentRef.value;
+      const elContent = contentRef.value?.popupRef;
       if (
         element &&
         !element.contains(event.target) &&
