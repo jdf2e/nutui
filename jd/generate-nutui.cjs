24244777ec6dd1ec70fd1a6d1558f91da4d8c306
richard1015 <51844712@qq.com>
2022-11-29 12:35:00 +0800

    chore: del cz-git

--- jd/generate-nutui.cjs
+++ jd/generate-nutui.cjs
@@ -1,10 +1,7 @@
-import packageConfig from '../package.json' assert { type: 'json' };
-import config from '../src/config.json' assert { type: 'json' };
-import { fileURLToPath } from 'url';
-import { resolve, dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-import { outputFile } from 'fs-extra';
+const packageConfig = require('../package.json');
+const config = require('../src/config.json');
+const path = require('path');
+const fs = require('fs-extra');
 let importStr = `import { App } from 'vue';
 import Locale from './locale';\n`;
 let importScssStr = `\n`;
@@ -33,7 +30,7 @@ const version = '${packageConfig.version}';
 export { install, version, Locale, ${packages.join(',')}};
 export default { install, version, Locale};`;
 
-outputFile(resolve(__dirname, '../src/packages/nutui.vue.build.ts'), fileStrBuild, 'utf8', (error) => {
+fs.outputFile(path.resolve(__dirname, '../src/packages/nutui.vue.build.ts'), fileStrBuild, 'utf8', (error) => {
   // logger.success(`${package_config_path} 文件写入成功`);
 });
 
@@ -43,6 +40,6 @@ ${importScssStr}
 export const testComponents = { ${packages.join(',')}};
 export { install, Locale, ${packages.join(',')}  };
 export default { install, version:'${packageConfig.version}', Locale};`;
-outputFile(resolve(__dirname, '../src/packages/nutui.vue.ts'), fileStrDev, 'utf8', (error) => {
+fs.outputFile(path.resolve(__dirname, '../src/packages/nutui.vue.ts'), fileStrDev, 'utf8', (error) => {
   // logger.success(`${package_config_path} 文件写入成功`);
 });
