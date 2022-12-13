12a6a31bc9ffb0afb3a32a3fc69c53ff3867d0c4
eiinu <eiinu@qq.com>
2022-12-13 16:47:23 +0800

    chore: numberkeyboard 使用名称变更

--- src/packages/__VUE/numberkeyboard/index.vue
+++ src/packages/__VUE/numberkeyboard/index.vue
@@ -80,7 +80,7 @@
 import { computed, onMounted, provide, reactive, nextTick, ref, watch, Ref } from 'vue';
 import { createComponent } from '@/packages/utils/create';
 import Popup from '../popup/index.vue';
-const { create, translate } = createComponent('numberkeyboard');
+const { create, translate } = createComponent('numberkey-board');
 export interface keys {
   id: number | string;
   type: string;
