4c4e72443d6758999e4f10d1b550b4ab92e32ed4
谭海祥 <haixiang.tan@sfmail.sf-express.com>
2022-12-14 16:33:31 +0800

    chore(tabs): use icons-vue component

--- src/packages/__VUE/tabs/index.vue
+++ src/packages/__VUE/tabs/index.vue
@@ -3,10 +3,12 @@
 import { createComponent } from '@/packages/utils/create';
 import { component } from './common';
 import Sticky from '../sticky/index.vue';
+import { JoySmile } from '@nutui/icons-vue';
 const { create } = createComponent('tabs');
 export default create(
   component({
-    [Sticky.name]: Sticky
+    [Sticky.name]: Sticky,
+    JoySmile
   })
 );
 </script>
