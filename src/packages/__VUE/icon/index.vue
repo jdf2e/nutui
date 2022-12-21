36b12bb5e3dfbebcbdb1f2b6c5193615076c8323
richard1015 <51844712@qq.com>
2022-12-21 16:31:03 +0800

    fix(icon): taro h5差异bug修复

--- src/packages/__VUE/icon/index.vue
+++ src/packages/__VUE/icon/index.vue
@@ -3,3 +3,8 @@ import { createComponent } from '@/packages/utils/create';
 const { create } = createComponent('icon');
 export default create({});
 </script>
+<style>
+.nut-icon {
+  scale: 0.8;
+}
+</style>
