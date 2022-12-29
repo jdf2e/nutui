011c3727736b256af002dced82cdd481f3eb9a91
richard1015 <51844712@qq.com>
2022-12-29 20:02:34 +0800

    release: 4.0.0-beta.7

--- vite.config.build.css.ts
+++ vite.config.build.css.ts
@@ -1,11 +1,4 @@
 import { defineConfig } from 'vite';
-import config from './package.json';
-
-const banner = `/*!
-* ${config.name} v${config.version} ${new Date()}
-* (c) 2022 @jdf2e.
-* Released under the MIT License.
-*/`;
 
 export default defineConfig({
   build: {
