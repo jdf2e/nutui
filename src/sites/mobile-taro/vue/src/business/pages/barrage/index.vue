2e509028cfd5e87903d10542bce037ea1f787750
Ymm0008 <being0008@163.com>
2022-12-29 14:59:30 +0800

    fix: barrage 小程序不展示问题处理

--- src/sites/mobile-taro/vue/src/business/pages/barrage/index.vue
+++ src/sites/mobile-taro/vue/src/business/pages/barrage/index.vue
@@ -4,15 +4,6 @@
     <nut-cell>
       <nut-barrage ref="danmu" :danmu="list"></nut-barrage>
     </nut-cell>
-    <!-- <h2>slot 用法</h2>
-    <nut-cell>
-      <nut-barrage>
-        <span>aaa</span>
-        <span>bbb</span>
-        <span>ccc</span>
-        <span>ddd</span>
-      </nut-barrage>
-    </nut-cell> -->
     <div class="test">
       <button @click="addDanmu" class="add nut-button--primary">随机添加</button>
     </div>
@@ -26,7 +17,7 @@ export default {
   setup() {
     const inputVal = ref<any>('');
     const danmu = ref<any>(null);
-    let list = ref(['画美不看画美不看画美不看画美不看', '不明觉厉', '喜大普奔', '男默女泪', '累觉不爱', '爷青结']);
+    let list = ref(['画美不看', '不明觉厉', '喜大普奔', '男默女泪', '累觉不爱', '爷青结']);
     function addDanmu() {
       let n = Math.random();
       danmu.value.add('随机——' + String(n).substr(2, 10));
@@ -46,15 +37,5 @@ export default {
 .nut-barrage {
   padding: 20px 0;
   height: 150px;
-}
-
-.test {
-  .add {
-    display: block;
-    padding: 5px 30px;
-    margin: 20px auto;
-    border-radius: 15px;
-    font-size: 12px;
-  }
 }
 </style>
