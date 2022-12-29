72c8ee8669237b36f0680d5582e3546bdadbdda9
richard1015 <51844712@qq.com>
2022-12-29 18:52:28 +0800

    chore: split publish pkg nutui & taro

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import config from './package.json';
 import autoprefixer from 'autoprefixer';
 const banner = `/*!
-* ${config.name} v${config.version} ${new Date()}
+* @nutui/nutui v${config.version} ${new Date()}
 * (c) 2022 @jdf2e.
 * Released under the MIT License.
 */`;
