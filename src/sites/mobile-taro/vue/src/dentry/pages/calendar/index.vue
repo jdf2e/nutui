a44ed20b07f651ca004de5e9a95f4ce21d29db17
lkjh3214 <13121007159@163.com>
2023-01-17 16:31:27 +0800

    fix: 样式修改

--- src/sites/mobile-taro/vue/src/dentry/pages/calendar/index.vue
+++ src/sites/mobile-taro/vue/src/dentry/pages/calendar/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="demo" :class="{ web: env === 'WEB' }">
+  <div class="demo calendar_demo" :class="{ web: env === 'WEB' }">
     <Header v-if="env === 'WEB'" />
     <h2>基础用法</h2>
     <div>
@@ -342,7 +342,7 @@ export default {
 </script>
 
 <style lang="scss">
-.demo {
+.calendar_demo {
   .nut-cell__value {
     flex: initial;
   }
