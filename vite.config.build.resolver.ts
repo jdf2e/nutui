4e6f3a3566649c251f6a454bc0755fa1eb6ca9f2
eiinu <eiinu@qq.com>
2023-02-08 15:18:21 +0800

    feat: 增加 unplugin 插件配置导出

---
+++ vite.config.build.resolver.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    minify: true,
+    lib: {
+      entry: path.resolve(__dirname, './src/packages/resolver/index.ts'),
+      name: 'name',
+      fileName: (format) => {
+        if (format === 'es') {
+          return 'index.mjs';
+        } else {
+          return 'index.js';
+        }
+      },
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      output: {
+        dir: path.resolve(__dirname, './dist/resolver')
+      }
+    },
+    emptyOutDir: false
+  }
+});
