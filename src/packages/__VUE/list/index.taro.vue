86986eac233f74b9cb37bbba7f3db61107f4fa87
suzigang <1039168735@qq.com>
2023-02-09 17:37:04 +0800

    fix(list v3): 虚拟列表在ios手机上，滚动条滚动到顶部会出现白屏。 #2031

--- src/packages/__VUE/list/index.taro.vue
+++ src/packages/__VUE/list/index.taro.vue
@@ -84,7 +84,7 @@ export default create({
     });
 
     const handleScrollEvent = async (e: any) => {
-      const scrollTop = e.detail ? e.detail.scrollTop : e.target.scrollTop;
+      const scrollTop = Math.max(e.detail ? e.detail.scrollTop : e.target.scrollTop, 0.1);
       state.start = Math.floor(scrollTop / props.height);
       if (end.value > state.list.length) {
         emit('scroll');
