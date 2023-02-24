6172a3e334a0db64948a7e77da7d2b7842c5ccc7
Drjingfubo <50685476+Drjingfubo@users.noreply.github.com>
2023-02-24 10:50:47 +0800

    fix(form): modify validation rules (#2134)

--- src/packages/__VUE/form/common.ts
+++ src/packages/__VUE/form/common.ts
@@ -98,7 +98,7 @@ export const component = {
         const { required, regex, message } = ruleWithoutValidator;
         const errorMsg = { prop, message };
         if (required) {
-          if (!value) {
+          if (!value && value !== 0) {
             return _Promise(errorMsg);
           }
         }
