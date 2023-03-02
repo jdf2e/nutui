902046e912d71a66e944f5b4bcfd13483befd7b3
胡绍同 <1476403073@qq.com>
2023-03-02 15:58:54 +0800

    fix: list 虚拟列表 列表少渲染一个item

--- src/packages/__VUE/list/__tests__/list.spec.ts
+++ src/packages/__VUE/list/__tests__/list.spec.ts
@@ -32,3 +32,17 @@ test('should render height', async () => {
   const listItem = wrapper.findAll('.nut-list-item');
   expect(listItem.length).toBe(visibleCount);
 });
+
+test('should render actual length', async () => {
+  const wrapper = mount(List, {
+    props: {
+      height: 50,
+      listData: new Array(3).fill(0)
+    }
+  });
+
+  await nextTick();
+
+  const listItem = wrapper.findAll('.nut-list-item');
+  expect(listItem.length).toEqual(100);
+});
