a9f63f74880bb5ea20e26acb86c013ee97c9dd55
Eiinu <eiinu@qq.com>
2023-06-06 17:42:32 +0800

    test(configprovider): update test case

--- src/packages/__VUE/configprovider/__tests__/configprovider.spec.ts
+++ src/packages/__VUE/configprovider/__tests__/configprovider.spec.ts
@@ -3,7 +3,7 @@ import ConfigProvider from '../index.vue';
 import { Close } from '@nutui/icons-vue';
 import { h } from 'vue';
 
-test('prop theme & tag', async () => {
+test('ConfigProvider: props theme & tag', async () => {
   const wrapper = mount(ConfigProvider, {
     props: {
       theme: 'dark',
@@ -13,17 +13,18 @@ test('prop theme & tag', async () => {
   expect(wrapper.find('div.nut-theme-dark').exists()).toBeTruthy();
 });
 
-test('ConfigProvider prop: fontClassName & classPrefix', async () => {
+test('ConfigProvider: props themeVars', async () => {
   const wrapper = mount(ConfigProvider, {
     props: {
-      theme: 'dark',
-      tag: 'div',
-      fontClassName: 'iconfont',
-      classPrefix: 'icon'
+      themeVars: {
+        primaryColor: '#fff',
+        titleColor: 'blue',
+        helpColor: '#f5f5f5'
+      }
     },
     slots: {
       default: h(Close)
     }
   });
-  expect(wrapper.find('.nut-icon').exists()).toBeTruthy();
+  expect(wrapper.html()).toMatchSnapshot();
 });
