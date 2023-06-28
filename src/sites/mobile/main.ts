a489e2e2d3478543560c107daaf017ad5704eaae
Eiinu <eiinu@qq.com>
2023-06-28 22:58:08 +0800

    chore: bump to taro 3.6.8

--- src/sites/mobile/main.ts
+++ src/sites/mobile/main.ts
@@ -2,5 +2,5 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import NutUI from '@/packages/nutui.vue';
-import '@/packages/utils/touchEmulator';
+import '@nutui/touch-emulator';
 createApp(App).use(router).use(NutUI).mount('#app');
