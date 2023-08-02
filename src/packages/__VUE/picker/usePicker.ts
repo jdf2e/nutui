db9ef40331461560101a8488c0d6be4045bf75ff
bordon <1078464063@qq.com>
2023-08-02 18:23:09 +0800

    feat(picker): update

--- src/packages/__VUE/picker/usePicker.ts
+++ src/packages/__VUE/picker/usePicker.ts
@@ -40,21 +40,21 @@ export const usePicker = (props: any, emit: any) => {
   });
 
   const selectedOptions = computed(() => {
+    const fields = columnFieldNames.value;
     return (columnsList.value as PickerOption[][]).map((column: PickerOption[], index: number) => {
-      return column.find((item) => item[columnFieldNames.value.value] === defaultValues.value[index]);
+      return column.find((item) => item[fields.value] === defaultValues.value[index]);
     });
   });
-
-  const childrenField = computed(() => columnFieldNames.value.children);
 
   // 当前类型
   const columnsType = computed(() => {
     const firstColumn: PickerOption | PickerOption[] = state.formattedColumns[0];
+    const fields = columnFieldNames.value;
     if (firstColumn) {
       if (Array.isArray(firstColumn)) {
         return 'multiple';
       }
-      if (childrenField.value in firstColumn) {
+      if (fields.children in firstColumn) {
         return 'cascade';
       }
     }
@@ -84,20 +84,21 @@ export const usePicker = (props: any, emit: any) => {
   // 级联数据格式化
   const formatCascade = (columns: PickerOption[], defaultValues: (number | string)[]) => {
     const formatted: PickerOption[][] = [];
+    const fields = columnFieldNames.value;
     let cursor: PickerOption = {
       text: '',
       value: '',
-      [childrenField.value]: columns
+      [fields.children]: columns
     };
 
     let columnIndex = 0;
 
-    while (cursor && cursor[childrenField.value]) {
-      const options: PickerOption[] = cursor[childrenField.value];
+    while (cursor && cursor[fields.children]) {
+      const options: PickerOption[] = cursor[fields.children];
       const value = defaultValues[columnIndex];
       let index = options.findIndex((columnItem) => columnItem.value === value);
       if (index === -1) index = 0;
-      cursor = cursor[childrenField.value][index];
+      cursor = cursor[fields.children][index];
 
       columnIndex++;
       formatted.push(options);
@@ -113,26 +114,27 @@ export const usePicker = (props: any, emit: any) => {
   };
 
   const changeHandler = (columnIndex: number, option: PickerOption) => {
+    const fields = columnFieldNames.value;
     if (option && Object.keys(option).length) {
       defaultValues.value = defaultValues.value ? defaultValues.value : [];
 
       if (columnsType.value === 'cascade') {
-        defaultValues.value[columnIndex] = option[columnFieldNames.value.value] ?? '';
+        defaultValues.value[columnIndex] = option[fields.value] ?? '';
         let index = columnIndex;
         let cursor = option;
-        while (cursor && cursor[childrenField.value] && cursor[childrenField.value][0]) {
-          defaultValues.value[index + 1] = cursor[childrenField.value][0].value;
+        while (cursor && cursor[fields.children] && cursor[fields.children][0]) {
+          defaultValues.value[index + 1] = cursor[fields.children][0].value;
           index++;
-          cursor = cursor[childrenField.value][0];
+          cursor = cursor[fields.children][0];
         }
 
         // 当前改变列 的 下一列 children 值为空
-        if (cursor && cursor[childrenField.value] && cursor[childrenField.value].length === 0) {
+        if (cursor && cursor[fields.children] && cursor[fields.children].length === 0) {
           defaultValues.value = defaultValues.value.slice(0, index + 1);
         }
       } else {
-        defaultValues.value[columnIndex] = Object.prototype.hasOwnProperty.call(option, columnFieldNames.value.value)
-          ? option[columnFieldNames.value.value]
+        defaultValues.value[columnIndex] = Object.prototype.hasOwnProperty.call(option, fields.value)
+          ? option[fields.value]
           : '';
       }
 
@@ -145,9 +147,10 @@ export const usePicker = (props: any, emit: any) => {
   };
 
   const confirm = () => {
+    const fields = columnFieldNames.value;
     if (defaultValues.value && !defaultValues.value.length) {
       columnsList.value.forEach((columns) => {
-        defaultValues.value.push(columns[0][columnFieldNames.value.value]);
+        defaultValues.value.push(columns[0][fields.value]);
       });
     }
 
