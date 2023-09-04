1f9092266358c01cc064f903fcfb38e90d1d50df
bordon <1078464063@qq.com>
2023-09-04 10:41:42 +0800

    feat: update

--- src/packages/__VUE/configprovider/common.ts
+++ src/packages/__VUE/configprovider/common.ts
@@ -1,52 +1,80 @@
-export const hexToRgb = (color: string) => {
-  //十六进制颜色值的正则表达式
-  const reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
-  if (!reg.test(color)) return '';
-  color = color.toLowerCase();
-  // 如果是16进制颜色
-  if (color.length === 4) {
-    color =
-      '#' +
-      color
-        .slice(1)
-        .split('')
-        .map((char) => char + char)
-        .join('');
-  }
-  //处理六位的颜色值
-  const colors = [];
-  for (let i = 1; i < 7; i += 2) {
-    colors.push(parseInt('0x' + color.slice(i, i + 2)));
-  }
-  return colors.join(',');
-};
+import { h, computed } from 'vue';
 
-export const kebabCase = (str: string): string => {
-  str = str.replace(str.charAt(0), str.charAt(0).toLocaleLowerCase());
-  return str.replace(/([a-z])([A-Z])/g, (_, p1, p2) => p1 + '-' + p2.toLowerCase());
-};
+export const component = (tag: string) => {
+  return {
+    props: {
+      theme: { type: String, default: '' },
+      themeVars: { type: Object, default: {} },
+      tag: { type: String, default: tag }
+    },
 
-export const mapThemeVarsToCSSVars = (themeVars: Record<string, string>) => {
-  if (!themeVars) return;
-  const cssVars: Record<string, string> = {};
-  const primaryColor = themeVars?.primaryColor;
-  // 为了处理一些组件的rgba透明颜色
-  if (primaryColor) {
-    const primaryColorRgb = hexToRgb(primaryColor);
-    cssVars[
-      '--nut-address-region-tab-line'
-    ] = `linear-gradient(90deg, ${primaryColor} 0%, rgba(${primaryColorRgb}, 0.15) 100%)`;
-    cssVars[
-      '--nut-tabs-horizontal-tab-line-color'
-    ] = `linear-gradient(90deg, ${primaryColor} 0%, rgba(${primaryColorRgb}, 0.15) 100%)`;
-    cssVars[
-      '--nut-tabs-vertical-tab-line-color'
-    ] = `linear-gradient(180deg, ${primaryColor} 0%, rgba(${primaryColorRgb}, 0.15) 100%)`;
-  }
+    setup(props: any, { slots }: any) {
+      const hexToRgb = (color: string) => {
+        //十六进制颜色值的正则表达式
+        const reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+        if (!reg.test(color)) return '';
+        color = color.toLowerCase();
+        // 如果是16进制颜色
+        if (color.length === 4) {
+          color =
+            '#' +
+            color
+              .slice(1)
+              .split('')
+              .map((char) => char + char)
+              .join('');
+        }
+        //处理六位的颜色值
+        const colors = [];
+        for (let i = 1; i < 7; i += 2) {
+          colors.push(parseInt('0x' + color.slice(i, i + 2)));
+        }
+        return colors.join(',');
+      };
 
-  Object.keys(themeVars).forEach((key) => {
-    cssVars[`--nut-${kebabCase(key)}`] = themeVars[key];
-  });
+      const kebabCase = (str: string): string => {
+        str = str.replace(str.charAt(0), str.charAt(0).toLocaleLowerCase());
+        return str.replace(/([a-z])([A-Z])/g, (_, p1, p2) => p1 + '-' + p2.toLowerCase());
+      };
 
-  return cssVars;
+      const mapThemeVarsToCSSVars = (themeVars: Record<string, string>) => {
+        if (!themeVars) return;
+        const cssVars: Record<string, string> = {};
+        const primaryColor = themeVars?.primaryColor;
+        // 为了处理一些组件的rgba透明颜色
+        if (primaryColor) {
+          const primaryColorRgb = hexToRgb(primaryColor);
+          cssVars[
+            '--nut-address-region-tab-line'
+          ] = `linear-gradient(90deg, ${primaryColor} 0%, rgba(${primaryColorRgb}, 0.15) 100%)`;
+          cssVars[
+            '--nut-tabs-horizontal-tab-line-color'
+          ] = `linear-gradient(90deg, ${primaryColor} 0%, rgba(${primaryColorRgb}, 0.15) 100%)`;
+          cssVars[
+            '--nut-tabs-vertical-tab-line-color'
+          ] = `linear-gradient(180deg, ${primaryColor} 0%, rgba(${primaryColorRgb}, 0.15) 100%)`;
+        }
+
+        Object.keys(themeVars).forEach((key) => {
+          cssVars[`--nut-${kebabCase(key)}`] = themeVars[key];
+        });
+
+        return cssVars;
+      };
+
+      const themeStyle = computed(() => mapThemeVarsToCSSVars(props.themeVars));
+      const defaultSlots = slots.default?.();
+
+      return () => {
+        return h(
+          props.tag,
+          {
+            class: `nut-theme-${props.theme}`,
+            style: themeStyle.value
+          },
+          defaultSlots
+        );
+      };
+    }
+  };
 };
--- src/packages/__VUE/configprovider/index.vue
+++ src/packages/__VUE/configprovider/index.vue
@@ -1,29 +1,8 @@
 <script lang="ts">
-import { h } from 'vue';
 import { createComponent } from '@/packages/utils/create';
-import { mapThemeVarsToCSSVars } from './common';
+import { component } from './common';
 
 const { create } = createComponent('config-provider');
 
-export default create({
-  props: {
-    theme: { type: String, default: '' },
-    themeVars: { type: Object, default: {} },
-    tag: { type: String, default: 'div' }
-  },
-
-  setup(props: any, { slots }: any) {
-    return () => {
-      const defaultSlots = slots.default?.();
-      return h(
-        props.tag,
-        {
-          class: `nut-theme-${props.theme}`,
-          style: mapThemeVarsToCSSVars(props.themeVars)
-        },
-        defaultSlots
-      );
-    };
-  }
-});
+export default create(component('div'));
 </script>
