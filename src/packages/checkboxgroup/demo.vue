0b9c3aaa160476bc92a94fc0492de9b96604a4a5
liuwei1025 <we1025@qq.com>
2023-09-18 13:55:13 +0800

    Refactor/checkboxgroup (#2569)
    * fix: 处理group因为在render中未引用value导致value更新后rende不执行的问题
    
    * chore: 还原示例

--- src/packages/checkboxgroup/demo.vue
+++ src/packages/checkboxgroup/demo.vue
@@ -84,34 +84,34 @@ export default {
         { id: 11, value: '选项A', label: '选项A' },
         { id: 12, value: '选项B', label: '选项B' },
         { id: 13, value: '选项C', label: '选项C' },
-        { id: 14, value: '选项D', label: '选项D' }
+        { id: 14, value: '选项D', label: '选项D' },
       ],
       data2: [
         { id: 21, value: '选项1', label: '选项1', disabled: true },
-        { id: 22, value: '选项2', label: '选项2', disabled: true }
+        { id: 22, value: '选项2', label: '选项2', disabled: true },
       ],
       data3: [{ id: 31, value: '备选项', label: '备选项' }],
       data33: [{ id: 31, value: '备选项', label: '备选项', size: 'large' }],
       data4: [
         { id: 41, value: '选项1', label: '选项1' },
-        { id: 42, value: '选项2', label: '选项2' }
+        { id: 42, value: '选项2', label: '选项2' },
       ],
       data5: [
         { id: 51, value: 'A', label: '选项1' },
         { id: 52, value: 'B', label: '选项2' },
         { id: 53, value: 'C', label: '选项3' },
-        { id: 54, value: 'D', label: '选项4' }
+        { id: 54, value: 'D', label: '选项4' },
       ],
       data6: [
         { id: 51, value: '选项1', label: '选项1' },
         { id: 52, value: '选项2', label: '选项2' },
         { id: 53, value: '选项3', label: '选项3' },
-        { id: 54, value: '选项4', label: '选项4' }
+        { id: 54, value: '选项4', label: '选项4' },
       ],
       data7: [
         { id: 41, value: '选项1', label: '选项1' },
-        { id: 42, value: '选项2', label: '选项2' }
-      ]
+        { id: 42, value: '选项2', label: '选项2' },
+      ],
     };
   },
   mounted() {
@@ -126,8 +126,8 @@ export default {
     },
     checkAll(state) {
       this.$refs.checkboxGroup.toggleAll(state);
-    }
-  }
+    },
+  },
 };
 </script>
 
