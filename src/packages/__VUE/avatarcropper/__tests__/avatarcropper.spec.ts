86afda9ab6d1b48e7ed50ceceb4b01c6079cc7b9
Marvin <454846659@qq.com>
2023-10-01 00:53:24 +0800

    test: update test avatarcropper toouch

--- src/packages/__VUE/avatarcropper/__tests__/avatarcropper.spec.ts
+++ src/packages/__VUE/avatarcropper/__tests__/avatarcropper.spec.ts
@@ -1,7 +1,7 @@
 import 'vitest-canvas-mock';
 import { mount } from '@vue/test-utils';
 import AvatarCropper from '../index.vue';
-import { sleep } from '@/packages/utils/unit';
+import { sleep, trigger, triggerDrag } from '@/packages/utils/unit';
 import { h } from 'vue';
 
 const mockFile = new File([new ArrayBuffer(10000)], 'test.jpg', {
@@ -45,6 +45,13 @@ test('AvatarCropper: Select the image to open the crop window', async () => {
   expect(wrapper.find('.nut-cropper-popup').attributes()).toHaveProperty('style', '');
   const canvas = wrapper.find('.nut-cropper-popup__canvas');
   expect(canvas.exists()).toBe(true);
+
+  const track = wrapper.find('.nut-cropper-popup__highlight');
+
+  trigger(track, 'touchstart', 0, 0);
+  trigger(track, 'touchmove', 0, 20);
+  trigger(track, 'touchend', 0, 100);
+  triggerDrag(track, 0, 100);
 
   const toolbar = wrapper.findAll('.nut-cropper-popup__toolbar-item');
   expect(toolbar.length).toBe(4);
