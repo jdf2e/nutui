0a0a8a8e7f52aa37238361c978c656901acdbba3
mikasayw <mikasayw@163.com>
2023-10-18 15:41:34 +0800

    !fix(input): 修改/优化input属性问题 (#2578)
    * fix(input): 修改/优化input属性问题
    
    * chore(input): move to setup sugar
    
    * test(input): update input snapshot
    
    * fix(input): taro h5与小程序端对齐
    
    ---------
    
    Co-authored-by: Eiinu <eiinu@qq.com>

--- src/packages/__VUE/input/util.ts
+++ src/packages/__VUE/input/util.ts
@@ -1,3 +1,6 @@
+import type { HTMLAttributes, InputHTMLAttributes } from 'vue';
+import type { InputType } from './type';
+
 function trimExtraChar(value: string, char: string, regExp: RegExp) {
   const index = value.indexOf(char);
 
@@ -27,4 +30,27 @@ export function formatNumber(value: string, allowDot = true, allowMinus = true) 
   const regExp = allowDot ? /[^-0-9.]/g : /[^-0-9]/g;
 
   return value.replace(regExp, '');
+}
+
+export function mapInputType(type: InputType): {
+  type: InputHTMLAttributes['type'];
+  inputmode?: HTMLAttributes['inputmode'];
+} {
+  // type="number" is weird in iOS, and can't prevent dot in Android
+  // so use inputmode to set keyboard in modern browsers
+  if (type === 'number') {
+    return {
+      type: 'text',
+      inputmode: 'decimal'
+    };
+  }
+
+  if (type === 'digit') {
+    return {
+      type: 'tel',
+      inputmode: 'numeric'
+    };
+  }
+
+  return { type };
 }
