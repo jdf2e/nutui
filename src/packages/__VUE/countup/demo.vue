93d9ea50c5a79f5c3322fa1c5a8a0aecb1613408
eiinu <eiinu@qq.com>
2023-11-03 16:32:29 +0800

    chore: countup

--- src/packages/__VUE/countup/demo.vue
+++ src/packages/__VUE/countup/demo.vue
@@ -13,8 +13,8 @@
     <h2>{{ translate('title1') }}</h2>
     <div class="show-demo">
       <nut-countup
-        :custom-change-num="customNumber"
-        :custom-bg-img="bgImage"
+        :custom-change-num="data.customNumber"
+        :custom-bg-img="data.bgImage"
         :custom-spac-num="11"
         :num-width="33"
         :num-height="47"
@@ -27,11 +27,11 @@
       <nut-countup
         ref="countupMachine"
         type="machine"
-        :machine-num="machineNum"
+        :machine-num="data.machineNum"
         :machine-prize-num="5"
-        :machine-prize-level="prizeLevel"
+        :machine-prize-level="data.prizeLevel"
         :machine-turn-more="5"
-        :custom-bg-img="bgImage2"
+        :custom-bg-img="data.bgImage2"
         :num-width="100"
         :num-height="100"
         :during="3000"
@@ -39,15 +39,15 @@
       >
       </nut-countup>
       <div class="btnBtn">
-        <nut-button type="danger" :disabled="startFlag" @click="startRole">{{ translate('btn1') }}</nut-button>
-        <nut-button type="danger" :disabled="startFlag" @click="startRole2">{{ translate('btn2') }}</nut-button>
+        <nut-button type="danger" :disabled="data.startFlag" @click="startRole">{{ translate('btn1') }}</nut-button>
+        <nut-button type="danger" :disabled="data.startFlag" @click="startRole2">{{ translate('btn2') }}</nut-button>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { ref, reactive, toRefs, onMounted, defineComponent } from 'vue';
+<script setup lang="ts">
+import { ref, reactive, onMounted } from 'vue';
 import { createComponent } from '@/packages/utils/create';
 const { translate } = createComponent('countup');
 import { useTranslate } from '@/sites/assets/util/useTranslate';
@@ -70,62 +70,47 @@ const initTranslate = () =>
       btn2: 'Not winning'
     }
   });
-export default defineComponent({
-  props: {},
-  setup() {
-    initTranslate();
-    const data = reactive({
-      customNumber: 618,
-      bgImage:
-        'https://img10.360buyimg.com/imagetools/jfs/t1/133024/3/2251/2646/5ee7549aE8dc02d7e/de6901b6c72db396.png',
-      startFlag: false,
-      startFlag2: false,
-      machineNum: 3,
-      bgImage2:
-        'https://img10.360buyimg.com/imagetools/jfs/t1/121466/20/6784/28830/5f06e7f2Edbb8998c/9bdd9e7b24dff9fe.png',
-      prizeLevel: 0
-    });
-    const scrollAniEnd = (val: boolean) => {
-      console.log(val ? '恭喜中奖！！！' : '很遗憾！没有中奖');
-      setTimeout(() => {
-        data.startFlag = false;
-      }, 300);
-    };
-    const run = () => {
-      setInterval(() => {
-        data.customNumber = Math.floor(Math.random() * (700 - 100 + 1) + 100);
-      }, 5000);
-    };
-    const startRole = () => {
-      data.prizeLevel = Math.floor(Math.random() * 5 + 1);
-      console.log(data.prizeLevel, '中奖');
-      data.startFlag = true;
-      // 实际运行情况下，开始滚动函数，需要放在奖品回调函数之后，如果异步操作会影响转动结果
-      setTimeout(() => {
-        (countupMachine.value as any).machineLuck();
-      }, 500);
-    };
-    const countupMachine = ref(null);
-    const startRole2 = () => {
-      data.prizeLevel = -1;
-      console.log(data.prizeLevel, '不中奖');
-      data.startFlag = true;
-      setTimeout(() => {
-        (countupMachine.value as any).machineLuck();
-      }, 500);
-    };
-    onMounted(() => {
-      run();
-    });
-    return {
-      ...toRefs(data),
-      scrollAniEnd,
-      startRole,
-      startRole2,
-      countupMachine,
-      translate
-    };
-  }
+initTranslate();
+const data = reactive({
+  customNumber: 618,
+  bgImage: 'https://img10.360buyimg.com/imagetools/jfs/t1/133024/3/2251/2646/5ee7549aE8dc02d7e/de6901b6c72db396.png',
+  startFlag: false,
+  startFlag2: false,
+  machineNum: 3,
+  bgImage2: 'https://img10.360buyimg.com/imagetools/jfs/t1/121466/20/6784/28830/5f06e7f2Edbb8998c/9bdd9e7b24dff9fe.png',
+  prizeLevel: 0
+});
+const scrollAniEnd = (val: boolean) => {
+  console.log(val ? '恭喜中奖！！！' : '很遗憾！没有中奖');
+  setTimeout(() => {
+    data.startFlag = false;
+  }, 300);
+};
+const run = () => {
+  setInterval(() => {
+    data.customNumber = Math.floor(Math.random() * (700 - 100 + 1) + 100);
+  }, 5000);
+};
+const startRole = () => {
+  data.prizeLevel = Math.floor(Math.random() * 5 + 1);
+  console.log(data.prizeLevel, '中奖');
+  data.startFlag = true;
+  // 实际运行情况下，开始滚动函数，需要放在奖品回调函数之后，如果异步操作会影响转动结果
+  setTimeout(() => {
+    (countupMachine.value as any).machineLuck();
+  }, 500);
+};
+const countupMachine = ref(null);
+const startRole2 = () => {
+  data.prizeLevel = -1;
+  console.log(data.prizeLevel, '不中奖');
+  data.startFlag = true;
+  setTimeout(() => {
+    (countupMachine.value as any).machineLuck();
+  }, 500);
+};
+onMounted(() => {
+  run();
 });
 </script>
 
