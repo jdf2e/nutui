011c72930221a35d7dbbaf780c9974a86beb8938
Eiinu <eiinu@qq.com>
2023-12-13 16:13:14 +0800

    chore(divider): split taro demo (#2730)

---
+++ packages/nutui-taro-demo/src/layout/pages/divider/vertical.vue
@@ -0,0 +1,9 @@
+<template>
+  <div :style="{ fontSize: '14px', marginLeft: '27px', color: '#909ca4' }">
+    text1
+    <nut-divider direction="vertical" />
+    <a href="#" style="color: #1989fa"> link </a>
+    <nut-divider direction="vertical" />
+    text2
+  </div>
+</template>
