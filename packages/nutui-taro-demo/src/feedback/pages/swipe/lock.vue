201053b6da9abbd5f8b37b6043fe85d48ce13cc0
Eiinu <eiinu@qq.com>
2023-12-16 23:27:28 +0800

    chore(swipe): split demo (#2754)

---
+++ packages/nutui-taro-demo/src/feedback/pages/swipe/lock.vue
@@ -0,0 +1,24 @@
+<template>
+  <nut-swipe-group lock>
+    <nut-swipe name="11">
+      <nut-cell round-radius="0" title="Swipe Left" />
+      <template #right>
+        <nut-button shape="square" style="height: 100%" type="danger">Button</nut-button>
+      </template>
+    </nut-swipe>
+    <nut-swipe name="22">
+      <nut-cell round-radius="0" title="Swipe Left" />
+      <template #right>
+        <nut-button shape="square" style="height: 100%" type="danger">Button</nut-button>
+      </template>
+    </nut-swipe>
+    <div>
+      <nut-swipe name="33">
+        <nut-cell round-radius="0" title="Swipe Left" />
+        <template #right>
+          <nut-button shape="square" style="height: 100%" type="danger">Button</nut-button>
+        </template>
+      </nut-swipe>
+    </div>
+  </nut-swipe-group>
+</template>
--- src/packages/__VUE/swipe/demo/index.vue
+++ src/packages/__VUE/swipe/demo/index.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script setup lang="ts">
-import { useTranslate } from '../../../utils';
+import { useTranslate } from '@/sites/utils';
 import Basic from './basic.vue';
 import Disabled from './disabled.vue';
 import Direction from './direction.vue';
