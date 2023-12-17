73f613c35c3fecba7b876abcce863c21821f9fc9
Eiinu <eiinu@qq.com>
2023-12-17 22:06:58 +0800

    chore(skeleton): split demo

--- src/packages/__VUE/skeleton/demo/index.vue
+++ src/packages/__VUE/skeleton/demo/index.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script setup lang="ts">
-import { useTranslate } from '../../../utils';
+import { useTranslate } from '@/sites/utils';
 import Basic from './basic.vue';
 import Row from './row.vue';
 import Avatar from './avatar.vue';
