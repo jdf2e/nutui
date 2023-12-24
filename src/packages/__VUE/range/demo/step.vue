bbb26735ed18a6c70f934e41c2397832ed4616be
Eiinu <eiinu@qq.com>
2023-12-25 00:57:38 +0800

    chore(range): split demo (#2798)

---
+++ src/packages/__VUE/range/demo/step.vue
@@ -0,0 +1,10 @@
+<template>
+  <nut-range v-model="val" :step="5" @change="change"></nut-range>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const val = ref(20);
+const change = (value: number) => {
+  console.log(value);
+};
+</script>
