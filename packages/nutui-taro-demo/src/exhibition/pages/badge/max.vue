b473472a60e5de69b49be399b09a1f50eb643d01
Eiinu <eiinu@qq.com>
2023-12-25 11:06:31 +0800

    chore(badge): split demo (#2799)

---
+++ packages/nutui-taro-demo/src/exhibition/pages/badge/max.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nut-badge :value="200" :max="9">
+      <nut-avatar shape="square"></nut-avatar>
+    </nut-badge>
+    <nut-badge :value="200" :max="20">
+      <nut-avatar shape="square"></nut-avatar>
+    </nut-badge>
+    <nut-badge :value="200" :max="99">
+      <nut-avatar shape="square"></nut-avatar>
+    </nut-badge>
+  </div>
+</template>
--- src/packages/__VUE/badge/demo/index.vue
+++ src/packages/__VUE/badge/demo/index.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script setup lang="ts">
-import { useTranslate } from '../../../utils';
+import { useTranslate } from '@/sites/utils';
 import Basic from './basic.vue';
 import Max from './max.vue';
 import Color from './color.vue';
