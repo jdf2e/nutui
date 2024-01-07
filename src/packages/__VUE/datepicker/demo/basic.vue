b857e57959adaa5e2346c7b0492afde7238d3ded
Eiinu <eiinu@qq.com>
2024-01-08 01:19:05 +0800

    chore(date-picker): split demo

---
+++ src/packages/__VUE/datepicker/demo/basic.vue
@@ -0,0 +1,18 @@
+<template>
+  <nut-date-picker
+    v-model="val"
+    :min-date="min"
+    :max-date="max"
+    :three-dimensional="false"
+    @confirm="confirm"
+  ></nut-date-picker>
+</template>
+<script setup>
+import { ref } from 'vue';
+const min = new Date(2020, 0, 1);
+const max = new Date(2025, 10, 1);
+const val = ref(new Date(2022, 4, 10));
+const confirm = ({ selectedValue }) => {
+  console.log(selectedValue);
+};
+</script>
