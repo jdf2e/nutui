d157cb9e6f2ce882c3646a6be68e974898d6cb52
Eiinu <eiinu@qq.com>
2024-01-16 00:45:59 +0800

    refactor(divider): script setup

--- src/packages/__VUE/divider/doc.en-US.md
+++ src/packages/__VUE/divider/doc.en-US.md
@@ -65,6 +65,17 @@ User can custom divider style with style attribute.
 | --- | --- |
 | default | Default slot, when `direction` = `horizontal` |
 
+### Types
+
+The component exports the following type definitions:
+
+```js
+import type {
+  DividerPosition,
+  DividerDirection
+} from '@nutui/nutui';
+```
+
 ## Theming
 
 ### CSS Variables
---
+++ src/packages/__VUE/divider/index.ts
@@ -0,0 +1,11 @@
+import Divider from './divider.vue';
+import type { ComponentPublicInstance } from 'vue';
+import { withInstall } from '@/packages/utils';
+
+withInstall(Divider);
+
+export type { DividerPosition, DividerDirection } from './types';
+
+export type DividerInstance = ComponentPublicInstance & InstanceType<typeof Divider>;
+
+export { Divider, Divider as default };
