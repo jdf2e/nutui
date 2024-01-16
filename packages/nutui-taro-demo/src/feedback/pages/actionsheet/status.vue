fde04b687e2a432425cfab69ede85d746b590590
Eiinu <eiinu@qq.com>
2024-01-16 22:43:19 +0800

    chore(action-sheet): split demo

---
+++ packages/nutui-taro-demo/src/feedback/pages/actionsheet/status.vue
@@ -0,0 +1,28 @@
+<template>
+  <nut-cell title="click" :desc="val" @click="click"></nut-cell>
+  <nut-action-sheet v-model:visible="show" :menu-items="menuItems" @choose="choose" />
+</template>
+<script setup>
+import { ref } from 'vue';
+const show = ref(false);
+const val = ref('');
+const menuItems = [
+  {
+    name: 'Default'
+  },
+  {
+    name: 'Disabled',
+    disable: true
+  },
+  {
+    name: 'Color',
+    color: 'red'
+  }
+];
+const click = () => {
+  show.value = true;
+};
+const choose = (item) => {
+  val.value = item.name;
+};
+</script>
