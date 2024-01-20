03a61ede5a675378d34cab1de4ca2b56b81d33d2
Eiinu <eiinu@qq.com>
2024-01-21 00:49:38 +0800

    chore(countdown): split demo (#2854)

---
+++ src/packages/__VUE/countdown/demo/control.vue
@@ -0,0 +1,21 @@
+<template>
+  <nut-space align="center">
+    <nut-countdown :end-time="end" :paused="paused" @paused="onPaused" @restart="onRestart" />
+    <nut-button size="small" @click="toggle">{{ paused ? 'start' : 'stop' }}</nut-button>
+  </nut-space>
+</template>
+<script setup>
+import { ref } from 'vue';
+const paused = ref(false);
+const end = ref(Date.now() + 60 * 1000);
+
+const toggle = () => {
+  paused.value = !paused.value;
+};
+const onPaused = (v) => {
+  console.log('paused: ', v);
+};
+const onRestart = (v) => {
+  console.log('restart: ', v);
+};
+</script>
