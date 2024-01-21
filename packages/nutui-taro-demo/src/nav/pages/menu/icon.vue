173c63ed09d83d34f36c663a69ba45a6547eda2e
Eiinu <eiinu@qq.com>
2024-01-21 20:48:11 +0800

    chore(menu): split demo

---
+++ packages/nutui-taro-demo/src/nav/pages/menu/icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <nut-menu>
+    <template #icon>
+      <TriangleDown />
+    </template>
+    <nut-menu-item v-model="val1" :options="options1" />
+    <nut-menu-item v-model="val2" :options="options2">
+      <template #icon>
+        <Checked></Checked>
+      </template>
+    </nut-menu-item>
+  </nut-menu>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { TriangleDown, Checked } from '@nutui/icons-vue-taro';
+const val1 = ref(0);
+const val2 = ref('a');
+const options1 = ref([
+  { text: '全部商品', value: 0 },
+  { text: '新款商品', value: 1 },
+  { text: '活动商品', value: 2 }
+]);
+const options2 = ref([
+  { text: '默认排序', value: 'a' },
+  { text: '好评排序', value: 'b' },
+  { text: '销量排序', value: 'c' }
+]);
+</script>
--- src/packages/__VUE/menu/demo/index.vue
+++ src/packages/__VUE/menu/demo/index.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script setup lang="ts">
-import { useTranslate } from '../../../utils';
+import { useTranslate } from '@/sites/utils';
 import Basic from './basic.vue';
 import Custom from './custom.vue';
 import Col from './col.vue';
