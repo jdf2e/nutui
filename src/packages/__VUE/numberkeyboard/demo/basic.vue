e6067ba9fe59e9919ec91df61702fa88c070a1d6
Eiinu <eiinu@qq.com>
2024-01-23 23:40:54 +0800

    chore(number-keyboard): split demo (#2867)

---
+++ src/packages/__VUE/numberkeyboard/demo/basic.vue
@@ -0,0 +1,21 @@
+<template>
+  <nut-cell title="default" @click="show = true" />
+  <nut-number-keyboard
+    v-model:visible="show"
+    @input="onInput"
+    @delete="onDelete"
+    @blur="show = false"
+    @close="show = false"
+  >
+  </nut-number-keyboard>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const show = ref(false);
+const onInput = (val: string) => {
+  console.log(val);
+};
+const onDelete = () => {
+  console.log('onDelete');
+};
+</script>
