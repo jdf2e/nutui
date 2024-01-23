77ab05f5147213dd6412b740ec25f6fb5149c11d
Eiinu <eiinu@qq.com>
2024-01-23 23:34:45 +0800

    chore(number-keyboard): split demo

---
+++ src/packages/__VUE/numberkeyboard/demo/right.vue
@@ -0,0 +1,21 @@
+<template>
+  <nut-cell title="rightColumn" @click="show = true"></nut-cell>
+  <nut-number-keyboard
+    v-model:visible="show"
+    type="rightColumn"
+    :custom-key="customKey"
+    confirm-text="支付"
+    @input="onInput"
+    @blur="show = false"
+    @close="show = false"
+  >
+  </nut-number-keyboard>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const show = ref(false);
+const customKey = ref(['.']);
+const onInput = (val: string) => {
+  console.log(val);
+};
+</script>
---
+++ src/packages/__VUE/numberkeyboard/demo/value.vue
@@ -0,0 +1,10 @@
+<template>
+  <nut-cell title="v-model" :desc="val" @click="show = true" />
+  <nut-number-keyboard v-model:visible="show" v-model="val" :max-length="6" @blur="show = false" @close="show = false">
+  </nut-number-keyboard>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const show = ref(false);
+const val = ref('');
+</script>
