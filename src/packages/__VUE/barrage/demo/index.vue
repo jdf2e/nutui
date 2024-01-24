a37070f9cc3e5e7eaa4a0ea5e5eeb8d570b930fa
Eiinu <eiinu@qq.com>
2024-01-24 22:25:54 +0800

    chore(barrage): split demo

--- src/packages/__VUE/barrage/demo/index.vue
+++ src/packages/__VUE/barrage/demo/index.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import { useTranslate } from '../../../utils';
+import { useTranslate } from '@/sites/utils';
 import Basic from './basic.vue';
 
 const t = useTranslate({
