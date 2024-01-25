cc7f79a3a467c780aa5cfae9b6353cb1e7ac1231
eiinu <eiinu@qq.com>
2024-01-25 17:49:40 +0800

    fix: update

--- src/packages/__VUE/ellipsis/__tests__/ellipsis.spec.ts
+++ src/packages/__VUE/ellipsis/__tests__/ellipsis.spec.ts
@@ -2,12 +2,20 @@ import { mount } from '@vue/test-utils';
 import { nextTick } from 'vue';
 import Ellipsis from '../index.vue';
 
+const originGetComputedStyle = window.getComputedStyle;
 const lineHeight = 19.5;
-
 const content =
   'NutUI3.0上线后我们研发团队也在不断的优化、测试、使用、迭代 Vue3 的相关组件，但是在跨端小程序的开发过程中，发现没有合适的组件库可以支持多端开发。为了填补这一空白，同时为了优化开发者体验，让 NutUI 能够为更多的开发者带来便利，我们决定在 NutUI 中增加小程序多端适配的能力。';
 
 beforeAll(() => {
+  window.getComputedStyle = (el) => {
+    const style = originGetComputedStyle(el);
+    return {
+      lineHeight: `${lineHeight}px`,
+      paddingTop: style.paddingTop,
+      paddingBottom: style.paddingBottom
+    } as any;
+  };
   Object.defineProperty(HTMLElement.prototype, 'offsetHeight', {
     get() {
       if (this.innerText.includes('...')) {
@@ -17,6 +25,10 @@ beforeAll(() => {
       return lineHeight * 4;
     }
   });
+});
+
+afterAll(() => {
+  window.getComputedStyle = originGetComputedStyle;
 });
 
 test('Ellipsis Props Direction Start', async () => {
