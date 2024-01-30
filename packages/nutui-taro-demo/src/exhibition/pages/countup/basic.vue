488d620e29e1c55e1d6dd8d692fbfcfbec8518e1
Eiinu <eiinu@qq.com>
2024-01-30 11:18:04 +0800

    chore(countup): split dmeo (#2897)

---
+++ packages/nutui-taro-demo/src/exhibition/pages/countup/basic.vue
@@ -0,0 +1,5 @@
+<template>
+  <nut-countup :init-num="0" :end-num="200"></nut-countup>
+  <nut-countup :init-num="150" :end-num="0" :speed="2.62" :to-fixed="2"></nut-countup>
+  <nut-countup :init-num="1000" :end-num="0" :speed="50" :to-fixed="2"></nut-countup>
+</template>
