f045bdd57197b8c2b4f286cb538f62f6c81c6422
eiinu <eiinu@qq.com>
2024-02-04 16:44:57 +0800

    chore(progress): update demo

--- packages/nutui-taro-demo/src/exhibition/pages/progress/content.vue
+++ packages/nutui-taro-demo/src/exhibition/pages/progress/content.vue
@@ -1,9 +1,8 @@
 <template>
   <nut-progress :percentage="60" text-inside>
-    <img
+    <image
       src="https://img11.360buyimg.com/imagetools/jfs/t1/137646/13/7132/1648/5f4c748bE43da8ddd/a3f06d51dcae7b60.png"
-      width="30"
-      height="30"
+      style="width: 30px; height: 30px"
     />
   </nut-progress>
 </template>
