3cdff676cd3e5f45c09e8d75a05ec6f5f006b774
eiinu <eiinu@qq.com>
2024-02-19 16:22:31 +0800

    fix(picker): demo

--- src/packages/__VUE/picker/demo/basic.vue
+++ src/packages/__VUE/picker/demo/basic.vue
@@ -4,13 +4,13 @@
 <script setup>
 import { ref } from 'vue';
 const columns = ref([
-  { text: '南京', value: 'Nanjing', className: 'nanjing-class' },
+  { text: '南京', value: 'Nanjing' },
   { text: '无锡', value: 'Wuxi' },
   { text: '海北', value: 'Haibei' },
   { text: '北京', value: 'Beijing' },
   { text: '连云港', value: 'Lianyungang' },
   { text: '长沙', value: 'Changsha' },
-  { text: '武汉', value: 'Wuhan', className: 'wuhan-class' }
+  { text: '武汉', value: 'Wuhan' }
 ]);
 
 const confirm = ({ selectedValue, selectedOptions }) => {
