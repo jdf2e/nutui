69acb468803a5ca528ccd078505c1b41ba233f5d
eiinu <eiinu@qq.com>
2024-03-08 15:53:57 +0800

    refactor(price): move to script setup

---
+++ src/packages/__VUE/price/index.ts
@@ -0,0 +1,13 @@
+import Price from './price.vue';
+import type { ComponentPublicInstance } from 'vue';
+import { withInstall } from '@/packages/utils';
+
+withInstall(Price);
+
+export type { PriceProps } from './price.vue';
+
+export type {} from './types';
+
+export type PriceInstance = ComponentPublicInstance & InstanceType<typeof Price>;
+
+export { Price, Price as default };
