08f6f2702ff234300528a20033c4e216a695ebb2
eiinu <eiinu@qq.com>
2024-03-11 10:44:42 +0800

    fix(form): 修复 disabled 关联子组件收集问题

--- src/packages/__VUE/form/common.ts
+++ src/packages/__VUE/form/common.ts
@@ -1,9 +1,11 @@
 import { computed } from 'vue';
 import { useParent } from '@/packages/utils';
-import { FORM_KEY } from './types';
 import type { ComputedRef, Ref } from 'vue';
 
+export const FORM_KEY = Symbol('nut-form');
+export const FORM_DISABLED_KEY = Symbol('nut-form-disabled');
+
 export const useFormDisabled = (disabled: Ref<boolean>): ComputedRef<boolean> => {
-  const { parent } = useParent(FORM_KEY);
+  const { parent } = useParent(FORM_DISABLED_KEY);
   return computed(() => disabled.value || parent?.props?.disabled || false);
 };
