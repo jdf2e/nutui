018d83eb5e34bd4d3f4dd3eb28f39dd82c28951a
bordon <1078464063@qq.com>
2024-03-14 15:00:40 +0800

    feat: update

--- src/packages/__VUE/backtop/index.taro.ts
+++ src/packages/__VUE/backtop/index.taro.ts
@@ -1,10 +1,10 @@
-import Backtop from './index.taro.vue';
+import Backtop from './backtop.taro.vue';
 import type { ComponentPublicInstance } from 'vue';
 import { withInstall } from '@/packages/utils';
 
 withInstall(Backtop);
 
-export type { BacktopProps } from './index.taro.vue';
+export type { BacktopProps } from './backtop.taro.vue';
 
 export type BacktopInstance = ComponentPublicInstance & InstanceType<typeof Backtop>;
 
--- src/packages/__VUE/backtop/index.ts
+++ src/packages/__VUE/backtop/index.ts
@@ -1,10 +1,10 @@
-import Backtop from './index.vue';
+import Backtop from './backtop.vue';
 import type { ComponentPublicInstance } from 'vue';
 import { withInstall } from '@/packages/utils';
 
 withInstall(Backtop);
 
-export type { BacktopProps } from './index.vue';
+export type { BacktopProps } from './backtop.vue';
 
 export type BacktopInstance = ComponentPublicInstance & InstanceType<typeof Backtop>;
 
