12399efecbc497b28e233b69516c318ece357442
eiinu <eiinu@qq.com>
2024-03-27 14:18:21 +0800

    chore: eslint format

--- scripts/generate-nutui-taro-vue.cjs
+++ scripts/generate-nutui-taro-vue.cjs
@@ -2,8 +2,8 @@ const packageConfig = require('../package.json')
 const config = require('../src/config.json')
 const path = require('path')
 const fs = require('fs-extra')
-let importStr = `import { App } from 'vue';
-import Locale from './locale';\n`
+let importStr = `import { App } from 'vue'
+import Locale from './locale'\n`
 let importScssStr = `\n`
 let dts = `export {}
 declare module 'vue' {
@@ -15,18 +15,18 @@ config.nav.map((item) => {
     if (taro !== false) {
       if (setup === true) {
         dts += `    Nut${name}: typeof import('@/packages/__VUE/${name.toLowerCase()}/index.taro')['default']\n`
-        importStr += `import ${name} from './__VUE/${name.toLowerCase()}/index.taro';\n`
-        importStr += `export * from './__VUE/${name.toLowerCase()}/index.taro';\n`
+        importStr += `import ${name} from './__VUE/${name.toLowerCase()}/index.taro'\n`
+        importStr += `export * from './__VUE/${name.toLowerCase()}/index.taro'\n`
       } else {
         dts += `    Nut${name}: typeof import('@/packages/__VUE/${name.toLowerCase()}/index.taro.vue')['default']\n`
         const filePath = path.join(`src/packages/__VUE/${name.toLowerCase()}/index.taro.vue`)
         if (name !== 'Icon') {
           importStr += `import ${name} from './__VUE/${name.toLowerCase()}/index${
             fs.existsSync(filePath) ? '.taro' : ''
-          }.vue';\n`
+          }.vue'\n`
         }
       }
-      importScssStr += `import './__VUE/${name.toLowerCase()}/index.scss';\n`
+      importScssStr += `import './__VUE/${name.toLowerCase()}/index.scss'\n`
       if (exclude !== true) {
         packages.push(name)
       }
@@ -34,27 +34,27 @@ config.nav.map((item) => {
   })
 })
 let installFunction = `function install(app: App) {
-  const packages = [${packages.join(',')}];
+  const packages = [${packages.join(',')}]
   packages.forEach((item:any) => {
     if (item.install) {
-      app.use(item);
+      app.use(item)
     } else if (item.name) {
-      app.component(item.name, item);
+      app.component(item.name, item)
     }
-  });
+  })
 }`
 let fileStrBuild = `${importStr}
 ${installFunction}
-const version = '${packageConfig.version}';
-export { install, version, Locale, ${packages.join(',')} };
-export default { install, version, Locale};`
+const version = '${packageConfig.version}'
+export { install, version, Locale, ${packages.join(',')} }
+export default { install, version, Locale}`
 
 fs.outputFile(path.resolve(__dirname, '../src/packages/taro.build.ts'), fileStrBuild, 'utf8')
 let fileStrDev = `${importStr}
 ${installFunction}
 ${importScssStr}
-export { install, Locale, ${packages.join(',')}  };
-export default { install, version:'${packageConfig.version}', Locale};`
+export { install, Locale, ${packages.join(',')}  }
+export default { install, version:'${packageConfig.version}', Locale}`
 fs.outputFile(path.resolve(__dirname, '../src/packages/taro.ts'), fileStrDev, 'utf8')
 
 dts += `  }
--- scripts/generate-nutui.cjs
+++ scripts/generate-nutui.cjs
@@ -2,8 +2,8 @@ const packageConfig = require('../package.json')
 const config = require('../src/config.json')
 const path = require('path')
 const fs = require('fs-extra')
-let importStr = `import { App } from 'vue';
-import Locale from './locale';\n`
+let importStr = `import { App } from 'vue'
+import Locale from './locale'\n`
 let importScssStr = `\n`
 let dts = `export {}
 declare module 'vue' {
@@ -15,47 +15,47 @@ config.nav.map((item) => {
     let { name, funcCall, exclude, setup } = element
     if (setup === true) {
       dts += `    Nut${name}: typeof import('./__VUE/${name.toLowerCase()}/index')['default']\n`
-      importStr += `import ${name} from './__VUE/${name.toLowerCase()}/index';\n`
-      importStr += `export * from './__VUE/${name.toLowerCase()}/index';\n`
+      importStr += `import ${name} from './__VUE/${name.toLowerCase()}/index'\n`
+      importStr += `export * from './__VUE/${name.toLowerCase()}/index'\n`
     } else {
       dts += `    Nut${name}: typeof import('./__VUE/${name.toLowerCase()}/index.vue')['default']\n`
       if (name !== 'Icon') {
-        importStr += `import ${name} from './__VUE/${name.toLowerCase()}/index.vue';\n`
+        importStr += `import ${name} from './__VUE/${name.toLowerCase()}/index.vue'\n`
       }
       if (funcCall === true) {
-        importStr += `import { show${name} } from './__VUE/${name.toLowerCase()}/index';\n`
+        importStr += `import { show${name} } from './__VUE/${name.toLowerCase()}/index'\n`
         methods.push(`show${name}`)
       }
     }
-    importScssStr += `import './__VUE/${name.toLowerCase()}/index.scss';\n`
+    importScssStr += `import './__VUE/${name.toLowerCase()}/index.scss'\n`
     if (exclude != true) {
       packages.push(name)
     }
   })
 })
 let installFunction = `function install(app: App) {
-  const packages = [${packages.join(',')}];
+  const packages = [${packages.join(',')}]
   packages.forEach((item:any) => {
     if (item.install) {
-      app.use(item);
+      app.use(item)
     } else if (item.name) {
-      app.component(item.name, item);
+      app.component(item.name, item)
     }
-  });
+  })
 }`
 let fileStrBuild = `${importStr}
 ${installFunction}
-const version = '${packageConfig.version}';
-export { install, version, Locale, ${packages.join(',')}, ${methods.join(',')}};
-export default { install, version};`
+const version = '${packageConfig.version}'
+export { install, version, Locale, ${packages.join(',')}, ${methods.join(',')}}
+export default { install, version}`
 
 fs.outputFile(path.resolve(__dirname, '../src/packages/index.build.ts'), fileStrBuild, 'utf8')
 
 let fileStrDev = `${importStr}
 ${installFunction}
 ${importScssStr}
-export { install, Locale, ${packages.join(',')}, ${methods.join(',')}  };
-export default { install, version:'${packageConfig.version}'};`
+export { install, Locale, ${packages.join(',')}, ${methods.join(',')}  }
+export default { install, version:'${packageConfig.version}'}`
 fs.outputFile(path.resolve(__dirname, '../src/packages/index.ts'), fileStrDev, 'utf8')
 
 dts += `  }
