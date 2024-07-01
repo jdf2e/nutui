233a6fa7b14740b81ac6c0d88ac0e8023a8d71dc
Eiinu <eiinu@qq.com>
2024-07-02 01:03:10 +0800

    chore(deps): update

--- src/packages/__VUE/tabbar/__tests__/index.spec.tsx
+++ src/packages/__VUE/tabbar/__tests__/index.spec.tsx
@@ -35,10 +35,10 @@ test('should render custom img when using img prop', async () => {
             icon: (props: any) =>
               props.active
                 ? (
-                  <img src="http://img13.360buyimg.com/uba/jfs/t1/29316/38/1115/3203/5c0f3d61E35d0c7da/9e557f2cb5c9dab6.jpg" />
+                    <img src="http://img13.360buyimg.com/uba/jfs/t1/29316/38/1115/3203/5c0f3d61E35d0c7da/9e557f2cb5c9dab6.jpg" />
                   )
                 : (
-                  <img src="http://img20.360buyimg.com/uba/jfs/t1/9996/36/8646/4833/5c0f3d61E7c1b7e0f/c98ad61124172e93.jpg" />
+                    <img src="http://img20.360buyimg.com/uba/jfs/t1/9996/36/8646/4833/5c0f3d61E7c1b7e0f/c98ad61124172e93.jpg" />
                   )
           }}
         </TabbarItem>
@@ -47,10 +47,10 @@ test('should render custom img when using img prop', async () => {
             icon: (props: any) =>
               props.active
                 ? (
-                  <img src="http://img13.360buyimg.com/uba/jfs/t1/29316/38/1115/3203/5c0f3d61E35d0c7da/9e557f2cb5c9dab6.jpg" />
+                    <img src="http://img13.360buyimg.com/uba/jfs/t1/29316/38/1115/3203/5c0f3d61E35d0c7da/9e557f2cb5c9dab6.jpg" />
                   )
                 : (
-                  <img src="http://img20.360buyimg.com/uba/jfs/t1/9996/36/8646/4833/5c0f3d61E7c1b7e0f/c98ad61124172e93.jpg" />
+                    <img src="http://img20.360buyimg.com/uba/jfs/t1/9996/36/8646/4833/5c0f3d61E7c1b7e0f/c98ad61124172e93.jpg" />
                   )
           }}
         </TabbarItem>
