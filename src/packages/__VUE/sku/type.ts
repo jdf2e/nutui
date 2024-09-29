c735a283dc3395108857636da9009870e3619925
huxiyang3 <1872591453@qq.com>
2024-09-29 18:27:10 +0800

    fix: ts

--- src/packages/__VUE/sku/type.ts
+++ src/packages/__VUE/sku/type.ts
@@ -1,7 +1,7 @@
-export interface SkuInfo {
+export interface SkuInfoBase {
   name: string
   id: number
   active: boolean
   disable: boolean
-  [props: string]: any
 }
+export type SkuInfo<T = unknown> = SkuInfoBase & Record<string, T>
